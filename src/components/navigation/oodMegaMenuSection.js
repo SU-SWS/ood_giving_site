a76f5abae6994717b4d7befaea747385cdf6ff27
Katria Lesser <Katria@stanford.edu>
2020-08-19 10:38:30 -0600

    ADAPT-567: Adjusted to use a button element for main nav section.

--- src/components/navigation/oodMegaMenuSection.js
+++ src/components/navigation/oodMegaMenuSection.js
@@ -6,7 +6,7 @@ import Components from "../components";
 const OodMegaMenuSection = (props) => (
     <SbEditable content={props.blok}>
         {/*<div className="ood-mega-menu--section">*/}
-            <div className="ood-mega-menu--section-top-link">{ props.blok.linkText }</div>
+            <button className="ood-mega-menu--section-top-link">{ props.blok.linkText }</button>
             <div className="ood-mega-menu--section">
                 {props.blok.navigationColumns && props.blok.navigationColumns.map((blok) => React.createElement(Components(blok.component), {
                     key: blok._uid,
