51d217b6fb12eef8e31e6f6e333e4de096b3eba9
Katria Lesser <Katria@stanford.edu>
2020-08-26 16:39:53 -0600

    ADAPT-573: Updated navigation link commponent with storyblok link component.

--- src/components/navigation/oodNavigationLink.js
+++ src/components/navigation/oodNavigationLink.js
@@ -4,16 +4,24 @@ import SbEditable from 'storyblok-react'
 import Components from "../components";
 
 const OodNavigationLink = (props) => (
+
     <SbEditable content={props.blok}>
         <li className="nav-item active">
-            <Link className="nav-link" to={'/' + (props.blok.url === 'home' ? '' : props.blok.link.cached_url)}>
-                {props.blok.name}
-            </Link>
+            {props.blok.link.linktype === "story" &&
+            <Link to={`/${props.blok.link.cached_url}/`} className={props.blok.linkClass ? "su-link " + props.blok.linkClass : "su-link "} activeClassName="active">
+                {props.blok.linkTextLabel}
+            </Link>}
+            {props.blok.link.linktype === "url" &&
+            <a href={props.blok.link.url} className={props.blok.linkClass ? "su-link su-link--external " + props.blok.linkClass : "su-link su-link--external"}>
+                { props.blok.linkText }
+            </a>}
         </li>
-        <div className={props.blok.linkClass ? "ood-navigation-link " + props.blok.linkClass : "ood-navigation-link "}>
-            { props.blok.linkText }
-            { props.blok.url }
-        </div>
+            {/* Check if we need anything about url home in here? */}
+            {/*<Link className="nav-link" to={'/' + (props.blok.url === 'home' ? '' : props.blok.link.cached_url)}>*/}
+            {/*    {props.blok.name}*/}
+            {/*</Link>*/}
+
+
     </SbEditable>
 )
 
