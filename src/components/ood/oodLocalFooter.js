72f9601eabfa97414e25ac99d64610b14d8d89db
Yvonne Tang <ytang2@stanford.edu>
2020-08-26 17:08:32 -0700

    Local Footer stuff

--- src/components/ood/oodLocalFooter.js
+++ src/components/ood/oodLocalFooter.js
@@ -20,7 +20,21 @@ const OodLocalFooter = (props) => (
               <h2 className="ood-local-footer__list-heading">{props.blok.contactHeading}</h2>
             )}
             <address className="ood-local-footer__address">
-
+              {props.blok.addressLine1 &&
+                <span>{props.blok.addressLine1}</span>
+              }
+              {props.blok.addressLine2 &&
+                <span>{props.blok.addressLine2}</span>
+              }
+              {props.blok.addressLine3 &&
+                <span>{props.blok.addressLine3}</span>
+              }
+              {props.blok.phone &&
+                <span>{props.blok.phone}</span>
+              }
+              {props.blok.email &&
+              <a href={`mailto:${props.blok.email}`}>{props.blok.email}</a>
+              }
             </address>
           </div>
 
