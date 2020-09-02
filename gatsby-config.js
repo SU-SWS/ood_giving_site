ac21eca44a119a758b1843ac0717955b6d09e592
Yvonne Tang <ytang2@stanford.edu>
2020-09-01 23:18:23 -0700

    Remove gatsby-plugin manifest options; add title to doc head

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
   siteMetadata: {
     title: `Giving to Stanford`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@SU-SWS`,
+    author: `Stanford University Office of Development`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -40,13 +40,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Giving to Stanford`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
