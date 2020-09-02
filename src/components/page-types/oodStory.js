6045855873868dc5ef031a4c2e59dd2902187d68
Yvonne Tang <ytang2@stanford.edu>
2020-09-02 11:50:15 -0700

    Use React Helmet to dynamically add story title to HTML head title

--- src/components/page-types/oodStory.js
+++ src/components/page-types/oodStory.js
@@ -4,6 +4,8 @@ import SbEditable from 'storyblok-react'
 import RichTextField from '../richTextField'
 import transformImage from '../../utilities/transformImage'
 import Components from "../components"
+import { Helmet } from 'react-helmet';
+
 
 const OodStory = (props) => {
   let processedHeroImg = "";
@@ -12,43 +14,48 @@ const OodStory = (props) => {
   }
 
   return (
-    <article className="ood-story" id="main-content">
-      <header className="ood-story__header">
-        {props.blok.heroImage && (
-          <figure className={`su-media ood-story__media`}>
-            <img src={processedHeroImg} alt={props.blok.heroImage.alt}
-                 className={`ood-story__image su-obj-position-h-center-v-${props.blok.visibleVertical}`}
-            />
-          </figure>
-        )}
-        <div className="centered-container flex-container ood-story__header-content">
-          <div className="ood-story__header-content-wrapper flex-md-12-of-12 flex-lg-10-of-12 flex-2xl-9-of-12">
-            <h1 className="ood-story__title">{props.blok.title}</h1>
-            {props.blok.intro && (
-              <p className="su-intro-text ood-story__intro-text">{props.blok.intro}</p>
+    <>
+      <Helmet><title>{props.blok.title} | Giving to Stanford</title></Helmet>
+      <SbEditable content={props.blok}>
+        <article className="ood-story" id="main-content">
+          <header className="ood-story__header">
+            {props.blok.heroImage && (
+              <figure className={`su-media ood-story__media`}>
+                <img src={processedHeroImg} alt={props.blok.heroImage.alt}
+                     className={`ood-story__image su-obj-position-h-center-v-${props.blok.visibleVertical}`}
+                />
+              </figure>
             )}
-          </div>
-        </div>
-      </header>
-      <div className="ood-story__content">
-        {props.blok.storyContent && props.blok.storyContent.map((blok) => React.createElement(Components(blok.component), {
-          key: blok._uid,
-          blok: blok
-        }))}
-      </div>
-      <footer className="ood-story__footer">
-        <div className="centered-container flex-container">
-          <div className="ood-story__footer-wrapper flex-md-10-of-12 flex-lg-8-of-12 flex-2xl-6-of-12">
-            <div className="ood-story__metadata">
-              <p>Author</p>
-              <span>{props.blok.author}</span>
-              <p>Date</p>
-              <span>{props.blok.publishedDate}</span>
+            <div className="centered-container flex-container ood-story__header-content">
+              <div className="ood-story__header-content-wrapper flex-md-12-of-12 flex-lg-10-of-12 flex-2xl-9-of-12">
+                <h1 className="ood-story__title">{props.blok.title}</h1>
+                {props.blok.intro && (
+                  <p className="su-intro-text ood-story__intro-text">{props.blok.intro}</p>
+                )}
+              </div>
             </div>
+          </header>
+          <div className="ood-story__content">
+            {props.blok.storyContent && props.blok.storyContent.map((blok) => React.createElement(Components(blok.component), {
+              key: blok._uid,
+              blok: blok
+            }))}
           </div>
-        </div>
-      </footer>
-    </article>
+          <footer className="ood-story__footer">
+            <div className="centered-container flex-container">
+              <div className="ood-story__footer-wrapper flex-md-10-of-12 flex-lg-8-of-12 flex-2xl-6-of-12">
+                <div className="ood-story__metadata">
+                  <p>Author</p>
+                  <span>{props.blok.author}</span>
+                  <p>Date</p>
+                  <span>{props.blok.publishedDate}</span>
+                </div>
+              </div>
+            </div>
+          </footer>
+        </article>
+      </SbEditable>
+    </>
   )
 }
 
