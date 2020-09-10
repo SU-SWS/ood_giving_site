0bd4a6389c5c08744570af306732f8bfd8341413
Yvonne Tang <ytang2@stanford.edu>
2020-09-10 08:41:33 -0700

    Add SBeditable to embed script component

--- src/components/embedScript.js
+++ src/components/embedScript.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import SbEditable from "storyblok-react";
 
-const EmbedScript = (props) => (
-    <div
-      dangerouslySetInnerHTML={{
-        __html: props.blok.script,
-      }}
-    />
-)
+const EmbedScript = (props) => {
+  return (
+    <SbEditable content={props.blok}>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: props.blok.script,
+        }}
+      />
+    </SbEditable>
+  )
+}
 
 export default EmbedScript
