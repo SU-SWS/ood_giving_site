37b9140ec08e98b57595a4341cd77491a0289752
Yvonne Tang <ytang2@stanford.edu>
2020-09-10 22:40:28 -0700

    ADAPT-676 Add optional related links section for content menu and add styling (#13)
    * Add optional related links section for content menu and styling
    
    * Add SBeditable to embed script component
    
    * Update packages including Decanter; import alert styles and remove lockup version d styles

--- src/components/navigation/oodContentMenu.js
+++ src/components/navigation/oodContentMenu.js
@@ -5,17 +5,33 @@ import Components from "../components";
 const OodContentMenu = (props) => {
   return (
     <SbEditable content={props.blok}>
-      {props.blok.menuTitle && (
-        <h2>{props.blok.menuTitle}</h2>
-      )}
       <nav className="su-secondary-nav ood-content-nav no-js" id="content-nav" aria-label="Content Menu">
-        <ul
-          className="su-secondary-nav__menu su-secondary-nav__menu-lv1 ood-content-nav__menu ood-content-nav__menu-lv1">
-          {props.blok.menuLinks && props.blok.menuLinks.map((blok) => React.createElement(Components(blok.component), {
-            key: blok._uid,
-            blok: blok,
-          }))}
-        </ul>
+        <div className={`ood-content-nav__menu-group`}>
+          {props.blok.menuTitle && (
+            <h2 className="ood-content-nav__title">{props.blok.menuTitle}</h2>
+          )}
+          <ul
+            className="su-secondary-nav__menu su-secondary-nav__menu-lv1 ood-content-nav__menu ood-content-nav__menu-lv1">
+            {props.blok.menuLinks && props.blok.menuLinks.map((blok) => React.createElement(Components(blok.component), {
+              key: blok._uid,
+              blok: blok,
+            }))}
+          </ul>
+        </div>
+        {props.blok.relatedMenuLinks && (
+          <div className={`ood-content-nav__menu-group`}>
+            {props.blok.relatedMenuTitle && (
+              <h2 className="ood-content-nav__title">{props.blok.relatedMenuTitle}</h2>
+            )}
+            <ul
+              className="su-secondary-nav__menu su-secondary-nav__menu-lv1 ood-content-nav__menu ood-content-nav__menu-lv1 ood-content-nav__menu-related">
+              {props.blok.relatedMenuLinks && props.blok.relatedMenuLinks.map((blok) => React.createElement(Components(blok.component), {
+                key: blok._uid,
+                blok: blok,
+              }))}
+            </ul>
+          </div>
+        )}
       </nav>
     </SbEditable>
   )
