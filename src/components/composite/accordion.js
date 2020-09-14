cd751e18cb0ee67e2b2dae71321cbf88b5300eea
Yvonne Tang <ytang2@stanford.edu>
2020-09-14 13:42:05 -0700

    Accordion styles

--- src/components/composite/accordion.js
+++ src/components/composite/accordion.js
@@ -6,11 +6,16 @@ import Components from "../components";
 const Accordion = (props) => {
   return (
     <SbEditable content={props.blok}>
-      <div className={`su-accordion no-js ood-accordion`}>
-        <h2 className={`ood-accordion__title`}>{props.blok.title}</h2>
+      <div className={`su-accordion no-js ood-accordion
+           ${props.blok.spacingTop !== "none" ? `su-pt-${props.blok.spacingTop}` : ""}
+           ${props.blok.spacingBottom !== "none" ? `su-pb-${props.blok.spacingBottom}` : ""}
+      `}>
+        {props.blok.title &&
+          <h2 className={`ood-accordion__heading su-serif`}>{props.blok.title}</h2>
+        }
         <button className="su-accordion__collapse-all su-button--secondary">Collapse all -</button>
         <button className="su-accordion__expand-all su-button--secondary">Expand all +</button>
-        <ul className="su-accordion__list ood-accordion__list">
+        <ul className="su-accordion__list ood-accordion__list su-border-color-palo-alto-light">
           {props.blok.accordionItems && props.blok.accordionItems.map((blok) => React.createElement(Components(blok.component), {
             key: blok._uid,
             blok: blok,
--- src/components/composite/accordionItem.js
+++ src/components/composite/accordionItem.js
@@ -8,11 +8,11 @@ const AccordionItem = (props) => {
 
   return (
     <SbEditable content={props.blok}>
-      <li className="su-accordion__item">
-        <Heading className="su-accordion__title">
-          <button className="su-accordion__button" aria-expanded="false">{props.blok.title}</button>
+      <li className="su-accordion__item ood-accordion__item su-border-color-palo-alto-light">
+        <Heading className={`su-accordion__title ood-accordion__title su-serif`}>
+          <button className="su-accordion__button ood-accordion__button" aria-expanded="false">{props.blok.title}</button>
         </Heading>
-        <div className="su-accordion__content">
+        <div className="su-accordion__content ood-accordion__content">
           <RichTextField data={props.blok.content}/>
         </div>
       </li>
