e5a3d3eb70f1c18df7787e3f32a0a1f703d2c4d9
Yvonne Tang <ytang2@stanford.edu>
2020-09-14 23:02:09 -0700

    Add fluid typography mixin and use it for interior page header with image; responsive styles for interior page header

--- src/components/page-types/oodInteriorPage.js
+++ src/components/page-types/oodInteriorPage.js
@@ -8,10 +8,10 @@ import { Helmet } from 'react-helmet';
 const HeaderWithImage = (props) => (
   <SbEditable content={props.blok}>
     <header className={`ood-interior-page__header ood-interior-page__header--has-image`}>
-      <div className={`ood-interior-page__header-title-wrapper su-py-6 su-bg-${props.blok.headerBackgroundColor}`}>
+      <div className={`ood-interior-page__header-title-wrapper su-pt-6 su-pb-5 su-bg-${props.blok.headerBackgroundColor}`}>
         <div className={`centered-container flex-container`}>
-          <h1 className="ood-interior-page__title flex-lg-5-of-12 flex-xl-5-of-12 flex-2xl-6-of-12 su-serif su-text-white su-text-align-left">{props.blok.title}</h1>
-          <figure className="su-media flex-lg-7-of-12 flex-xl-7-of-12 flex-2xl-6-of-12 ood-interior-page__header-media">
+          <h1 className="ood-interior-page__title flex-lg-6-of-12 flex-xl-5-of-12 flex-2xl-6-of-12 su-serif su-text-white su-text-align-left">{props.blok.title}</h1>
+          <figure className="su-media flex-lg-6-of-12 flex-xl-7-of-12 flex-2xl-6-of-12 ood-interior-page__header-media">
             <div className="su-media__wrapper su-aspect-ratio su-aspect-ratio--3x2">
               <img className="ood-interior-page__image" src={props.blok.headerImage.filename ? props.blok.headerImage.filename : ""} alt="" />
             </div>
