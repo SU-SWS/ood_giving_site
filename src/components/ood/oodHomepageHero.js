15a14cd6c6144ba047d8f8bcead57a93dbc2b776
Yvonne Tang <ytang2@stanford.edu>
2020-09-17 16:34:25 -0700

    Hero section refactor; header

--- src/components/ood/oodHomepageHero.js
+++ src/components/ood/oodHomepageHero.js
@@ -14,7 +14,7 @@ const OodHomepageHero = (props) => (
       </figure>
       <div className="centered-container flex-container ood-hero-home__content">
         <h1 className={`flex-lg-8-of-12 ood-hero-home__splash-text su-semibold su-mod-type-${props.blok.splashTextSize} su-text-white su-text-focus-in`}>{props.blok.splashText}</h1>
-        <a className={`flex-lg-4-of-12 ood-hero-home__link su-text-white su-bg-${props.blok.ctaBackgroundColor}`} href={props.blok.link} rel="nofollow noopener">
+        <a className={`flex-lg-4-of-12 ood-hero-home__link su-bg-${props.blok.ctaBackgroundColor}`} href={props.blok.link} rel="nofollow noopener">
           <h2 className={`ood-hero-home__cta-headline su-text-white`}>{props.blok.ctaHeadline}</h2>
           <p className={`su-link--action ood-hero-home__cta-text su-text-white su-text-align-right`}>{props.blok.ctaText}</p>
         </a>
