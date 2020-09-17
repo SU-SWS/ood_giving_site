a830040ca9f9116244467097abd93afbfb453bac
Yvonne Tang <ytang2@stanford.edu>
2020-09-17 16:48:12 -0700

    Interior page title alignment for minimal header version

--- src/components/page-types/oodInteriorPage.js
+++ src/components/page-types/oodInteriorPage.js
@@ -35,7 +35,7 @@ const HeaderMinimal = (props) => (
   <SbEditable content={props.blok}>
     <header className={`ood-interior-page__header ood-interior-page__header--minimal su-bg-fog-light su-py-7`}>
       <div className={`centered-container`}>
-        <h1 className="ood-interior-page__title su-serif su-text-align-left">{props.blok.title}</h1>
+        <h1 className="ood-interior-page__title su-serif su-text-align-center">{props.blok.title}</h1>
       </div>
     </header>
   </SbEditable>
