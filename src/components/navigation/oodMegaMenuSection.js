760776d41b0c7df229defa4d1dc296b7405f3185
Yvonne Tang <ytang2@stanford.edu>
2020-09-18 16:09:37 -0700

    Format and BEM naming fixup

--- src/components/navigation/oodMegaMenuSection.js
+++ src/components/navigation/oodMegaMenuSection.js
@@ -4,18 +4,15 @@ import SbEditable from 'storyblok-react'
 import Components from "../components";
 
 const OodMegaMenuSection = (props) => (
-    <SbEditable content={props.blok}>
-        {/*<div className="ood-mega-menu--section">*/}
-            <button className="ood-mega-menu--section-top-link">{ props.blok.linkText }</button>
-            <div className="ood-mega-menu--section">
-                {props.blok.navigationColumns && props.blok.navigationColumns.map((blok) => React.createElement(Components(blok.component), {
-                    key: blok._uid,
-                    blok: blok,
-                }))}
-
-            </div>
-        {/*</div>*/}
-    </SbEditable>
+  <SbEditable content={props.blok}>
+    <button className="ood-mega-menu__trigger">{props.blok.linkText}</button>
+    <div className="ood-mega-menu__section">
+      {props.blok.navigationColumns && props.blok.navigationColumns.map((blok) => React.createElement(Components(blok.component), {
+        key: blok._uid,
+        blok: blok,
+      }))}
+    </div>
+  </SbEditable>
 )
 
 export default OodMegaMenuSection
