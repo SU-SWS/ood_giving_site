01bf02c9cf81eda866c43599a7571a0af73abb42
Yvonne Tang <ytang2@stanford.edu>
2020-09-18 00:44:58 -0700

    Add spacing top and bottom option for story content WYSIWYG

--- src/components/layout/singleColumnContent.js
+++ src/components/layout/singleColumnContent.js
@@ -4,7 +4,10 @@ import RichTextField from '../richTextField'
 
 const SingleColumnContent = (props) => (
   <SbEditable content={props.blok}>
-    <div className={`ood-single-column-content su-bg-${props.blok.backgroundColor}`}>
+    <div className={`ood-single-column-content su-bg-${props.blok.backgroundColor}
+                   ${props.blok.spacingTop !== "none" ? `su-pt-${props.blok.spacingTop}` : ""}
+                   ${props.blok.spacingBottom !== "none" ? `su-pb-${props.blok.spacingBottom}` : ""}
+    `}>
       <div id={props.blok.id} className={`centered-container flex-container}`}>
         <div class={`ood-single-column-content__wrapper ${props.blok.contentWidth}`}>
           <RichTextField data={props.blok.content}/>
