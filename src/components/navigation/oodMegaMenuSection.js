72800db846023bf0c71d236bf43aa2d089c1cf5e
Yvonne Tang <ytang2@stanford.edu>
2020-09-18 16:49:16 -0700

    Mega Menu Section fixup

--- src/components/navigation/oodMegaMenuSection.js
+++ src/components/navigation/oodMegaMenuSection.js
@@ -6,7 +6,11 @@ const OodMegaMenuSection = (props) => (
   <SbEditable content={props.blok}>
     <button className="ood-mega-menu__trigger">{props.blok.linkText}</button>
     <div className="ood-mega-menu__section">
-      {props.blok.navigationColumns && props.blok.navigationColumns.map((blok) => React.createElement(Components(blok.component), {
+      {props.blok.linkGroups && props.blok.linkGroups.map((blok) => React.createElement(Components(blok.component), {
+        key: blok._uid,
+        blok: blok,
+      }))}
+      {props.blok.card && props.blok.card.map((blok) => React.createElement(Components(blok.component), {
         key: blok._uid,
         blok: blok,
       }))}
