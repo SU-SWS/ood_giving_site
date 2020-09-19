69ff0a78d3aeaf6bbe2a0ba9073dd3a62f2a3598
Yvonne Tang <ytang2@stanford.edu>
2020-09-18 17:37:18 -0700

    fixup mega menu card

--- src/components/navigation/oodMegaMenuCard.js
+++ src/components/navigation/oodMegaMenuCard.js
@@ -3,17 +3,22 @@ import Link from 'gatsby-link'
 import SbEditable from 'storyblok-react'
 
 const OodMegaMenuCard = (props) => (
-    <SbEditable content={props.blok}>
+  <SbEditable content={props.blok}>
     <div className="ood-mega-menu__card">
-    <div className="ood-mega-menu__card-image">
-        <img src={props.blok.image.filename} />
-    </div>
-    <div className={props.blok.backgroundColor ? "ood-mega-menu__card-content su-text-white su-bg-" + props.blok.backgroundColor : "ood-mega-menu--card--text-area su-bg-bright-red"}>
-        <h3 className="ood-mega-menu__card-heading">{props.blok.heading}</h3>
+      {props.blok.image.filename && (
+        <figure className="su-media ood-mega-menu__card-media">
+          <div className="su-media__wrapper su-aspect-ratio su-aspect-ratio--3x2">
+            <img className="ood-mega-menu__card-image" src={props.blok.image.filename} alt="" />
+          </div>
+        </figure>
+      )}
+      <div
+        className={props.blok.backgroundColor ? "ood-mega-menu__card-content su-text-white su-bg-" + props.blok.backgroundColor : "ood-mega-menu--card--text-area su-bg-bright-red"}>
+        <h3 className="ood-mega-menu__card-heading su-semibold">{props.blok.heading}</h3>
         <div className="ood-mega-menu--card-cta">{props.blok.ctaText}</div>
+      </div>
     </div>
-    </div>
-    </SbEditable>
+  </SbEditable>
 )
 
 export default OodMegaMenuCard
