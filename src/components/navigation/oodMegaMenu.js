15e344ea035104517ef24fa7d022698bf4f460a8
Yvonne Tang <ytang2@stanford.edu>
2020-09-19 01:17:07 -0700

    Mega menu no js version for desktop working (works with mouse on hover and keyboard tabbing)

--- src/components/navigation/oodMegaMenu.js
+++ src/components/navigation/oodMegaMenu.js
@@ -5,7 +5,7 @@ import Components from "../components";
 
 const OodMegaMenu = (props) => (
   <SbEditable content={props.blok}>
-    <nav className="ood-mega-nav" aria-label="Main Menu">
+    <nav className="ood-mega-nav no-js" aria-label="Main Menu">
       <button className="ood-mega-nav__toggle su-mr-none su-ml-auto" aria-expanded="false">Menu</button>
       <ul className="ood-mega-nav__menu-lv1 su-list-none">
         {props.blok.topLevelLinks && props.blok.topLevelLinks.map((blok) => React.createElement(Components(blok.component), {
