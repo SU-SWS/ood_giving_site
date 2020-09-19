e0d0ddd544dec425c3d97e1b4882e77af429f585
Yvonne Tang <ytang2@stanford.edu>
2020-09-19 11:19:49 -0700

    Mega menu top level active link style; add focus style for filter buttons on areas to support page; add logic for rendering person info for quote cards; add pseudo element decorators; make scroll bar always appear in browser (#26)

--- src/components/cards/oodQuoteCard.js
+++ src/components/cards/oodQuoteCard.js
@@ -10,8 +10,13 @@ const OodQuoteCard = (props) => (
     }>
       <div className="ood-quote-card__content">
         <blockquote className={`ood-quote-card__quote su-before-color-${props.blok.quotationMarkColor}`}>{props.blok.quoteText}</blockquote>
-        <p className="ood-quote-card__name">{props.blok.personName}</p>
-        <p className="ood-quote-card__bio">{`${props.blok.personTitle}, ${props.blok.personClassYear}`}</p>
+        {props.blok.personName && (
+          <p className="ood-quote-card__name">{props.blok.personName}</p>
+        )}
+        {(props.blok.personClassYear || props.blok.personTitle) && (
+          <p className="ood-quote-card__bio">
+            {`${props.blok.personTitle}${(props.blok.personTitle && props.blok.personClassYear) ? ", " : ""}${props.blok.personClassYear}`}</p>
+        )}
       </div>
       {(props.blok.photo.filename && props.blok.showImage === "has-image") && (
         <figure className="su-media ood-quote-card__media">
