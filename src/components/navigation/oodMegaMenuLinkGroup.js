de9e4a492372e3431816b2ee417663a0801f4bfe
Yvonne Tang <ytang2@stanford.edu>
2020-09-18 22:54:48 -0700

    More mega menu desktop styles and CTA link button styles; remove redundant header scss file

--- src/components/navigation/oodMegaMenuLinkGroup.js
+++ src/components/navigation/oodMegaMenuLinkGroup.js
@@ -5,7 +5,9 @@ import Components from "../components";
 const OodMegaMenuLinkGroup = (props) => (
   <SbEditable content={props.blok}>
     <div className="ood-mega-nav__link-group flex-lg-3-of-12">
-      <h3 className={`ood-mega-nav__link-group-heading su-uppercase su-bold`}>{props.blok.heading}</h3>
+      {props.blok.heading && (
+        <h3 className={`ood-mega-nav__link-group-heading su-uppercase su-bold`}>{props.blok.heading}</h3>
+      )}
       <ul className="ood-mega-nav__menu-lv2 su-list-none">
         {props.blok.links && props.blok.links.map((blok) => React.createElement(Components(blok.component), {
           key: blok._uid,
