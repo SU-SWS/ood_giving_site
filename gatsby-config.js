ceb0f6dc100cea8b083f089b5f06df5c72d7bafc
Shea McKinney <sherakama@gmail.com>
2020-09-25 15:15:37 -0700

    fixup! nope.

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-react-helmet-canonical-urls`,
       options: {
-        siteUrl: `${site.siteMetadata.siteUrl}`,
+        siteUrl: 'https://giving-preview.stanford.edu',
       },
     },
     {
