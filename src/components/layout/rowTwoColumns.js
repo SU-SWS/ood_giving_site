0470f31f8ac835be0fc559155a8bd04f64ca3c96
Yvonne Tang <ytang2@stanford.edu>
2020-09-24 22:25:55 -0700

    Add row width option to Rows with 2 Columns

--- src/components/layout/rowTwoColumns.js
+++ src/components/layout/rowTwoColumns.js
@@ -5,15 +5,17 @@ import SbEditable from 'storyblok-react'
 const RowTwoColumns = (props) => (
   <SbEditable content={props.blok}>
     <div className={`flex-container row--2-columns su-align-items-${props.blok.contentAlignment} su-mb-${props.blok.spacingBottom}`}>
-      <div className={`${props.blok.oneColumnMd === true ? "flex-lg": "flex-md"}${!props.blok.widthRatio ? "-6-of-12" : ""}${props.blok.widthRatio === "1-to-2" ? "-4-of-12" : ""}${props.blok.widthRatio === "2-to-1" ? "-8-of-12" : ""}`}>
-        {props.blok.columnOneContent.map((blok) =>
-          React.createElement(Components(blok.component), {key: blok._uid, blok: blok})
-        )}
-      </div>
-      <div className={`${props.blok.oneColumnMd === true ? "flex-lg": "flex-md"}${!props.blok.widthRatio ? "-6-of-12" : ""}${props.blok.widthRatio === "1-to-2" ? "-8-of-12" : ""}${props.blok.widthRatio === "2-to-1" ? "-4-of-12" : ""}`}>
-        {props.blok.columnTwoContent.map((blok) =>
-          React.createElement(Components(blok.component), {key: blok._uid, blok: blok})
-        )}
+      <div className={`flex-container su-mx-auto ${props.blok.rowWidth}`}>
+        <div className={`${props.blok.oneColumnMd === true ? "flex-lg": "flex-md"}${!props.blok.widthRatio ? "-6-of-12" : ""}${props.blok.widthRatio === "1-to-2" ? "-4-of-12" : ""}${props.blok.widthRatio === "2-to-1" ? "-8-of-12" : ""}`}>
+          {props.blok.columnOneContent.map((blok) =>
+            React.createElement(Components(blok.component), {key: blok._uid, blok: blok})
+          )}
+        </div>
+        <div className={`${props.blok.oneColumnMd === true ? "flex-lg": "flex-md"}${!props.blok.widthRatio ? "-6-of-12" : ""}${props.blok.widthRatio === "1-to-2" ? "-8-of-12" : ""}${props.blok.widthRatio === "2-to-1" ? "-4-of-12" : ""}`}>
+          {props.blok.columnTwoContent.map((blok) =>
+            React.createElement(Components(blok.component), {key: blok._uid, blok: blok})
+          )}
+        </div>
       </div>
     </div>
   </SbEditable>
