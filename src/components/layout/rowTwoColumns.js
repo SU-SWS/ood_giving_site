058dcd10f0e07bda3f2c6ba18c93f3a4c45f8961
Yvonne Tang <ytang2@stanford.edu>
2020-09-24 21:58:16 -0700

    Add 3 width ratio options for Row with 2 items component

--- src/components/layout/rowTwoColumns.js
+++ src/components/layout/rowTwoColumns.js
@@ -5,12 +5,12 @@ import SbEditable from 'storyblok-react'
 const RowTwoColumns = (props) => (
   <SbEditable content={props.blok}>
     <div className={`flex-container row--2-columns su-align-items-${props.blok.contentAlignment} su-mb-${props.blok.spacingBottom}`}>
-      <div className={`${props.blok.oneColumnMd === true ? "flex-lg-6-of-12": "flex-md-6-of-12"}`}>
+      <div className={`${props.blok.oneColumnMd === true ? "flex-lg": "flex-md"}${!props.blok.widthRatio ? "-6-of-12" : ""}${props.blok.widthRatio === "1-to-2" ? "-4-of-12" : ""}${props.blok.widthRatio === "2-to-1" ? "-8-of-12" : ""}`}>
         {props.blok.columnOneContent.map((blok) =>
           React.createElement(Components(blok.component), {key: blok._uid, blok: blok})
         )}
       </div>
-      <div className={`${props.blok.oneColumnMd === true ? "flex-lg-6-of-12": "flex-md-6-of-12"}`}>
+      <div className={`${props.blok.oneColumnMd === true ? "flex-lg": "flex-md"}${!props.blok.widthRatio ? "-6-of-12" : ""}${props.blok.widthRatio === "1-to-2" ? "-8-of-12" : ""}${props.blok.widthRatio === "2-to-1" ? "-4-of-12" : ""}`}>
         {props.blok.columnTwoContent.map((blok) =>
           React.createElement(Components(blok.component), {key: blok._uid, blok: blok})
         )}
