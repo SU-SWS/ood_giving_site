fd83687723927eff0b96cfe4eed19f123f61e56b
Yvonne Tang <ytang2@stanford.edu>
2020-09-30 23:39:09 -0700

    Add heading level options for tile card and icon cards

--- src/components/cards/oodIconCard.js
+++ src/components/cards/oodIconCard.js
@@ -2,27 +2,34 @@ import React from 'react'
 import Link from "gatsby-link"
 import SbEditable from 'storyblok-react'
 
-const OodIconCard = (props) => (
-  <SbEditable content={props.blok}>
-    <article className={`su-card su-card--link ood-icon-card
+const OodIconCard = (props) => {
+  const Heading = props.blok.headingLevel;
+
+  return (
+    <SbEditable content={props.blok}>
+      <article className={`su-card su-card--link ood-icon-card
              ${(props.blok.backgroundColor !== "white" && props.blok.backgroundColor !== "fog-light") ? "su-text-white" : ""}
              su-text-align-${props.blok.contentAlign}`
-    }>
-      <a href={props.blok.link} rel="nofollow noopener" className={`tile-card__link su-bg-${props.blok.backgroundColor}`}>
-        <div className="ood-icon-card__media">
+      }>
+        <a href={props.blok.link} rel="nofollow noopener"
+           className={`tile-card__link su-bg-${props.blok.backgroundColor}`}>
+          <div className="ood-icon-card__media">
           <span aria-hidden="true"
                 className={`ood-icon-card__icon
                 ${props.blok.iconStyle ? props.blok.iconStyle : props.blok.icon.type}
                 ${props.blok.extraIcon ? `fa-${props.blok.extraIcon}` : props.blok.icon.icon}
                 ${(props.blok.backgroundColor !== "white" && props.blok.backgroundColor !== "fog-light") ? "su-text-white" : "su-text-digital-red"}
-           `} />
-        </div>
-        <section className="su-card__contents ood-icon-card__contents">
-          <h2 className="ood-icon-card__headline su-semibold">{props.blok.headline}</h2>
-        </section>
-      </a>
-    </article>
-  </SbEditable>
-)
+           `}/>
+          </div>
+          <section className="su-card__contents ood-icon-card__contents">
+            <Heading
+              className="ood-icon-card__headline su-semibold">{props.blok.headline}
+            </Heading>
+          </section>
+        </a>
+      </article>
+    </SbEditable>
+  )
+}
 
 export default OodIconCard
--- src/components/cards/oodTileCard.js
+++ src/components/cards/oodTileCard.js
@@ -3,9 +3,11 @@ import { Link } from "gatsby"
 import SbEditable from 'storyblok-react'
 
 const OodTileCard = (props) => {
+  const Heading = props.blok.headingLevel;
+
   const TileCardContent = (props) => (
     <SbEditable content={props.blok}>
-      {(props.blok.image.filename && props.blok.showImage === true) && (
+      {(props.blok.image.filename != null && props.blok.showImage === true) && (
         <figure className="su-media ood-tile-card__media">
           <div className="su-media__wrapper su-aspect-ratio su-aspect-ratio--3x2">
             <img className="ood-tile-card__image" src={props.blok.image.filename} alt="" />
@@ -16,7 +18,9 @@ const OodTileCard = (props) => {
         {props.blok.superheadline && (
           <span className="ood-tile-card__superhead su-uppercase su-bold">{props.blok.superheadline}</span>
         )}
-        <h2 className="ood-tile-card__headline su-semibold">{props.blok.headline}</h2>
+        {props.blok.headline && (
+          <Heading className="ood-tile-card__headline su-semibold">{props.blok.headline}</Heading>
+        )}
       </section>
     </SbEditable>
   );
@@ -24,7 +28,7 @@ const OodTileCard = (props) => {
   return (
     <SbEditable content={props.blok}>
       <article className={`ood-tile-card
-             ${(props.blok.image.filename && props.blok.showImage === true) ? "ood-tile-card--has-image" : "ood-tile-card--no-image"}
+             ${(props.blok.image.filename != null && props.blok.showImage === true) ? "ood-tile-card--has-image" : "ood-tile-card--no-image"}
              ${(props.blok.backgroundColor !== "white" && props.blok.backgroundColor !== "fog-light") ? "su-text-white" : ""}`
       }>
         {props.blok.link.linktype === "story" &&
