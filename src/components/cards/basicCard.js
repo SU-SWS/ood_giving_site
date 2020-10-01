f630f586d324d207fe2367b50f8be6103333c842
Yvonne Tang <ytang2@stanford.edu>
2020-10-01 08:36:30 -0700

    add ood-superhead mixin; more clean up

--- src/components/cards/basicCard.js
+++ src/components/cards/basicCard.js
@@ -5,19 +5,20 @@ import RichTextField from '../../utilities/richTextField'
 
 const BasicCard = (props) => {
   const Heading = props.blok.headingLevel;
+
   return (
     <SbEditable content={props.blok}>
-      <article className={`ood-basic-card
+      <article className={`ood-basic-card ood-shadow-shallow
                ${props.blok.orientation ? "ood-basic-card--horizontal" : ""}
                su-bg-${props.blok.backgroundColor}
                ${props.blok.backgroundColor !== "white" ? `su-border-color-${props.blok.backgroundColor}`: ""}
                ${(props.blok.orientation && props.blok.largeCardPadding === true) ? "su-pt-5" : ""}
                ${props.blok.largeCardPadding === true ? "su-px-5 su-pb-5" : "su-px-2 su-pb-2"}
                su-text-align-${props.blok.textAlign}
-               ${(props.blok.image.filename && props.blok.showImage === true) ? "ood-basic-card--has-image" : "ood-basic-card--no-image"}
+               ${(props.blok.image.filename != null && props.blok.showImage === true) ? "ood-basic-card--has-image" : "ood-basic-card--no-image"}
                ${(props.blok.backgroundColor !== "white" && props.blok.backgroundColor !== "fog-light") ? "su-text-white" : ""}`
       }>
-          {(props.blok.image.filename && props.blok.showImage === true) && (
+          {(props.blok.image.filename != null && props.blok.showImage === true) && (
             <figure className={`su-media ood-basic-card__media`}>
               <div className={`su-media__wrapper su-aspect-ratio--${props.blok.imageAspectRatio}`}>
                 <img className="ood-basic-card__image" src={props.blok.image.filename} alt="" />
