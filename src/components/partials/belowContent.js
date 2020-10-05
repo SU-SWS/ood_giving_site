f6ba6d1981e4f7e50c96c1fc9221e05445ca51ef
Yvonne Tang <ytang2@stanford.edu>
2020-10-05 16:28:54 -0700

    Add belowContent to partials and add it to Areas to Support page type (#50)

---
+++ src/components/partials/belowContent.js
@@ -0,0 +1,18 @@
+import SbEditable from "storyblok-react";
+import React from "react";
+import Components from "../components"
+
+const BelowContent = (props) => (
+  <SbEditable content={props.blok}>
+    {(props.blok.belowContent != null && Object.keys(props.blok.belowContent).length > 0) && (
+      <section className="ood-interior-page__below-body">
+        {props.blok.belowContent && props.blok.belowContent.map((blok) => React.createElement(Components(blok.component), {
+          key: blok._uid,
+          blok: blok
+        }))}
+      </section>
+    )}
+  </SbEditable>
+);
+
+export default BelowContent
