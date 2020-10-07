a8456a4fea101587059ce56702eb396be5d922c9
Yvonne Tang <ytang2@stanford.edu>
2020-10-07 13:23:41 -0700

    Add item horizontal alignment option

--- src/components/layout/rowOneColumn.js
+++ src/components/layout/rowOneColumn.js
@@ -5,13 +5,11 @@ import SbEditable from 'storyblok-react'
 const RowOneColumn = (props) => (
   <SbEditable content={props.blok}>
     <div className={`flex-container row--1-column su-mb-${props.blok.spacingBottom}`}>
-      <div className={`flex-container su-mx-auto ${props.blok.rowWidth}`}>
-        <div className={``}>
-          {props.blok.columnContent.map((blok) => React.createElement(Components(blok.component), {
-            key: blok._uid,
-            blok: blok
-          }))}
-        </div>
+      <div className={`${props.blok.align} ${props.blok.rowWidth}`}>
+        {props.blok.columnContent.map((blok) => React.createElement(Components(blok.component), {
+          key: blok._uid,
+          blok: blok
+        }))}
       </div>
     </div>
   </SbEditable>
