ae2a92a37c572f493a77b60ce9fe5d908b3372dc
Sherakama <sherakama@gmail.com>
2020-10-07 16:39:33 -0700

    ADAPT-679: Update javascript compile from Decanter. (#46)
    * ADAPT-679: Update javascript compile from Decanter.
    
    * ADAPT-679: Updated accordion and JS compilation.
    
    * fixup! removed comments.
    
    * ADAPT-679: Make buttons conditional to having more than 1 child.

--- src/components/composite/accordionItem.js
+++ src/components/composite/accordionItem.js
@@ -1,23 +1,55 @@
 import React from 'react'
 import SbEditable from 'storyblok-react'
 import RichTextField from '../../utilities/richTextField'
-import Components from "../components";
 
-const AccordionItem = (props) => {
-  const Heading = props.blok.headingLevel ? props.blok.headingLevel : "h4";
+class AccordionItem extends React.Component {
 
-  return (
-    <SbEditable content={props.blok}>
-      <li className="su-accordion__item ood-accordion__item su-border-color-palo-alto-light">
-        <Heading className={`su-accordion__title ood-accordion__title su-serif`}>
-          <button className="su-accordion__button ood-accordion__button" aria-expanded="false">{props.blok.title}</button>
-        </Heading>
-        <div className="su-accordion__content ood-accordion__content">
-          <RichTextField data={props.blok.content}/>
-        </div>
-      </li>
-    </SbEditable>
-  )
+  // This is required.
+  constructor(props) {
+    super(props);
+    this.state = {
+      expanded: true
+    };
+  }
+
+  // On render, collapse all the items.
+  componentDidMount() {
+    this.setState({expanded: false});
+  }
+
+  // Expand/Collapse toggle.
+  toggle = () => {
+    this.state.expanded ? this.collapse() : this.expand();
+  }
+
+  // Open the doors!
+  expand = () => {
+    this.setState({ expanded: true });
+  }
+
+  // Close the hatches!
+  collapse = () => {
+    this.setState({ expanded: false });
+  }
+
+  // RENDER!
+  render() {
+    let Heading = this.props.blok.headingLevel ? this.props.blok.headingLevel : "h4";
+    let props = this.props;
+
+    return (
+      <SbEditable content={props.blok}>
+        <li className="su-accordion__item ood-accordion__item su-border-color-palo-alto-light">
+          <Heading className={`su-accordion__title ood-accordion__title su-serif`}>
+            <button className="su-accordion__button ood-accordion__button" aria-expanded={this.state.expanded} onClick={this.toggle}>{props.blok.title}</button>
+          </Heading>
+          <div className="su-accordion__content ood-accordion__content" aria-hidden={!this.state.expanded}>
+            <RichTextField data={props.blok.content}/>
+          </div>
+        </li>
+      </SbEditable>
+    )
+  }
 }
 
 export default AccordionItem
