27bae45e878c5923357593d5a6e2fe6c956a0f8f
Shea McKinney <sherakama@gmail.com>
2020-10-07 15:41:34 -0700

    fixup! removed comments.

--- src/components/composite/accordion.js
+++ src/components/composite/accordion.js
@@ -2,48 +2,6 @@ import React from 'react'
 import SbEditable from 'storyblok-react'
 import Components from "../components"
 
-// document.addEventListener('DOMContentLoaded', event => {
-//   Array.prototype.forEach.call(titleButtons, btn => {
-//     setExpanded(btn, 'false');
-//     setHidden(btn.parentNode.nextElementSibling, 'true');
-//   });
-// });
-
-// Array.prototype.forEach.call(titleButtons, btn => {
-//   btn.addEventListener('click', function (e) {
-//     if (!isExpanded(btn)) {
-//       setExpanded(btn, 'true');
-//       setHidden(btn.parentNode.nextElementSibling, 'false');
-//     } else {
-//       setExpanded(btn, 'false');
-//       setHidden(btn.parentNode.nextElementSibling, 'true');
-//     }
-//   }, false);
-// });
-
-// Array.prototype.forEach.call(expandButtons, expandBtn => {
-//   expandBtn.addEventListener('click', function (e) {
-//     const closestAccordion = expandBtn.closest('.su-accordion');
-//     const closestBtns = closestAccordion.querySelectorAll('.su-accordion__button');
-//     Array.prototype.forEach.call(closestBtns, closestBtn => {
-//       setExpanded(closestBtn, 'true');
-//       setHidden(closestBtn.parentNode.nextElementSibling, 'false');
-//     });
-//   }, false);
-// });
-
-// Array.prototype.forEach.call(collapseButtons, collapseBtn => {
-//   collapseBtn.addEventListener('click', function (e) {
-//     const closestAccordion = collapseBtn.closest('.su-accordion');
-//     const closestBtns = closestAccordion.querySelectorAll('.su-accordion__button');
-//     Array.prototype.forEach.call(closestBtns, closestBtn => {
-//       setExpanded(closestBtn, 'false');
-//       setHidden(closestBtn.parentNode.nextElementSibling, 'true');
-//     });
-//   }, false);
-// });
-
-
 class Accordion extends React.Component {
 
   // Must build the things.
