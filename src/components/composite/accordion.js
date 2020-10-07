77f3f185d1a5c2907ff21b1b6279a3c2298a7836
Shea McKinney <sherakama@gmail.com>
2020-10-07 15:29:04 -0700

    ADAPT-679: Updated accordion and JS compilation.

--- src/components/composite/accordion.js
+++ src/components/composite/accordion.js
@@ -1,30 +1,109 @@
 import React from 'react'
 import SbEditable from 'storyblok-react'
-import Components from "../components";
+import Components from "../components"
 
-const Accordion = (props) => {
-  const Heading = props.blok.headingLevel ? props.blok.headingLevel : "h3";
+// document.addEventListener('DOMContentLoaded', event => {
+//   Array.prototype.forEach.call(titleButtons, btn => {
+//     setExpanded(btn, 'false');
+//     setHidden(btn.parentNode.nextElementSibling, 'true');
+//   });
+// });
 
-  return (
-    <SbEditable content={props.blok}>
-      <div className={`su-accordion ood-accordion
-           ${props.blok.spacingTop !== "none" ? `su-pt-${props.blok.spacingTop}` : ""}
-           ${props.blok.spacingBottom !== "none" ? `su-pb-${props.blok.spacingBottom}` : ""}
-      `}>
-        {props.blok.title &&
-          <Heading className={`ood-accordion__heading su-serif`}>{props.blok.title}</Heading>
-        }
-        <button className="su-accordion__collapse-all su-button ood-cta__button--secondary">Collapse all -</button>
-        <button className="su-accordion__expand-all su-button ood-cta__button--secondary">Expand all +</button>
-        <ul className="su-accordion__list ood-accordion__list su-border-color-palo-alto-light">
-          {props.blok.accordionItems && props.blok.accordionItems.map((blok) => React.createElement(Components(blok.component), {
-            key: blok._uid,
-            blok: blok,
-          }))}
-        </ul>
-      </div>
-    </SbEditable>
-  )
+// Array.prototype.forEach.call(titleButtons, btn => {
+//   btn.addEventListener('click', function (e) {
+//     if (!isExpanded(btn)) {
+//       setExpanded(btn, 'true');
+//       setHidden(btn.parentNode.nextElementSibling, 'false');
+//     } else {
+//       setExpanded(btn, 'false');
+//       setHidden(btn.parentNode.nextElementSibling, 'true');
+//     }
+//   }, false);
+// });
+
+// Array.prototype.forEach.call(expandButtons, expandBtn => {
+//   expandBtn.addEventListener('click', function (e) {
+//     const closestAccordion = expandBtn.closest('.su-accordion');
+//     const closestBtns = closestAccordion.querySelectorAll('.su-accordion__button');
+//     Array.prototype.forEach.call(closestBtns, closestBtn => {
+//       setExpanded(closestBtn, 'true');
+//       setHidden(closestBtn.parentNode.nextElementSibling, 'false');
+//     });
+//   }, false);
+// });
+
+// Array.prototype.forEach.call(collapseButtons, collapseBtn => {
+//   collapseBtn.addEventListener('click', function (e) {
+//     const closestAccordion = collapseBtn.closest('.su-accordion');
+//     const closestBtns = closestAccordion.querySelectorAll('.su-accordion__button');
+//     Array.prototype.forEach.call(closestBtns, closestBtn => {
+//       setExpanded(closestBtn, 'false');
+//       setHidden(closestBtn.parentNode.nextElementSibling, 'true');
+//     });
+//   }, false);
+// });
+
+
+class Accordion extends React.Component {
+
+  // Must build the things.
+  constructor(props) {
+    super(props);
+    this.state = {
+      expanded: true
+    };
+
+    // Storage for the accordion items.
+    this.accordionItems = [];
+
+    // If there are neste accordion items we need to track them so we can call their handlers.
+    if (this.props.blok.accordionItems !== undefined) {
+      this.accordionItems = this.props.blok.accordionItems.map((item, index) => {
+        return React.createRef();
+      })
+    }
+  }
+
+  // Expand all children accordion items.
+  expandAll = () => {
+    this.setState({ expanded: true });
+    this.accordionItems.map((item) => {
+      item.current.expand();
+    })
+  }
+
+  // Collapse all children accordion items.
+  collapseAll = () => {
+    this.setState({ expanded: false });
+    this.accordionItems.map((item) => {
+      item.current.collapse();
+    })
+  }
+
+  // Render method.
+  render() {
+    let props = this.props;
+
+    return (
+      <SbEditable content={props.blok}>
+        <div className={`su-accordion ${props.blok.classes}
+            ${props.blok.spacingTop !== "none" ? `su-pt-${props.blok.spacingTop}` : ""}
+            ${props.blok.spacingBottom !== "none" ? `su-pb-${props.blok.spacingBottom}` : ""}
+        `}>
+          {props.blok.title &&
+            <h2 className={`ood-accordion__heading su-serif`}>{props.blok.title}</h2>
+          }
+          <button className="su-accordion__collapse-all su-button ood-cta__button--secondary" onClick={this.collapseAll}>Collapse all -</button>
+          <button className="su-accordion__expand-all su-button ood-cta__button--secondary" onClick={this.expandAll}>Expand all +</button>
+          <ul className="su-accordion__list ood-accordion__list su-border-color-palo-alto-light">
+            {props.blok.accordionItems && props.blok.accordionItems.map((blok, index) =>
+              React.createElement(Components(blok.component), { key: blok._uid, blok: blok, ref: this.accordionItems[index]})
+            )}
+          </ul>
+        </div>
+      </SbEditable>
+    )
+  }
 }
 
-export default Accordion
+export default Accordion;
