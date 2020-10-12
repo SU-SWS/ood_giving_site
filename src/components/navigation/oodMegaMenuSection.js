04f95c06475b546dd3ee888f63a6de6263104654
Yvonne Tang <ytang2@stanford.edu>
2020-10-12 14:31:22 -0700

    megamenu tweak

--- src/components/navigation/oodMegaMenuSection.js
+++ src/components/navigation/oodMegaMenuSection.js
@@ -8,7 +8,7 @@ const OodMegaMenuSection = (props) => (
       <button className="ood-mega-nav__trigger">{props.blok.linkText}</button>
       <section className="ood-mega-nav__section su-bg-white">
         <div className="centered-container flex-container su-py-3">
-          <div className="flex-lg-8-of-12 su-flex su-flex-col">
+          <div className="flex-lg-8-of-12 flex-2xl-9-of-12 su-flex su-flex-col">
             <div className="flex-container">
               {props.blok.linkGroups && props.blok.linkGroups.map((blok) => React.createElement(Components(blok.component), {
                 key: blok._uid,
@@ -20,7 +20,7 @@ const OodMegaMenuSection = (props) => (
               blok: blok,
             }))}
           </div>
-          <div className="flex-lg-4-of-12">
+          <div className="flex-lg-4-of-12 flex-2xl-3-of-12">
             {props.blok.card && props.blok.card.map((blok) => React.createElement(Components(blok.component), {
               key: blok._uid,
               blok: blok,
