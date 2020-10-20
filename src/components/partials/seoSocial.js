8c449f8d8bc12a5b3e7287e73727ac8998a559be
Yvonne Tang <ytang2@stanford.edu>
2020-10-19 22:40:22 -0700

    more metadata including OG image and Twitter image

--- src/components/partials/seoSocial.js
+++ src/components/partials/seoSocial.js
@@ -2,29 +2,45 @@ import React from 'react'
 import { Helmet } from "react-helmet"
 import SbEditable from "storyblok-react"
 import UseSiteMetadata from "../../hooks/useSiteMetadata"
+import transformImage from "../../utilities/transformImage"
+
+/*
+** If no Twitter specific metadata is provided,
+* Twitter can still read the generic OG metadata.
+*/
 
 const SeoSocial = (props) => {
   const { title } = UseSiteMetadata();
+  let ogImage = props.blok.seo.og_image;
+  let twitterImage = props.blok.seo.twitter_image;
+
+  if (ogImage !== "") {
+    ogImage = transformImage(ogImage, "/1200x630");
+  }
+
+  if (twitterImage !== "") {
+    twitterImage = transformImage(twitterImage, "/1200x600");
+  }
 
   return (
     <SbEditable content={props.blok}>
       <Helmet>
         <title>{`${props.blok.title} | ${title}`}</title>
-        {props.blok.seo.description &&
+        {(props.blok.seo.description || props.blok.teaser) &&
           <meta name="description"
-              content={props.blok.seo.description} />
+              content={props.blok.seo.description || props.blok.teaser} />
         }
-        {props.blok.seo.og_title &&
+        {(props.blok.seo.og_title || props.blok.title) &&
           <meta property="og:title"
-              content={props.blok.seo.og_title} />
+              content={props.blok.seo.og_title || props.blok.title} />
         }
-        {props.blok.seo.og_description &&
+        {(props.blok.seo.og_description || props.blok.teaser) &&
           <meta property="og:description"
-              content={props.blok.seo.og_description} />
+              content={props.blok.seo.og_description || props.blok.teaser} />
         }
-        {(props.blok.seo.og_image || props.blok.seo.twitter_image || props.blok.image || props.blok.heroImage || props.blok.cardImage) &&
+        {ogImage !== "" &&
           <meta property="og:image"
-              content={props.blok.seo.og_image} />
+              content={ogImage} />
         }
         {props.blok.seo.twitter_title &&
           <meta name="twitter:title"
@@ -34,9 +50,9 @@ const SeoSocial = (props) => {
           <meta name="twitter:description"
               content={props.blok.seo.twitter_description} />
         }
-        {(props.blok.seo.twitter_image || props.blok.seo.og_image || props.blok.image || props.blok.heroImage || props.blok.cardImage) &&
+        {twitterImage !== "" &&
           <meta name="twitter:image"
-              content={props.blok.seo.twitter_image} />
+              content={twitterImage} />
         }
       </Helmet>
     </SbEditable>
