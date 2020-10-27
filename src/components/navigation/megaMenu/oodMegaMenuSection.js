074830e0ed4843e0f0d2a4a1a46dd5c2f38952da
Yvonne Tang <ytang2@stanford.edu>
2020-10-26 23:03:39 -0700

    Change onClick function to not be inline

--- src/components/navigation/megaMenu/oodMegaMenuSection.js
+++ src/components/navigation/megaMenu/oodMegaMenuSection.js
@@ -5,12 +5,16 @@ import CreateBloks from "../../../utilities/createBloks"
 const OodMegaMenuSection = (props) => {
   const [sectionOpened, setSectionOpened] = useState(false);
 
+  const toggleSection = () => {
+    setSectionOpened(!sectionOpened);
+  }
+
   return (
     <SbEditable content={props.blok}>
       <li className="ood-mega-nav__item--parent">
         <button className="ood-mega-nav__trigger"
                 aria-expanded={sectionOpened}
-                onClick={() => setSectionOpened(!sectionOpened)}>{props.blok.linkText}</button>
+                onClick={toggleSection}>{props.blok.linkText}</button>
         <section className="ood-mega-nav__section su-bg-white" aria-hidden={!sectionOpened}>
           <div className="centered-container flex-container su-py-3">
             <div className="flex-lg-8-of-12 flex-2xl-9-of-12 su-flex su-flex-col">
