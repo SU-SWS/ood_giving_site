31188faa7c3a3e54cf0f2103f4047949d51b4169
Shea McKinney <sherakama@gmail.com>
2020-10-27 11:43:16 -0700

    Moar progress.

--- lambda/idcs/utils/config.js
+++ lambda/idcs/utils/config.js
@@ -1,6 +1,6 @@
 // lambda/utils/config.js
 // Circumvent problem with Netlify CLI.
 // https://github.com/netlify/netlify-dev-plugin/issues/147
-exports.BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:8000' : process.env.BASE_URL
+exports.BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:64946' : process.env.BASE_URL
 exports.COOKIE_SECURE = process.env.NODE_ENV !== 'development'
-exports.SECRET = process.env.SECRET || 'SUPERSECRET'
+exports.SECRET = process.env.SECRET || '!S00P3R$ECR3T!'
