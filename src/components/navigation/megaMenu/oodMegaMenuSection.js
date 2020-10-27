63b94b7ad4ce8b1db9f75a2a9b90ae4a0b391d0a
Yvonne Tang <ytang2@stanford.edu>
2020-10-27 10:37:30 -0700

    ADAPT-783: Mega Menu dropdowns open on click (#72)

--- src/components/navigation/megaMenu/oodMegaMenuSection.js
+++ src/components/navigation/megaMenu/oodMegaMenuSection.js
@@ -1,26 +1,36 @@
-import React from 'react'
-import SbEditable from 'storyblok-react'
+import React, {useState} from "react"
+import SbEditable from "storyblok-react"
 import CreateBloks from "../../../utilities/createBloks"
 
-const OodMegaMenuSection = (props) => (
-  <SbEditable content={props.blok}>
-    <li className="ood-mega-nav__item--parent">
-      <button className="ood-mega-nav__trigger">{props.blok.linkText}</button>
-      <section className="ood-mega-nav__section su-bg-white">
-        <div className="centered-container flex-container su-py-3">
-          <div className="flex-lg-8-of-12 flex-2xl-9-of-12 su-flex su-flex-col">
-            <div className="flex-container">
-              <CreateBloks blokSection={props.blok.linkGroups} />
+const OodMegaMenuSection = (props) => {
+  const [sectionOpened, setSectionOpened] = useState(false);
+
+  const toggleSection = () => {
+    setSectionOpened(!sectionOpened);
+  }
+
+  return (
+    <SbEditable content={props.blok}>
+      <li className="ood-mega-nav__item--parent">
+        <button className="ood-mega-nav__trigger"
+                aria-expanded={sectionOpened}
+                onClick={toggleSection}>{props.blok.linkText}</button>
+        <div className="ood-mega-nav__section su-bg-white" aria-hidden={!sectionOpened}>
+          <div className="centered-container flex-container su-py-3">
+            <div className="flex-lg-8-of-12 flex-2xl-9-of-12 su-flex su-flex-col">
+              <div className="flex-container">
+                <CreateBloks blokSection={props.blok.linkGroups} />
+              </div>
+              <CreateBloks blokSection={props.blok.sectionCtaLink} />
             </div>
-            <CreateBloks blokSection={props.blok.sectionCtaLink} />
-          </div>
-          <div className="flex-lg-4-of-12 flex-2xl-3-of-12">
-            <CreateBloks blokSection={props.blok.card} />
+            <div className="flex-lg-4-of-12 flex-2xl-3-of-12">
+              <CreateBloks blokSection={props.blok.card} />
+            </div>
           </div>
         </div>
-      </section>
-    </li>
-  </SbEditable>
-)
+      </li>
+    </SbEditable>
+  )
+}
 
 export default OodMegaMenuSection
