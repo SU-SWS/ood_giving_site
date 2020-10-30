d58dfe7901bfd12949c42a2cb6aca546b88d7b7b
Yvonne Tang <ytang2@stanford.edu>
2020-10-30 12:31:38 -0700

    ADAPT-1050 interior page finetune (#68)
    * Interior page header image now 9 of 12 column for MD breakpoint and right aligned
    
    * Finetune interior page header font sizes and spacing for all 3 version
    
    * Step down icon card font size
    
    * Poster mobile spacing and font size finetune
    
    * Step down icon card padding at md and lg breakpoint and they're in 3 column row
    
    * Poster font size, image height and spacing finetune
    
    * poster card bottom padding fixup
    
    * cc fixup
    
    * Finetune title/intro text alignment and font sizes for different variants and breakpoints
    
    * MD breakpoint title font size
    
    * Have icon cards go 1 column at MD
    
    * Fiinetune section header spacing
    
    * More section header finetuning for mobile
    
    * Fixup title font size
    
    * fixup
    
    * Creat a mixin for h1 font size
    
    * fixup for when phpstorm did a hard wrapping for JS at 80 characters which added unnecessary concatenation

--- src/components/partials/headerWithImage.js
+++ src/components/partials/headerWithImage.js
@@ -10,13 +10,13 @@ const HeaderWithImage = (props) => (
     <header className={`ood-interior-page__header ood-interior-page__header--has-image`}>
       <div className={`ood-interior-page__header-title-wrapper su-pt-6 su-pb-5 su-bg-${props.blok.headerBackgroundColor}`}>
         <div className={`centered-container flex-container`}>
-          <h1 className="ood-interior-page__title flex-lg-6-of-12 flex-xl-5-of-12 flex-2xl-6-of-12 su-serif su-text-white su-text-align-left">{props.blok.title}</h1>
+          <h1 className="ood-interior-page__title flex-lg-6-of-12 flex-xl-5-of-12 flex-2xl-6-of-12 su-serif su-text-white">{props.blok.title}</h1>
           <AspectRatioImage
             {...props}
             filename={props.blok.headerImage.filename}
             alt={props.blok.headerImage.alt}
             classPrefix={"ood-interior-page__header"}
-            otherClasses={"flex-lg-6-of-12 flex-xl-7-of-12 flex-2xl-6-of-12"}
+            otherClasses={"flex-md-9-of-12 flex-lg-6-of-12 flex-xl-7-of-12 flex-2xl-6-of-12 su-ml-auto su-mr-none"}
             imageSize={"header"}
             aspectRatio={"3x2"}
           />
