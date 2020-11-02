bef8a2bcf03e464f29622597cb8d746dd73da67c
Yvonne Tang <ytang2@stanford.edu>
2020-11-02 10:07:06 -0800

    Add background color option for minimal header; finetune interior page body title font size for mobile

--- src/components/partials/headerMinimal.js
+++ src/components/partials/headerMinimal.js
@@ -5,7 +5,7 @@ import React from "react"
 
 const HeaderMinimal = (props) => (
   <SbEditable content={props.blok}>
-    <header className={`ood-interior-page__header ood-interior-page__header--minimal su-bg-fog-light su-py-7`}>
+    <header className={`ood-interior-page__header ood-interior-page__header--minimal su-text-white su-py-7 su-bg-${props.blok.headerBackgroundColor}`}>
       <div className={`centered-container`}>
         <h1 className="ood-interior-page__title su-serif su-text-align-center">{props.blok.title}</h1>
       </div>
