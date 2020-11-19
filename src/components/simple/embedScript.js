19af588f7342e739b4e84a5c661140d57142741d
Shea McKinney <sherakama@gmail.com>
2020-11-18 20:15:29 -0800

    ADAPT-893: Allow dangerous embedscript widget.

--- src/components/simple/embedScript.js
+++ src/components/simple/embedScript.js
@@ -2,6 +2,9 @@
  * Credit where credit is deserved.
  * @see: https://github.com/christo-pr/dangerously-set-html-content
  *
+ * Use this widget with caution. There are no safeguards on what it can do. It
+ * is also not good practice to inject and manipulate the page outside of
+ * REACT as that can lead to irregularities and troubles.
  */
 import React, { useEffect, useRef } from 'react'
 import SbEditable from "storyblok-react"
@@ -9,18 +12,17 @@ import SbEditable from "storyblok-react"
 const EmbedScript = (props) => {
 
   const myEmbed = useRef(null);
-  const { blok: { script:html } , ...rest } = props
+  const { blok: { script:html } } = props
 
   useEffect(() => {
     if (!html) return
 
     // Create a 'tiny' document and parse the html string.
     // https://developer.mozilla.org/en-US/docs/Web/API/DocumentFragment
-    const slotHtml = document.createRange().createContextualFragment(html)
+    const miniDom = document.createRange().createContextualFragment(html)
 
-    console.log(slotHtml)
-
-    const scripts = slotHtml.querySelectorAll('script')
+    // Force the scripts in the embed script field to load sync.
+    const scripts = miniDom.querySelectorAll('script')
     if (scripts.length >= 1) {
       for (let item of scripts) {
         if (item.src && item.src.length > 1) {
@@ -34,18 +36,13 @@ const EmbedScript = (props) => {
     myEmbed.current.innerHTML = ''
 
     // Append the new content.
-    myEmbed.current.appendChild(slotHtml)
-
-    console.log(myEmbed.current)
+    myEmbed.current.appendChild(miniDom)
   }, [html])
 
 
   return (
     <SbEditable content={props.blok}>
-      <div
-        ref={myEmbed}
-        {...rest}
-      />
+      <div ref={myEmbed} />
     </SbEditable>
   )
 }
