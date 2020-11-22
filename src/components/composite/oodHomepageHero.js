6ed60c10956e33cd7a0c4e31d01fd2768f465613
Yvonne Tang <ytang2@stanford.edu>
2020-11-22 11:15:01 -0800

    Break everything in hero and start from mobile

--- src/components/composite/oodHomepageHero.js
+++ src/components/composite/oodHomepageHero.js
@@ -9,27 +9,27 @@ const OodHomepageHero = (props) => {
   processedImg = transformImage(props.blok.image.filename, "/2000x0");
 
   const CtaBoxContent = (props) => (
-    <SbEditable content={props.blok}>
-      <h2 className={`ood-hero-home__cta-headline su-text-white su-semibold`}>{props.blok.ctaHeadline}</h2>
+    <>
+      <h2 className={`hero__cta-headline su-text-white su-semibold`}>{props.blok.ctaHeadline}</h2>
       {props.blok.ctaText &&
-        <p className={`ood-hero-home__cta-text su-link--action su-text-white su-semibold su-ml-auto`}>{props.blok.ctaText}</p>
+        <p className={`hero__cta-text su-link--action su-text-white su-semibold su-ml-auto`}>{props.blok.ctaText}</p>
       }
-    </SbEditable>
+    </>
   );
 
   return (
     <SbEditable content={props.blok}>
-      <div className={`ood-hero-home su-bg-${props.blok.backgroundColor}`}>
-        <figure className="su-media ood-hero-home__media">
-          <div className="su-media__wrapper ood-hero-home__media-wrapper">
-            <img className="ood-hero-home__image"
+      <div className={`hero su-bg-${props.blok.backgroundColor}`}>
+        <figure className="su-media hero__media">
+          <div className="su-media__wrapper hero__media-wrapper">
+            <img className="hero__image"
                  src={processedImg}
                  alt={props.blok.image.alt ? props.blok.image.alt : ""}
             />
           </div>
         </figure>
-        <CenteredContainer flex={true} classes={"ood-hero-home__content"}>
-          <h1 className={`flex-lg-7-of-12 ood-hero-home__splash-text su-semibold su-text-white su-text-focus-in
+        <CenteredContainer flex={true} classes={"hero__content"}>
+          <h1 className={`flex-lg-7-of-12 hero__splash-text su-semibold su-text-white su-text-focus-in
               su-mod-type-${props.blok.splashTextSize}
               su-after-bg-${props.blok.tabColor}`}>
               {props.blok.splashText}
@@ -37,13 +37,13 @@ const OodHomepageHero = (props) => {
           {props.blok.link.linktype === "story" &&
             <Link
               to={props.blok.link.cached_url === "home" ? "/" : `/${props.blok.link.cached_url}${props.blok.link.cached_url.endsWith("/") ? "" : "/"}`}
-              className={`flex-lg-5-of-12 ood-hero-home__link su-bg-${props.blok.ctaBackgroundColor}`}
+              className={`flex-lg-5-of-12 hero__link su-bg-${props.blok.ctaBackgroundColor}`}
             >
               <CtaBoxContent {...props}/>
             </Link>
           }
           {props.blok.link.linktype === "url" &&
-            <a href={props.blok.link.url} className={`flex-lg-5-of-12 ood-hero-home__link su-bg-${props.blok.ctaBackgroundColor}`}>
+            <a href={props.blok.link.url} className={`flex-lg-5-of-12 hero__link su-bg-${props.blok.ctaBackgroundColor}`}>
               <CtaBoxContent {...props}/>
             </a>
           }
