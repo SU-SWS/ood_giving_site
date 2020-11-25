30f30e7615f957c472f549907632f7913794e924
Yvonne Tang <ytang2@stanford.edu>
2020-11-25 09:48:08 -0800

    Use Heading component for link group headings

--- src/components/navigation/megaMenu/oodMegaMenuLinkGroup.js
+++ src/components/navigation/megaMenu/oodMegaMenuLinkGroup.js
@@ -2,12 +2,13 @@ import React from 'react'
 import SbEditable from 'storyblok-react'
 import CreateBloks from "../../../utilities/createBloks"
 import FlexCell from "../../partials/flexCell"
+import Heading from "../../partials/heading"
 
 const OodMegaMenuLinkGroup = (props) => (
   <SbEditable content={props.blok}>
     <FlexCell md={4} classes={"ood-mega-nav__link-group"}>
       {props.blok.heading &&
-        <h3 className={`ood-mega-nav__link-group-heading su-uppercase su-bold`}>{props.blok.heading}</h3>
+        <Heading weight={"bold"} classes={`ood-mega-nav__link-group-heading su-uppercase`}>{props.blok.heading}</Heading>
       }
       {props.blok.links != "" &&
         <ul className="ood-mega-nav__menu-lv2 su-list-none">
