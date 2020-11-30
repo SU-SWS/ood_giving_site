216929d0280a4316d576f5c5c5969890ef58ee1b
Katria Lesser <Katria@stanford.edu>
2020-11-30 13:16:37 -0700

    ADAPT-1233: Added anchor links plugin to make anchor links reliable.

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,6 +19,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-postcss`,
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-anchor-links`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     {
