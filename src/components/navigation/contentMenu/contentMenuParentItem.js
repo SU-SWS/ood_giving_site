889f2a0701b165d65b5abd3c85138786de75ebad
Sherakama <sherakama@gmail.com>
2020-12-03 15:59:31 -0800

    ADAPT-1321: Preserve utm parameters throughout site. (#125)
    * ADAPT-1321: Preserve utm parameters throughout site.
    
    * Use External Classes Param.
    
    * Update src/components/navigation/contentMenu/contentNavItem.js
    
    Co-authored-by: Yvonne Tang <ytang2@stanford.edu>
    
    * Check for vals,
    
    * Update src/components/navigation/contentMenu/contentMenuParentItem.js
    
    Co-authored-by: Yvonne Tang <ytang2@stanford.edu>
    
    Co-authored-by: Yvonne Tang <ytang2@stanford.edu>

--- src/components/navigation/contentMenu/contentMenuParentItem.js
+++ src/components/navigation/contentMenu/contentMenuParentItem.js
@@ -1,20 +1,24 @@
-import React from 'react'
-import Link from 'gatsby-link'
-import SbEditable from 'storyblok-react'
+import React from "react"
+import SbEditable from "storyblok-react"
 import CreateBloks from "../../../utilities/createBloks"
+import SbLink from "../../partials/sbLink"
 
-const ContentMenuParentItem = (props) => (
-  <SbEditable content={props.blok}>
-    <li className="su-secondary-nav__item su-secondary-nav__item--parent ood-content-nav__item ood-content-nav__item--parent">
-      {props.blok.parentItemLink.linktype === "story" &&
-      <Link to={`/${props.blok.parentItemLink.cached_url}/`} activeClassName="ood-content-nav__link--active" className="su-secondary-nav__link ood-content-nav__link">{props.blok.parentItemText}
-      </Link>}
-      {props.blok.parentItemLink.linktype === "url" &&
-      <a href={props.blok.parentItemLink.url} className="su-secondary-nav__link su-link--external ood-content-nav__link">{props.blok.parentItemText}
-      </a>}
-      <CreateBloks blokSection={props.blok.nestedMenu} />
-    </li>
-  </SbEditable>
-)
+const ContentMenuParentItem = props => {
+  return (
+    <SbEditable content={props.blok}>
+      <li className="su-secondary-nav__item su-secondary-nav__item--parent ood-content-nav__item ood-content-nav__item--parent">
+        <SbLink
+          link={props.blok.parentItemLink}
+          activeClass="ood-content-nav__link--active"
+          classes="su-secondary-nav__link ood-content-nav__link"
+          externalClasses="su-link--external"
+        >
+          {props.blok.parentItemText}
+        </SbLink>
+        <CreateBloks blokSection={props.blok.nestedMenu} />
+      </li>
+    </SbEditable>
+  )
+}
 
 export default ContentMenuParentItem
