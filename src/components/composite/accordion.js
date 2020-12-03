f812982909fb367f0ad1134f46782e215fc2c613
Yvonne Tang <ytang2@stanford.edu>
2020-12-03 15:32:42 -0800

    ADAPT-1345: Accordion tweaks and fixes (#124)
    * Allow heading level selection of accordion main heading; font size tweaks; add +/- sign on expand/collapse button as pseudo elements so SR doesn't read it out
    * Fix the overlapping accordion item title with the icon on the right
    * Use Heading component for acc item
    * Set item title back to bold instead of semibold

--- src/components/composite/accordion.js
+++ src/components/composite/accordion.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import SbEditable from 'storyblok-react'
 import Components from "../components"
+import Heading from "../partials/heading"
 
 class Accordion extends React.Component {
 
@@ -50,12 +51,12 @@ class Accordion extends React.Component {
              {...props.blok.id ? {id : props.blok.id} : {}}
         >
           {props.blok.title &&
-            <h2 className={`ood-accordion__heading su-serif`}>{props.blok.title}</h2>
+            <Heading defaultLevel={"h3"} level={props.blok.headingLevel} serif={true} classes={"ood-accordion__heading"}>{props.blok.title}</Heading>
           }
           {props.blok.accordionItems.length > 1 &&
             <>
-            <button className="su-accordion__collapse-all su-button ood-cta__button--secondary" onClick={this.collapseAll}>Collapse all -</button>
-            <button className="su-accordion__expand-all su-button ood-cta__button--secondary" onClick={this.expandAll}>Expand all +</button>
+            <button className="su-accordion__collapse-all su-button ood-cta__button--secondary" onClick={this.collapseAll}>Collapse all</button>
+            <button className="su-accordion__expand-all su-button ood-cta__button--secondary" onClick={this.expandAll}>Expand all</button>
             </>
           }
           <ul className="su-accordion__list ood-accordion__list su-border-color-palo-alto-light">
