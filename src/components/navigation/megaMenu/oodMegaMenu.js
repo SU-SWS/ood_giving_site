96dc6777ab28c4efaf5d8fe464508e60172ef001
Johannes Aßmann <j.assmann@reply.de>
2020-12-04 10:10:52 +0100

    Implement toggling of search overlay

--- src/components/navigation/megaMenu/oodMegaMenu.js
+++ src/components/navigation/megaMenu/oodMegaMenu.js
@@ -1,21 +1,22 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import SbEditable from 'storyblok-react'
 import CreateBloks from "../../../utilities/createBloks"
+import {searchOverlayOpenContext} from '../../../context/searchOverlayStatusProvider'
 
 const OodMegaMenu = (props) => {
+  const {toggleSearchOverlay} = useContext(searchOverlayOpenContext)
   return (
     <SbEditable content={props.blok}>
       <nav className="ood-mega-nav no-js" aria-label="Main Menu">
-        <button className="ood-mega-nav__toggle su-mr-none su-ml-auto" aria-label="menu toggle" aria-expanded="false"><i aria-hidden="true" className={`fas fa-bars`} />Menu</button>
+        <button className="ood-mega-nav__toggle su-mr-none su-ml-auto" aria-label="menu toggle" aria-expanded="false"><i
+          aria-hidden="true" className={`fas fa-bars`}/>Menu
+        </button>
         <ul className="ood-mega-nav__menu-lv1 su-list-none">
-          <CreateBloks blokSection={props.blok.topLevelLinks} />
-          <li className="ood-mega-nav__item" onClick={console.log('test')}>
-            <div className="ood-mega-nav__link">Test</div>
-          </li>
+          <CreateBloks blokSection={props.blok.topLevelLinks}/>
+          <li onClick={toggleSearchOverlay}>Search</li>
         </ul>
       </nav>
     </SbEditable>
   )
 }
-
 export default OodMegaMenu
--- src/components/search/searchOverlay.js
+++ src/components/search/searchOverlay.js
@@ -1,48 +1,40 @@
-import React, { useState } from 'react'
+import React, {useContext} from 'react'
 import './searchOverlay.scss'
-import searchOverlayOpenContext from '../../context/searchOverlayStatusProvider'
-
+import {searchOverlayOpenContext} from '../../context/searchOverlayStatusProvider'
 const SearchOverlay = () => {
-  const [openStatus, setOpenStatus] = useState(true)
-  const toggleOpenStatus = () => {
-    setOpenStatus(!openStatus)
-  }
-  return openStatus ? (
-      <div id="search-overlay">
-        <div class="search-container">
-          <div class="search-header">
-            <span class="search-close-button" onClick={toggleOpenStatus}>
-              Close
-              <span class="search-close-x"></span>
-            </span>
-          </div>
-          <div class="search-body">
-            <input
-              type="input"
-              class="search-field"
-              placeholder="  Search"
-              name="search-field"
-              id="search-field"
-              required
-            />
-            <span class="search-icon">
-              Icon
-            </span>
-          </div>
-          <div class="search-error">
-            It looks like nothing was submitted<br />
-            Please enter Your search term above.
-          </div>
-          <div class="search-footer">
-            <div class="search-footer-headline">Consider browsing by category</div>
-            <div class="search-footer-cols">
-              <div class="search-footer-col">eins</div>
-              <div class="search-footer-col">zwei</div>
-            </div>
-          </div>
+  const {isOpen, toggleSearchOverlay} = useContext(searchOverlayOpenContext)
+  return (
+    <div id="search-overlay" style={{display: `${isOpen ? 'block' : 'none'}`}}>
+      <div className="search-container">
+        <div className="search-header">
+          <span className="search-close-button" onClick={toggleSearchOverlay}>
+            Close
+            <span className="search-close-x"></span>
+          </span>
+        </div>
+        <div className="search-body">
+          <input
+            type="input"
+            className="search-field"
+            placeholder="  Search"
+            name="search-field"
+            id="search-field"
+            required
+          />
+          <span className="search-icon">
+            Icon
+          </span>
+        </div>
+        <div className="search-error">
+          Error message<br />
+          Second Line
+        </div>
+        <div className="search-footer">
+          <div className="search-footer-col">eins</div>
+          <div className="search-footer-col">zwei</div>
         </div>
       </div>
-  ) : null
+    </div>
+  )
 }
-
 export default SearchOverlay
