a2033076204ed98bc1ef320a3b6a5ec0c123ac23
Yvonne Tang <ytang2@stanford.edu>
2020-12-03 22:22:02 -0800

    Add SR only heading for icon card section

--- src/components/partials/iconCardSection.js
+++ src/components/partials/iconCardSection.js
@@ -1,6 +1,7 @@
 import React from "react"
 import SbEditable from "storyblok-react"
 import CreateBloks from "../../utilities/createBloks"
+import Heading from "./heading"
 
 /*
 *
@@ -21,6 +22,7 @@ const IconCardSection = (props) => {
     <SbEditable content={props.blok}>
       {numIconCards > 0 && (
         <div className="ood-icon-card-section su-bg-black-10 su-py-6">
+          <Heading level="h2" classes="su-sr-only-element">{props.blok.iconCardHeading ? props.blok.iconCardHeading : "Links to more information"}</Heading>
           <div className={`centered-container flex-container ood-icon-card-section__container su-align-items-stretch su-flex-${numIconCards}-col`}>
             <CreateBloks blokSection={props.blok.iconCards} />
           </div>
