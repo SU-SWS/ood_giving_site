816404aaa63a5b0e5ed1fd6300a981ca0683aa2f
Yvonne Tang <ytang2@stanford.edu>
2020-12-10 00:10:31 -0800

    use ref instead of querySelector to select menu button

--- src/components/navigation/megaMenu/oodMegaMenu.js
+++ src/components/navigation/megaMenu/oodMegaMenu.js
@@ -9,19 +9,19 @@ import { config } from "../../../utilities/config"
 const OodMegaMenu = (props) => {
   let windowSize = UseWindowSize();
   const [menuOpened, setMenuOpened] = useState(false);
-  const ref = useRef();
+  const ref = useRef(null);
+  const burgerRef = useRef(null);
   const isExpanded = x => x.getAttribute('aria-expanded') === 'true';
 
   const toggleMenu = () => {
     setMenuOpened(!menuOpened);
   }
 
+  // Close menu if escape key is pressed and return focus to the menu button
   UseEscape(() => {
-      const hamburger = document.querySelector('.ood-mega-nav__toggle');
-
-      if (hamburger && isExpanded(hamburger)) {
+      if (burgerRef && isExpanded(burgerRef.current)) {
         setMenuOpened(false)
-        hamburger.focus();
+        burgerRef.current.focus();
       }
     }
   );
@@ -42,9 +42,11 @@ const OodMegaMenu = (props) => {
   return (
     <SbEditable content={props.blok}>
       <nav className="ood-mega-nav" aria-label="Main Menu" ref={ref}>
-        <button className="ood-mega-nav__toggle su-mr-none su-ml-auto" aria-label={menuOpened ? "Close menu" : "Open Menu"}
+        <button className="ood-mega-nav__toggle su-mr-none su-ml-auto" aria-label={menuOpened ? "Close Menu" : "Open Menu"}
                 aria-expanded={menuOpened}
-                onClick={toggleMenu}>
+                onClick={toggleMenu}
+                ref={burgerRef}
+        >
           <i aria-hidden="true" className={`fas fa-${menuOpened ? "times" : "bars"}`}/>{menuOpened ? "Close" : "Menu"}
         </button>
         <ul className="ood-mega-nav__menu-lv1 su-list-none" aria-hidden={!menuOpened}>
