dae55297b19ea9a34c01eb9c3170c3426b510cd5
Yvonne Tang <ytang2@stanford.edu>
2020-12-11 09:05:55 -0800

    Update src/components/navigation/megaMenu/oodMegaMenu.js
    Co-authored-by: Sherakama <sherakama@gmail.com>

--- src/components/navigation/megaMenu/oodMegaMenu.js
+++ src/components/navigation/megaMenu/oodMegaMenu.js
@@ -19,7 +19,7 @@ const OodMegaMenu = (props) => {
 
   // Close menu if escape key is pressed and return focus to the menu button
   UseEscape(() => {
-      if (burgerRef && isExpanded(burgerRef.current)) {
+      if (burgerRef.current && isExpanded(burgerRef.current)) {
         setMenuOpened(false)
         burgerRef.current.focus();
       }
