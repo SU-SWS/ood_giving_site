74f8f86a7207f99460236c7f793124edee8bb989
Yvonne Tang <ytang2@stanford.edu>
2020-12-14 01:30:16 -0800

    Make individual item accordion work

--- src/components/composite/accordion.js
+++ src/components/composite/accordion.js
@@ -6,35 +6,34 @@ const AccContext = React.createContext(null)
 
 const Accordion = (props) => {
   const [allAccOpened, setAllAccOpened] = useState(false);
-
-  const collapseAll = () => setAllAccOpened(false);
   const expandAll = () => setAllAccOpened(true);
+  const collapseAll = () => setAllAccOpened(false);
 
   return (
-    <SbEditable content={props.blok}>
-      <AccContext.Provider value={allAccOpened}>
-        <div className={`su-accordion
-            ${props.blok.spacingTop !== "none" ? `su-pt-${props.blok.spacingTop}` : ""}
-            ${props.blok.spacingBottom !== "none" ? `su-pb-${props.blok.spacingBottom}` : ""}`}
-             {...props.blok.id ? {id : props.blok.id} : {}}
-        >
-          {props.blok.title &&
-            <Heading defaultLevel={"h3"} level={props.blok.headingLevel} serif={true} classes={"ood-accordion__heading"}>{props.blok.title}</Heading>
-          }
-          {props.blok.accordionItems.length > 1 &&
-            <>
-            <button className="su-accordion__collapse-all su-button ood-cta__button--secondary" onClick={collapseAll}>Collapse all</button>
-            <button className="su-accordion__expand-all su-button ood-cta__button--secondary" onClick={expandAll}>Expand all</button>
-            </>
-          }
-          <ul className="su-accordion__list ood-accordion__list su-border-color-palo-alto-light">
-            {props.blok.accordionItems && props.blok.accordionItems.map((blok) =>
-              React.createElement(Components(blok.component), { key: blok._uid, blok: blok})
-            )}
-          </ul>
-        </div>
-      </AccContext.Provider>
-    </SbEditable>
+    <AccContext.Provider value={[allAccOpened, setAllAccOpened]}>
+      <SbEditable content={props.blok}>
+          <div className={`su-accordion
+              ${props.blok.spacingTop !== "none" ? `su-pt-${props.blok.spacingTop}` : ""}
+              ${props.blok.spacingBottom !== "none" ? `su-pb-${props.blok.spacingBottom}` : ""}`}
+               {...props.blok.id ? {id : props.blok.id} : {}}
+          >
+            {props.blok.title &&
+              <Heading defaultLevel={"h3"} level={props.blok.headingLevel} serif={true} classes={"ood-accordion__heading"}>{props.blok.title}</Heading>
+            }
+            {props.blok.accordionItems.length > 1 &&
+              <>
+              <button className="su-accordion__collapse-all su-button ood-cta__button--secondary" onClick={collapseAll}>Collapse all</button>
+              <button className="su-accordion__expand-all su-button ood-cta__button--secondary" onClick={expandAll}>Expand all</button>
+              </>
+            }
+            <ul className="su-accordion__list ood-accordion__list su-border-color-palo-alto-light">
+              {props.blok.accordionItems && props.blok.accordionItems.map((blok) =>
+                React.createElement(Components(blok.component), { key: blok._uid, blok: blok})
+              )}
+            </ul>
+          </div>
+      </SbEditable>
+    </AccContext.Provider>
   )
 }
 
--- src/components/composite/accordionItem.js
+++ src/components/composite/accordionItem.js
@@ -1,15 +1,23 @@
-import React, { useState, useContext } from "react"
+import React, { useState, useContext, useRef } from "react"
 import SbEditable from 'storyblok-react'
 import RichTextField from '../../utilities/richTextField'
 import Heading from "../partials/heading"
 import { AccContext } from "./accordion"
 
 const AccordionItem = (props) => {
-  const allAccOpened = useContext(AccContext);
-  const [accOpened, setAccOpened] = useState(false);
+  const buttonRef = useRef(null);
+  const contentRef = useRef(null);
+  const isExpanded = x => x.getAttribute('aria-expanded') === 'true';
+  const [allAccOpened, setAllAccOpened] = useContext(AccContext);
 
   const toggleAcc = () => {
-    setAccOpened(!accOpened);
+    if (isExpanded(buttonRef.current)) {
+      buttonRef.current.setAttribute('aria-expanded', 'false');
+      contentRef.current.setAttribute('aria-hidden', 'true');
+    } else {
+      buttonRef.current.setAttribute('aria-expanded', 'true');
+      contentRef.current.setAttribute('aria-hidden', 'false');
+    }
   }
 
   return (
@@ -18,9 +26,9 @@ const AccordionItem = (props) => {
         <Heading defaultLevel={"h4"} level={props.blok.headingLevel} serif={true} classes={"su-accordion__title ood-accordion__title"}
                  {...props.blok.id ? {id : props.blok.id} : {}}
         >
-          <button className="su-accordion__button ood-accordion__button" aria-expanded={allAccOpened} onClick={toggleAcc}>{props.blok.title}</button>
+          <button className="su-accordion__button ood-accordion__button" aria-expanded={allAccOpened} onClick={toggleAcc} ref={buttonRef}>{props.blok.title}</button>
         </Heading>
-        <div className="su-accordion__content ood-accordion__content" aria-hidden={!allAccOpened}>
+        <div className="su-accordion__content ood-accordion__content" aria-hidden={!allAccOpened} ref={contentRef}>
           <RichTextField data={props.blok.content}/>
         </div>
       </li>
