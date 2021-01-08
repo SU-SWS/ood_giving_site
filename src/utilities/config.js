9006c350da0ffb76a9b1c0d8462c75d7bb27a78a
Shea McKinney <sherakama@gmail.com>
2021-01-08 11:08:59 -0800

    fixup! env vars.

--- src/utilities/config.js
+++ src/utilities/config.js
@@ -1,7 +1,7 @@
 // Global variables for this project.
 
 const config = {
-  isNetlify: process.env.NETLIFY ?? false,
+  isNetlify: process.env.GATSBY_NETLIFY ?? false,
   basePath: process.env.GATSBY_BASE_PATH === undefined ? '/' : process.env.GATSBY_BASE_PATH,
   breakpoint: {
     "2xs": 0,
