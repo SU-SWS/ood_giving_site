4840502099f9f33342308f651a1c220c846c89b4
Sherakama <sherakama@gmail.com>
2021-01-08 12:34:09 -0800

    ADAPT-1441: Test redirect masks. (#145)

--- src/utilities/richTextField.js
+++ src/utilities/richTextField.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import StoryblokClient from 'storyblok-js-client';
 import sanitizeHtml from 'sanitize-html';
+import { config } from "./config"
 
 // Storyblok is now available as variable which contains the richTextResolver functionality
 const Storyblok = new StoryblokClient({});
 
 function createMarkup(storyblokHTML) {
-  return {
-    __html: sanitizeHtml(Storyblok.richTextResolver.render(storyblokHTML),
-      {
-        allowedTags: false, // This allows extra HTML tags to be rendered, e.g., H2, img
-        allowedAttributes: false, // This allows custom class names
-      }
-    ),
-  };
+
+  // Run sanitizer and renderer.
+  let markup = sanitizeHtml(
+    Storyblok.richTextResolver.render(storyblokHTML),
+    {
+      allowedTags: false, // This allows extra HTML tags to be rendered, e.g., H2, img
+      allowedAttributes: false, // This allows custom class names
+    }
+  )
+
+  // Rewrite the URL to the redirect link to mask the API endpoint.
+  if (config.isNetlify) {
+    markup = markup.replace(/http?(s)\:\/\/a\.storyblok\.com/ig, config.basePath + "cdn/asset")
+    markup = markup.replace(/http?(s)\:\/\/img?[0-9]\.storyblok\.com/ig, config.basePath + "cdn/img")
+  }
+
+  // Return object for setting inner html.
+  return { __html: markup }
 }
 
 const RichTextField = ({ data }) => {
