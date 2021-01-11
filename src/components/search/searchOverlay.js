941f1012b758b13a90bf5a92ff83e15227697aa8
Yannik Wissner <hello@yannik.im>
2021-01-11 14:30:55 +0100

    Allow closing search overlay with escape, fix autofocus not working

--- src/components/search/searchOverlay.js
+++ src/components/search/searchOverlay.js
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useRef, useState } from "react"
 import { SearchOverlayOpenContext } from "../../context/searchOverlayStatusProvider"
 import { navigate } from "gatsby"
 import SearchBox from "./searchBox"
+import UseEscape from "../../hooks/useEscape"
 
 const SearchOverlay = () => {
   const { isOpen, toggleSearchOverlay } = useContext(SearchOverlayOpenContext)
@@ -10,9 +11,9 @@ const SearchOverlay = () => {
   const inputRef = useRef(null)
 
   useEffect(() => {
-    if (isOpen && inputRef) {
+    if (isOpen && inputRef.current) {
       // when the search overlay opens, immediately give focus to the input
-      inputRef.current.focus()
+      setTimeout(() => inputRef.current.focus(), 100)
     }
   }, [isOpen, inputRef])
 
@@ -34,40 +35,44 @@ const SearchOverlay = () => {
     }
   }
 
+  useRef()
+
+  UseEscape(() => isOpen && toggleSearchOverlay())
+
   return (
     <div id="search-overlay" className={isOpen ? "visible" : "hidden"}>
-      {isOpen && (
-        <div className="search-container">
-          <div className="search-header">
-            <span className="search-close-button" onClick={toggleSearchOverlay}>
-              Close
-              <span className="search-close-x"></span>
-            </span>
-          </div>
-          <div className="search-body">
-            <strong className="search-heading">
-              Hello, what can we help you find today?
-            </strong>
-            <SearchBox
-              initialTerm={""}
-              onChange={handleChange}
-              onSubmit={submitTerm}
-              ref={inputRef}
-            />
-          </div>
-          {isEmptyErrorVisible && (
-            <div className="search-error">
-              Error message
-              <br />
-              Second Line
-            </div>
-          )}
-          <div className="search-footer">
-            <div className="search-footer-col">Lorem ipsum</div>
-            <div className="search-footer-col">doluit</div>
-          </div>
+      {/* {isOpen && ( */}
+      <div className="search-container">
+        <div className="search-header">
+          <span className="search-close-button" onClick={toggleSearchOverlay}>
+            Close
+            <span className="search-close-x"></span>
+          </span>
         </div>
-      )}
+        <div className="search-body">
+          <strong className="search-heading">
+            Hello, what can we help you find today?
+          </strong>
+          <SearchBox
+            initialTerm={""}
+            onChange={handleChange}
+            onSubmit={submitTerm}
+            ref={inputRef}
+          />
+        </div>
+        {isEmptyErrorVisible && (
+          <div className="search-error">
+            Error message
+            <br />
+            Second Line
+          </div>
+        )}
+        <div className="search-footer">
+          <div className="search-footer-col">Lorem ipsum</div>
+          <div className="search-footer-col">doluit</div>
+        </div>
+      </div>
+      {/* )} */}
     </div>
   )
 }
