d6136a7ddfb4316aec931f77d071cf578fa7c3fb
Shea McKinney <sherakama@gmail.com>
2021-01-11 05:25:00 -0800

    ADAPT-866: Sitemap.xml review.

--- src/components/partials/htmlHead.js
+++ src/components/partials/htmlHead.js
@@ -120,7 +120,6 @@ const HtmlHead = props => {
         src="https://js.stripe.com/v3"
         type="text/javascript"
       ></script>
-      <meta name="robots" content="noindex" />
       <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link
         href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&family=Source+Serif+Pro:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&display=swap"
