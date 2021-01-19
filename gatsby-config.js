c4222981259952ca4f5fbee9b36d938c9a71949f
Yannik Wissner <hello@yannik.im>
2021-01-19 07:35:04 +0100

    Remove duplicate plugin in gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -72,23 +72,6 @@ module.exports = {
       },
     },
     {
-      resolve: "gatsby-plugin-google-tagmanager",
-      options: {
-        id: "GTM-5RGQ5DD",
-
-        // Include GTM in development.
-        //
-        // Defaults to false meaning GTM will only be loaded in production.
-        includeInDevelopment: false,
-
-        // datalayer to be set before GTM is loaded
-        // should be an object or a function that is executed in the browser
-        //
-        // Defaults to null
-        defaultDataLayer: { platform: "gatsby" },
-      },
-    },
-    {
       resolve: `gatsby-plugin-sitemap`,
       options: {
         exclude: [
