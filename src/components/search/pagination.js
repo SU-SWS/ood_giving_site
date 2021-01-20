e8680727404e001b4dad3979925ee4d5e89832ba
Yannik Wissner <hello@yannik.im>
2021-01-20 12:08:37 +0100

    Update aria properties for pagination, use semantic tags

--- src/components/search/pagination.js
+++ src/components/search/pagination.js
@@ -24,99 +24,110 @@ const Pagination = ({ initialPage }) => {
 
       const isLastPageVisible = nextPages[nextPages.length - 1] < nbPages
 
-      const handlePageChange = newPage => {
+      const handlePageChange = (newPage, $event) => {
+        $event.preventDefault()
         refine(newPage)
         document.querySelector("#search-hits")?.scrollIntoView()
       }
 
       return (
-        <ul className="search-pagination">
-          {isPreviousVisible && (
+        <nav role="navigation" aria-label="Pagination Navigation">
+          <ul className="search-pagination">
+            {isPreviousVisible && (
+              <li
+                className="search-pagination-item search-pagination-item--text"
+                tabIndex={0}
+              >
+                <a
+                  onClick={$event => handlePageChange(currentPage - 1, $event)}
+                  href={`/search-results?page=${currentPage - 1}`}
+                  aria-label={`Go to previous page`}
+                >
+                  Previous
+                </a>
+              </li>
+            )}
+            {isFirstPageVisible && (
+              <>
+                <li
+                  role="button"
+                  className="search-pagination-item search-pagination-item--mobile-hidden"
+                >
+                  <a
+                    onClick={$event => handlePageChange(1, $event)}
+                    href={`/search-results?page=1`}
+                    aria-label={`Go to page 1`}
+                  >
+                    1
+                  </a>
+                </li>
+                <li className="search-pagination-item search-pagination-item--placeholder search-pagination-item--mobile-hidden">
+                  ...
+                </li>
+              </>
+            )}
+            {previousPages.map(page => {
+              return (
+                <li className="search-pagination-item" key={page}>
+                  <a
+                    onClick={$event => handlePageChange(page, $event)}
+                    href={`/search-results?page=${page}`}
+                    aria-label={`Go to page ${page}`}
+                  >
+                    {page}
+                  </a>
+                </li>
+              )
+            })}
             <li
-              className="search-pagination-item search-pagination-item--text"
-              onClick={() => handlePageChange(currentPage - 1)}
-              role="button"
-              title={`Go to previous page`}
-              tabIndex={0}
+              className="search-pagination-item search-pagination-item--current"
+              aria-label={`Page ${currentPage}, current page`}
+              aria-current="true"
             >
-              Previous
+              {currentPage}
             </li>
-          )}
-          {isFirstPageVisible && (
-            <>
-              <li
-                onClick={() => handlePageChange(1)}
-                role="button"
-                title={`Go to page 1`}
-                tabIndex={0}
-                className="search-pagination-item search-pagination-item--mobile-hidden"
-              >
-                1
+            {nextPages.map(page => {
+              return (
+                <li key={page} className="search-pagination-item">
+                  <a
+                    onClick={$event => handlePageChange(page, $event)}
+                    href={`/search-results?page=${page}`}
+                    aria-label={`Go to page ${page}`}
+                  >
+                    {page}
+                  </a>
+                </li>
+              )
+            })}
+            {isLastPageVisible && (
+              <>
+                <li className="search-pagination-item search-pagination-item--placeholder search-pagination-item--mobile-hidden">
+                  ...
+                </li>
+                <li className="search-pagination-item search-pagination-item--mobile-hidden">
+                  <a
+                    onClick={$event => handlePageChange(nbPages, $event)}
+                    href={`/search-results?page=${nbPages}`}
+                    aria-label={`Go to page ${nbPages}`}
+                  >
+                    {nbPages}
+                  </a>
+                </li>
+              </>
+            )}
+            {isNextVisible && (
+              <li className="search-pagination-item search-pagination-item--text">
+                <a
+                  onClick={$event => handlePageChange(currentPage + 1, $event)}
+                  href={`/search-results?page=${currentPage + 1}`}
+                  aria-label={`Go to next page`}
+                >
+                  Next
+                </a>
               </li>
-              <li className="search-pagination-item search-pagination-item--placeholder search-pagination-item--mobile-hidden">
-                ...
-              </li>
-            </>
-          )}
-          {previousPages.map(page => {
-            return (
-              <li
-                className="search-pagination-item"
-                key={page}
-                onClick={() => handlePageChange(page)}
-                role="button"
-                title={`Go to page ${page}`}
-                tabIndex={0}
-              >
-                {page}
-              </li>
-            )
-          })}
-          <li className="search-pagination-item search-pagination-item--current">
-            {currentPage}
-          </li>
-          {nextPages.map(page => {
-            return (
-              <li
-                key={page}
-                onClick={() => handlePageChange(page)}
-                role="button"
-                title={`Go to page ${page}`}
-                tabIndex={0}
-                className="search-pagination-item"
-              >
-                {page}
-              </li>
-            )
-          })}
-          {isLastPageVisible && (
-            <>
-              <li className="search-pagination-item search-pagination-item--placeholder search-pagination-item--mobile-hidden">
-                ...
-              </li>
-              <li
-                onClick={() => handlePageChange(nbPages)}
-                role="button"
-                title={`Go to page ${nbPages}`}
-                tabIndex={0}
-                className="search-pagination-item search-pagination-item--mobile-hidden"
-              >
-                {nbPages}
-              </li>
-            </>
-          )}
-          {isNextVisible && (
-            <li
-              className="search-pagination-item search-pagination-item--text"
-              onClick={() => handlePageChange(currentPage + 1)}
-              role="button"
-              title={`Go to next page`}
-              tabIndex={0}
-            >
-              Next
-            </li>
-          )}
-        </ul>
+            )}
+          </ul>
+        </nav>
       )
     }),
     []
