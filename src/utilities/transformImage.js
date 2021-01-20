40341db515def7d0df64e93133b226b5b0e7eb27
Yvonne Tang <ytang2@stanford.edu>
2021-01-19 19:30:22 -0800

    Change the link masks in richTextField utility; add semicolon at the end for prettierrc

--- src/utilities/transformImage.js
+++ src/utilities/transformImage.js
@@ -2,31 +2,31 @@
 // See all the "param" options on the website
 // https://www.storyblok.com/docs/image-service
 
-import { config } from "./config"
+import { config } from "./config";
 
 const transformImage = (image, param = null) => {
-  let imageService = "https://img2.storyblok.com"
+  let imageService = "https://img2.storyblok.com";
 
   if (config.isNetlify) {
-    imageService = config.assetCdn + "i"
+    imageService = config.assetCdn + "i";
   }
 
   if (image === null) {
-    return ""
+    return "";
   } else {
-    const path = image.replace("https://a.storyblok.com", "")
+    const path = image.replace("https://a.storyblok.com", "");
 
     // If the image is a jpg, optimize it by changing the quality to 60% (quality loss is mostly unnoticeable)
     if (image.endsWith(".jpg")) {
-      param += "/filters:quality(60)"
+      param += "/filters:quality(60)";
     }
 
     if (param === null) {
-      return imageService + path
+      return imageService + path;
     } else {
-      return imageService + param + path
+      return imageService + param + path;
     }
   }
-}
+};
 
-export default transformImage
+export default transformImage;
