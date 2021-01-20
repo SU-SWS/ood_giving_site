e6b93b8bbfe0c9e874e142b3d50b0a20fc047c4a
Yannik Wissner <hello@yannik.im>
2021-01-20 11:13:34 +0100

    Disable body scrolling while search overlay is open

--- src/context/searchOverlayStatusProvider.js
+++ src/context/searchOverlayStatusProvider.js
@@ -1,9 +1,20 @@
-import React, { useState } from "react"
+import React, { useEffect, useRef, useState } from "react"
 import SearchOverlay from "../components/search/searchOverlay"
 
 export const SearchOverlayOpenContext = React.createContext()
 const SearchOverlayOpenContextProvider = props => {
   const [isOpen, setIsOpen] = useState(false)
+  const previousBodyOverflowProperty = useRef("")
+
+  useEffect(() => {
+    if (isOpen) {
+      previousBodyOverflowProperty.current = document.body.style.overflow
+      document.body.style.overflow = "hidden"
+    } else {
+      document.body.style.overflow = previousBodyOverflowProperty.current
+    }
+  }, [isOpen])
+
   return (
     <SearchOverlayOpenContext.Provider
       value={{
