ed0bc99936bc07b154b1c77f02c194bc248d773c
Yvonne Tang <ytang2@stanford.edu>
2021-01-26 14:51:16 -0800

    ADAPT 1669 - improve image filename checking (#161)
    * Use a different check for whether an image filename exist and avoid undefined issues
    
    * Add another imageSize option "large-card" since basic cards sometimes are used by themselves and occupies full width of 8/12 columns
    
    * Also use new image logic in fullWidthImage
    
    * Change image filename check in other components
    
    * Allowing parenthesis in arrow functions to avoid confusion and so it doesn't modify existing code
    
    * Change category name on Areas to Support page per client request
    
    * Missed one for story card

--- src/components/cards/oodTileCard.js
+++ src/components/cards/oodTileCard.js
@@ -1,45 +1,67 @@
-import React from 'react'
-import SbEditable from 'storyblok-react'
-import AspectRatioImage from "../media/aspectRatioImage"
-import SbLink from "../partials/sbLink"
-import Heading from "../partials/heading"
+import React from "react";
+import SbEditable from "storyblok-react";
+import AspectRatioImage from "../media/aspectRatioImage";
+import SbLink from "../partials/sbLink";
+import Heading from "../partials/heading";
 
-const OodTileCard = (props) => {
+const OodTileCard = props => {
   return (
     <SbEditable content={props.blok}>
-      <article className={`ood-tile-card
-        ${(props.blok.image.filename != null && props.blok.showImage === true) ? "ood-tile-card--has-image" : "ood-tile-card--no-image"}
-        ${props.blok.backgroundColor !== "white" ? "su-text-white" : ""}`
-      }>
-        <SbLink link={props.blok.link}
-                classes={`ood-tile-card__link su-bg-${props.blok.backgroundColor} ood-shadow-shallow${props.blok.backgroundColor !== "white" ? "-dark" : ""}`}
-                >
-          {(props.blok.image.filename != null && props.blok.showImage === true) && (
-            <AspectRatioImage
-              {...props}
-              element={"div"}
-              filename={props.blok.image.filename}
-              alt=""
-              classPrefix={"ood-tile-card"}
-              imageSize={"card"}
-              aspectRatio={"3x2"}
-              visibleHorizontal={props.blok.visibleHorizontal}
-              visibleVertical={props.blok.visibleVertical}
-            />
-          )}
+      <article
+        className={`ood-tile-card
+        ${
+          props.blok.image.filename?.startsWith("http") &&
+          props.blok.showImage === true
+            ? "ood-tile-card--has-image"
+            : "ood-tile-card--no-image"
+        }
+        ${props.blok.backgroundColor !== "white" ? "su-text-white" : ""}`}
+      >
+        <SbLink
+          link={props.blok.link}
+          classes={`ood-tile-card__link su-bg-${
+            props.blok.backgroundColor
+          } ood-shadow-shallow${
+            props.blok.backgroundColor !== "white" ? "-dark" : ""
+          }`}
+        >
+          {props.blok.image.filename?.startsWith("http") &&
+            props.blok.showImage === true && (
+              <AspectRatioImage
+                {...props}
+                element={"div"}
+                filename={props.blok.image.filename}
+                alt=""
+                classPrefix={"ood-tile-card"}
+                imageSize={"card"}
+                aspectRatio={"3x2"}
+                visibleHorizontal={props.blok.visibleHorizontal}
+                visibleVertical={props.blok.visibleVertical}
+              />
+            )}
           <div className="ood-tile-card__contents">
             {props.blok.superheadline && (
-              <span className="ood-tile-card__superhead su-uppercase">{props.blok.superheadline}</span>
+              <span className="ood-tile-card__superhead su-uppercase">
+                {props.blok.superheadline}
+              </span>
             )}
             {props.blok.headline && (
-              <Heading level={props.blok.headingLevel} defaultLevel={"h3"} weight={"semibold"}
-                       classes={`ood-tile-card__headline ${props.blok.link.linktype === "url" ? "su-link--external" : ""}`}>{props.blok.headline}</Heading>
+              <Heading
+                level={props.blok.headingLevel}
+                defaultLevel={"h3"}
+                weight={"semibold"}
+                classes={`ood-tile-card__headline ${
+                  props.blok.link.linktype === "url" ? "su-link--external" : ""
+                }`}
+              >
+                {props.blok.headline}
+              </Heading>
             )}
           </div>
         </SbLink>
       </article>
     </SbEditable>
-  )
+  );
 };
 
-export default OodTileCard
+export default OodTileCard;
