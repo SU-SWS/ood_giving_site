8dc19661b9625388f5863440c376e6b79da03310
Yvonne Tang <ytang2@stanford.edu>
2021-01-26 21:41:03 -0800

    Make start time function easier to read

--- src/components/media/embedVideo.js
+++ src/components/media/embedVideo.js
@@ -3,12 +3,17 @@ import SbEditable from "storyblok-react";
 import RichTextField from "../../utilities/richTextField";
 import Heading from "../partials/heading";
 
-const EmbedVideo = props => {
+const EmbedVideo = (props) => {
   let embedUrl = "";
   const videoProvider = props.blok.provider ?? "youtube";
-  const startMin = props.blok.startMinute ? props.blok.startMinute : 0;
-  const startSec = props.blok.startSecond ? props.blok.startSecond : 0;
-  const convertToSecond = (min, sec) => +min * 60 + +sec;
+  const startMin = props.blok.startMinute
+    ? parseInt(props.blok.startMinute)
+    : 0;
+  const startSec = props.blok.startSecond
+    ? parseInt(props.blok.startSecond)
+    : 0;
+
+  const convertToSecond = (min, sec) => min * 60 + sec;
 
   if (videoProvider === "youtube") {
     embedUrl = `https://www.youtube-nocookie.com/embed/${props.blok.videoId}`;
@@ -16,40 +21,43 @@ const EmbedVideo = props => {
     embedUrl = `https://player.vimeo.com/video/${props.blok.videoId}?title=0&byline=0&portrait=0&badge=0`;
   }
 
+  // Youtube only option with a start time
   if (videoProvider === "youtube" && (startMin > 0 || startSec > 0)) {
     embedUrl += `?start=${convertToSecond(startMin, startSec)}`;
   }
 
   return (
     <SbEditable content={props.blok}>
-      {props.blok.heading && (
-        <Heading
-          level={props.blok.headingLevel}
-          serif={true}
-          weight={"bold"}
-          align={"left"}
-          classes={`section__title`}
-        >
-          {props.blok.heading}
-        </Heading>
-      )}
-      <figure className="su-media">
-        <div
-          className={`su-media__wrapper su-embed-container--${props.blok.aspectRatio}`}
-        >
-          <iframe
-            src={embedUrl}
-            frameBorder="0"
-            allow="accelerometer; clipboard-write; encrypted-media; fullscreen; gyroscope; picture-in-picture"
-            allowFullScreen
-          />
-        </div>
-        <figcaption
-          className={`su-media__caption ood-story-media__caption su-text-align-${props.blok.captionAlign}`}
-        >
-          <RichTextField data={props.blok.caption} />
-        </figcaption>
-      </figure>
+      <div className="video-embed">
+        {props.blok.heading && (
+          <Heading
+            level={props.blok.headingLevel}
+            serif={true}
+            weight={"bold"}
+            align={"left"}
+            classes={`section__title`}
+          >
+            {props.blok.heading}
+          </Heading>
+        )}
+        <figure className="su-media">
+          <div
+            className={`su-media__wrapper su-embed-container--${props.blok.aspectRatio}`}
+          >
+            <iframe
+              src={embedUrl}
+              frameBorder="0"
+              allow="accelerometer; clipboard-write; encrypted-media; fullscreen; gyroscope; picture-in-picture"
+              allowFullScreen
+            />
+          </div>
+          <figcaption
+            className={`su-media__caption ood-story-media__caption su-text-align-${props.blok.captionAlign}`}
+          >
+            <RichTextField data={props.blok.caption} />
+          </figcaption>
+        </figure>
+      </div>
     </SbEditable>
   );
 };
