bb74383fcf2affd49d00c9a563eac470d1abaeb9
Yvonne Tang <ytang2@stanford.edu>
2021-01-27 10:36:32 -0800

    Use Heading component and spacing tweaks

--- src/components/search/hits.js
+++ src/components/search/hits.js
@@ -1,17 +1,23 @@
-import React from "react"
-import { connectHits } from "react-instantsearch-dom"
-import Stats from "./stats"
-import SbLink from "../partials/sbLink"
-import Pagination from "./pagination"
+import React from "react";
+import { connectHits } from "react-instantsearch-dom";
+import Stats from "./stats";
+import SbLink from "../partials/sbLink";
+import Pagination from "./pagination";
+import Heading from "../partials/heading";
 
 const Hits = props => {
   const AlgoliaHits = connectHits(({ hits }) => (
     <div className="search-hits">
       {!hits.length && (
         <div className="search-hits-no-hits">
-          <h2 className="search-hits-no-hits-title">
+          <Heading
+            level={2}
+            serif={true}
+            weight={"bold"}
+            classes="search-hits-no-hits-title"
+          >
             {props.blok.noResultsErrorTitle}
-          </h2>
+          </Heading>
           <p className="search-hits-no-hits-text">
             {props.blok.noResultsErrorText}
           </p>
@@ -21,19 +27,19 @@ const Hits = props => {
       {hits.length > 0 && <Stats />}
 
       {hits.map(hit => (
-        <article className="search-hits-item" key={hit.objectID}>
-          <h2>
+        <article className="search-hits-item su-mb-3" key={hit.objectID}>
+          <Heading level={2} serif={true} weight={"bold"}>
             <SbLink link={{ cached_url: `/${hit.slug}` }}>{hit.title}</SbLink>
-          </h2>
+          </Heading>
           <p>{hit.intro || hit.teaser || hit.description}</p>
         </article>
       ))}
 
       <Pagination initialPage={props.initialPage} />
     </div>
-  ))
+  ));
 
-  return <AlgoliaHits />
-}
+  return <AlgoliaHits />;
+};
 
-export default Hits
+export default Hits;
