68366e731160e9b1a73423f652a07371ec9989d8
Yvonne Tang <ytang2@stanford.edu>
2021-02-22 09:05:06 -0800

    Put display prop on the ctagroup instead of in the links

--- src/components/composite/ctaGroup.js
+++ src/components/composite/ctaGroup.js
@@ -1,14 +1,17 @@
 import React from 'react'
-import Components from '../components.js'
 import SbEditable from 'storyblok-react'
 import CreateBloks from "../../utilities/createBloks";
 
-const ctaGroup = (props) => (
-  <SbEditable content={props.blok}>
-    <div className="cta-group">
-      <CreateBloks blokSection={props.blok.ctaLinks} display={props.blok.display} />
-    </div>
-  </SbEditable>
-)
+const ctaGroup = (props) => {
+  const display = props.blok.display === "inline-block" ? "su-flex-row su-flex-wrap su-justify-content-center" : "su-flex-col";
+
+  return (
+    <SbEditable content={props.blok}>
+      <div className={`cta-group su-flex ${display}`}>
+        <CreateBloks blokSection={props.blok.ctaLinks} />
+      </div>
+    </SbEditable>
+  )
+}
 
 export default ctaGroup
--- src/components/simple/ctaLink.js
+++ src/components/simple/ctaLink.js
@@ -2,28 +2,30 @@ import React from "react"
 import SbEditable from "storyblok-react"
 import SbLink from "../partials/sbLink"
 
-const CtaLink = React.forwardRef((props, ref) => (
-  <SbEditable content={props.blok}>
-    {props.blok.linkText && (
-      <div className={`ood-cta ${props.display === 'inline-block' ? 'su-inline-block' : 'su-block'} su-text-align-${props.blok.align}`}>
-        <SbLink
-          ref={ref}
-          link={props.blok.link}
-          classes={
-            props.blok.isButton === true
-              ? `ood-cta__button ${props.blok.linkButtonStyle} ${props.blok.linkButtonSize} ${props.blok.linkIcon}`
-              : `su-link ood-cta__link ${props.blok.linkIcon} ${props.blok.linkTextColor}`
-          }
-          attributes={props.blok.rel ? { rel: props.blok.rel } : {}}
-        >
-          {props.blok.linkText}
-          {props.blok.srText && (
-            <span className="su-sr-only-element">{` ${props.blok.srText}`}</span>
-          )}
-        </SbLink>
-      </div>
-    )}
-  </SbEditable>
-))
+const CtaLink = React.forwardRef((props, ref) => {
+  return (
+    <SbEditable content={props.blok}>
+      {props.blok.linkText && (
+        <div className={`ood-cta su-text-align-${props.blok.align}`}>
+          <SbLink
+            ref={ref}
+            link={props.blok.link}
+            classes={
+              props.blok.isButton === true
+                ? `ood-cta__button ${props.blok.linkButtonStyle} ${props.blok.linkButtonSize} ${props.blok.linkIcon}`
+                : `su-link ood-cta__link ${props.blok.linkIcon} ${props.blok.linkTextColor}`
+            }
+            attributes={props.blok.rel ? { rel: props.blok.rel } : {}}
+          >
+            {props.blok.linkText}
+            {props.blok.srText && (
+              <span className="su-sr-only-element">{` ${props.blok.srText}`}</span>
+            )}
+          </SbLink>
+        </div>
+      )}
+    </SbEditable>
+  )
+})
 
 export default CtaLink
