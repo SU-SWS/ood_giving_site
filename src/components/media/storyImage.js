4618e2bbb1733bab12c5a559e16b391b53ec6287
Yvonne Tang <ytang2@stanford.edu>
2021-03-05 09:45:29 -0800

    Adapt-1958 Story Image as card, optimize image (#171)
    * Add boolean option to display storyImage as a card with background color, padding and shadow
    
    * Add check for original image size and improving image processing for storyImage component
    
    * Update src/components/media/storyImage.js
    
    Co-authored-by: Sherakama <sherakama@gmail.com>
    
    * fixup image width check for su-w-inset to 700
    
    Co-authored-by: Sherakama <sherakama@gmail.com>

--- src/components/media/storyImage.js
+++ src/components/media/storyImage.js
@@ -6,24 +6,39 @@ import RichTextField from '../../utilities/richTextField'
 const StoryImage = (props) => {
   let processedImg = "";
 
-  if (props.blok.image) {
-    if (props.blok.imageWidth === "su-w-full") {
-      processedImg = transformImage(props.blok.image.filename, "/2000x0");
+  if (props.blok.image.filename != null) {
+    let originalWidth = 0;
+
+    // Get image width from URL of storyblok image
+    if (props.blok.image.filename?.startsWith("http")) {
+      originalWidth = props.blok.image.filename.split("/")[5].split("x")[0];
     }
-    else if (props.blok.imageWidth === "centered-container") {
-      processedImg = transformImage(props.blok.image.filename, "/1500x0");
-    }
-    else if (props.blok.imageWidth === "su-w-story") {
-      processedImg = transformImage(props.blok.image.filename, "/1000x0");
-    }
-    else {
-      processedImg = transformImage(props.blok.image.filename, "");
+
+    if (props.blok.image) {
+      if (props.blok.imageWidth === "su-w-full" && originalWidth > 2000) {
+        processedImg = transformImage(props.blok.image.filename, "/2000x0");
+      }
+      else if (props.blok.imageWidth === "centered-container" && originalWidth > 1500) {
+        processedImg = transformImage(props.blok.image.filename, "/1500x0");
+      }
+      else if ((props.blok.imageWidth === "su-w-story" || props.blok.imageWidth === "fit-container") && originalWidth > 1000) {
+        processedImg = transformImage(props.blok.image.filename, "/1000x0");
+      }
+      else if (props.blok.imageWidth === "su-w-inset" && originalWidth > 700) {
+        processedImg = transformImage(props.blok.image.filename, "/700x0");
+      }
+      // If no downsizing is needed, just run it through transformImage to reduce jpg quality to 60%
+      else {
+        processedImg = transformImage(props.blok.image.filename, "");
+      }
     }
   }
 
   return (
     <SbEditable content={props.blok}>
-      <div className={`ood-story-media su-bg-transparent
+      <div className={`ood-story-media
+                     ${(props.blok.isCard ? "ood-shadow-shallow" : "")}
+                     ${(props.blok.backgroundColor? `su-bg-${props.blok.backgroundColor}` : "su-bg-transparent")}
                      ${(props.blok.imageWidth === "su-w-full" || props.blok.imageWidth === "fit-container") ? props.blok.imageWidth : ""}
                      ${props.blok.spacingTop !== "none" ? `su-pt-${props.blok.spacingTop}` : ""}
                      ${props.blok.spacingBottom !== "none" ? `su-pb-${props.blok.spacingBottom}` : ""}
@@ -40,6 +55,7 @@ const StoryImage = (props) => {
             </div>
             {props.blok.caption && (
               <figcaption className={`su-media__caption ood-story-media__caption su-text-align-${props.blok.captionAlign}
+                          ${(props.blok.isCard ? "su-px-default su-pb-2" : "")}
                           ${props.blok.imageWidth === "su-w-full" ? "centered-container" : ""}`}>
                 <RichTextField data={props.blok.caption}/>
               </figcaption>
