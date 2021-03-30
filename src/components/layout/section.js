42ac9e8611a01e879be91d71c2955b9c843ff18e
andrewbaraniuk <andrew.baraniuk@ffwagency.com>
2021-03-30 11:35:51 +0300

    ADAPT-1997: adjusted updates for Centered container for all types of Content width

--- src/components/layout/section.js
+++ src/components/layout/section.js
@@ -18,8 +18,6 @@ const Section = (props) => {
     return titleStyleArray.toString().replace(/,/g, " ");
   };
 
-  const isE2E = (props.blok.contentWidth === "edge-to-edge");
-
   return (
     <SbEditable content={props.blok}>
       <div className={`section su-bg-${props.blok.backgroundColor}
@@ -28,10 +26,10 @@ const Section = (props) => {
            id={props.blok.id}
       >
         {(props.blok.title || props.blok.intro) && (
-          <CenteredContainer flex={true} centered_disabled={isE2E} srOnly={props.blok.srOnlyHeader} classes={"section__header"}>
+          <CenteredContainer flex={true} centered_disabled={props.blok.disableWrapping} srOnly={props.blok.srOnlyHeader} classes={"section__header"}>
             {props.blok.title &&
               <Heading
-                className={`section__title ${isE2E ? 'flex-lg-12-of-12' : 'flex-lg-5-of-12'} su-serif su-bold su-text-align-left
+                className={`section__title flex-lg-12-of-12 su-serif su-bold su-text-align-left
                   ${props.blok.titleSize}
                   su-before-bg-${props.blok.tabColor}
                   ${titleStyleClassList(props.blok.titleStyle)}`}>
@@ -39,7 +37,7 @@ const Section = (props) => {
               </Heading>
             }
             {props.blok.intro &&
-              <FlexCell lg={isE2E ? 12 : 7} classes={"intro-text section__intro su-mr-none"}><RichTextField data={props.blok.intro}/></FlexCell>
+              <FlexCell lg={12} classes={"intro-text section__intro su-mr-none"}><RichTextField data={props.blok.intro}/></FlexCell>
             }
           </CenteredContainer>
         )}
