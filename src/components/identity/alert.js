794875ec0891725d1ba344da8ca1d29bb505d1cf
andrewbaraniuk <andrew.baraniuk@ffwagency.com>
2021-04-09 20:01:36 +0300

    ADAPT-578: improvements

--- src/components/identity/alert.js
+++ src/components/identity/alert.js
@@ -1,16 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import RichTextField from '../../utilities/richTextField'
+import SbLink from '../partials/sbLink';
+import PropTypes from 'prop-types';
 
 const Alert = (props) => {
-  const [closed, setState] = React.useState(false);
+  const [isDismissed, setDismissed] = useState(false);
 
   const color = props.blok.backgroundColor;
   const icon = props.blok.fontAwesomeIcon;
   const label = props.blok.label;
   const text = props.blok.alertBodyText;
-  const link = props.blok.ctaLink[0];
-
-  const dismissSVGPath = 'M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z';
 
   const colorsMapping = {
     'green': 'success',
@@ -19,8 +18,8 @@ const Alert = (props) => {
   };
 
   let classList = 'su-bg-foggy-light';
-  let linkBlack = '';
-  let textBlack = '';
+  let linkBlack = 'su-link-black-false';
+  let textBlack = 'su-text-white';
   let hocus = 'hocus:su-text-white';
 
   let iconClass = 'fas fa-' + icon;
@@ -31,57 +30,52 @@ const Alert = (props) => {
   if (color === 'green' || color === 'red' || color === 'blue') {
     classList = 'su-alert--text-light su-bg-digital-' + color + ' su-text-white su-link-white su-alert--' + colorsMapping[color];
   }
-  if (color === 'yellow') {
+  else if (color === 'yellow') {
     classList = 'su-bg-illuminating-dark su-alert--warning';
     linkBlack = 'su-link-black-true';
     textBlack = 'su-text-black';
     hocus = 'hocus:su-text-black';
   }
-  if (color === 'light-grey') {
+  else if (color === 'light-grey') {
     linkBlack = 'su-link-black-true';
     textBlack = 'su-text-black';
     hocus = 'hocus:su-text-black';
   }
 
-  function alertFooter() {
-    if (link && link.link) {
-      return <div className="su-alert__footer">
-        <a href={link.link.url} className="su-link su-link--action">
-          {link.linkText ? link.linkText : link.link.url}
-        </a>
-      </div>;
-    }
+  const DefaultDismiss = (
+    <button aria-label="Dismiss alert" className={"su-alert__dismiss-button su-button su-group su-leading-display su-bg-transparent hocus:su-bg-transparent su-p-0 su-flex su-items-center su-w-fit su-sans su-font-semibold su-text-17 su-uppercase su-font-bold su-inline-block su-tracking-widest su-mr-0 su-ml-auto " + textBlack + " " + hocus} type="button" onClick={() => { setDismissed(true); }}>
+      Dismiss
+      <i className="fas fa-times-circle"> </i>
+    </button>
+  );
+  const dismissBtn = props.dismissBtn ?? DefaultDismiss;
 
-    return '';
+  // Dismissed State.
+  if (isDismissed === true) {
+    return null;
   }
 
   return (
-    <div className={"su-alert " + classList + (closed ? " closed" : "")}>
-      <div className="centered-container su-cc su-flex su-flex-wrap su-rs-py-1 sm:su-items-center">
-        <div className="su-alert__dismiss su-order-3 su-rs-ml-1 su-mt-15 sm:su-mt-0 su-items-center su-flex-shrink su-text-right su-w-full sm:su-w-auto">
-          <button
-            className={"su-button su-group su-leading-display su-bg-transparent hocus:su-bg-transparent su-p-0 su-flex su-items-center su-w-fit su-sans su-font-semibold su-text-17 su-uppercase su-font-bold su-inline-block su-tracking-widest su-mr-0 su-ml-auto " + textBlack + " " + hocus}
-            type="button"
-            onClick={()=>{setState(!closed)}}
-          >
-            Dismiss
-            <svg width="20" height="20" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" className="su-ml-02em"
-            >
-              <path fillRule="evenodd" d={dismissSVGPath} clipRule="evenodd"> </path>
-            </svg>
-          </button>
-        </div>
+    <div className={"su-alert " + classList}>
+      <div className="centered-container">
+        {props.hasDismiss && (
+          <div className="su-alert__dismiss">{dismissBtn}</div>
+        )}
         <div className="su-alert__header">
           <span className="su-alert__icon">
-            <i className={iconClass}> </i>
+            <i className={iconClass} />
           </span>
           <span className="su-alert__label">{label}</span>
         </div>
-        <div className="su-alert__body su-order-2 su-flex-1 su-flex-grow">
-          <div className={"su-text-normal hover:su-link-no-underline " + linkBlack + " " + textBlack}>
-            <RichTextField data={text}/>
+        <div className="su-alert__body">
+          <div className={"su-alert__text su-text-normal " + linkBlack + " " + textBlack}>
+            <RichTextField data={text} />
           </div>
-          {alertFooter()}
+          {props.blok.cta && props.blok.ctaText &&
+            <div className={"su-alert__footer " + linkBlack + " " + textBlack}>
+              <SbLink link={props.blok.cta} classes={'su-link su-link--action'}>{props.blok.ctaText}</SbLink>
+            </div>
+          }
         </div>
       </div>
     </div>
@@ -89,3 +83,13 @@ const Alert = (props) => {
 };
 
 export default Alert
+
+// Prop Types.
+Alert.propTypes = {
+  hasDismiss: PropTypes.bool
+};
+
+// Default Props.
+Alert.defaultProps = {
+  hasDismiss: true
+};
