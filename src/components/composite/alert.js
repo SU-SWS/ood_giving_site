5ca8130663be8fd8f26a589f885496c1683cc264
Yvonne Tang <ytang2@stanford.edu>
2021-04-13 10:10:54 -0700

    Alert clean up first batch

--- src/components/composite/alert.js
+++ src/components/composite/alert.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import RichTextField from '../../utilities/richTextField'
+import React, { useState } from 'react';
+import RichTextField from '../../utilities/richTextField';
 import SbLink from '../partials/sbLink';
 import PropTypes from 'prop-types';
 
@@ -15,12 +15,10 @@ const Alert = (props) => {
     'green': 'success',
     'red': 'error',
     'blue': 'info',
+    'yellow': 'warning'
   };
 
   let classList = 'su-bg-foggy-light';
-  let linkBlack = 'su-link-black-false';
-  let textBlack = 'su-text-white';
-  let hocus = 'hocus:su-text-white';
 
   let iconClass = 'fas fa-' + icon;
   if (icon === 'bell') {
@@ -28,24 +26,20 @@ const Alert = (props) => {
   }
 
   if (color === 'green' || color === 'red' || color === 'blue') {
-    classList = 'su-alert--text-light su-bg-digital-' + color + ' su-text-white su-link-white su-alert--' + colorsMapping[color];
+    classList = 'su-alert--text-light su-alert--' + colorsMapping[color];
   }
   else if (color === 'yellow') {
-    classList = 'su-bg-illuminating-dark su-alert--warning';
-    linkBlack = 'su-link-black-true';
-    textBlack = 'su-text-black';
-    hocus = 'hocus:su-text-black';
-  }
-  else if (color === 'light-grey') {
-    linkBlack = 'su-link-black-true';
-    textBlack = 'su-text-black';
-    hocus = 'hocus:su-text-black';
+    classList = 'su-alert--' + colorsMapping[color];
   }
 
   const DefaultDismiss = (
-    <button aria-label="Dismiss alert" className={"su-alert__dismiss-button su-button su-group su-leading-display su-bg-transparent hocus:su-bg-transparent su-p-0 su-flex su-items-center su-w-fit su-sans su-font-semibold su-text-17 su-uppercase su-font-bold su-inline-block su-tracking-widest su-mr-0 su-ml-auto " + textBlack + " " + hocus} type="button" onClick={() => { setDismissed(true); }}>
+    <button aria-label="Dismiss alert"
+            className={'su-alert__dismiss-button su-button su-bg-transparent hocus:su-bg-transparent'}
+            type="button"
+            onClick={() => {setDismissed(true);
+    }}>
       Dismiss
-      <i className="fas fa-times-circle"> </i>
+      <i className='fas fa-times-circle' />
     </button>
   );
   const dismissBtn = props.dismissBtn ?? DefaultDismiss;
@@ -56,7 +50,7 @@ const Alert = (props) => {
   }
 
   return (
-    <div className={"su-alert " + classList}>
+    <div className={'su-alert ' + classList}>
       <div className="centered-container">
         {props.hasDismiss && (
           <div className="su-alert__dismiss">{dismissBtn}</div>
@@ -68,27 +62,27 @@ const Alert = (props) => {
           <span className="su-alert__label">{label}</span>
         </div>
         <div className="su-alert__body">
-          <div className={"su-alert__text su-text-normal " + linkBlack + " " + textBlack}>
+          <div className={'su-alert__text su-text-normal'}>
             <RichTextField data={text} />
           </div>
           {props.blok.cta && props.blok.ctaText &&
-            <div className={"su-alert__footer " + linkBlack + " " + textBlack}>
-              <SbLink link={props.blok.cta} classes={'su-link su-link--action'}>{props.blok.ctaText}</SbLink>
-            </div>
+          <div className={'su-alert__footer'}>
+            <SbLink link={props.blok.cta} classes={'su-link su-link--action'}>{props.blok.ctaText}</SbLink>
+          </div>
           }
         </div>
       </div>
     </div>
-  )
+  );
 };
 
-export default Alert
+export default Alert;
 
 // Prop Types.
 Alert.propTypes = {
   hasDismiss: PropTypes.bool,
   dismissBtn: PropTypes.element,
-  blok: PropTypes.object,
+  blok: PropTypes.object
 };
 
 // Default Props.
