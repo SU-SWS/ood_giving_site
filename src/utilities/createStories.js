32ca6a85acae8819d3b07c4f169797698e7b7eaa
Nathan Plowman <nathan.plowman@ffwagency.com>
2021-05-07 09:57:49 -0600

    ADAPT-1837: Added guard code to avoid crashes on page previews with large numbers of components.

--- src/utilities/createStories.js
+++ src/utilities/createStories.js
@@ -6,14 +6,20 @@ import Components from "../components/components"
 
 const CreateStories = (props) => {
   if (props.stories) {
-    return (
-      props.stories.map((story) => React.createElement(Components(story.content.component), {
-        key: story.content._uid,
-        blok: story.content,
-        storyLink: story.full_slug,
-        ...props
-      }))
-    )
+    return props.stories.map((story) => {
+      // In some cases, stories will initially come through as a UUID if there are too many relationships to resolve at once.
+      // This resolves eventually - skip rendering until it does to avoid throwing errors. 
+      if (typeof story != 'string') {
+        return (
+          React.createElement(Components(story.content.component), {
+            key: story.content._uid,
+            blok: story.content,
+            storyLink: story.full_slug,
+            ...props
+          })
+        )
+      }
+    })
   }
 
   // Return null if no content provided.
