a38315478b2dd392f303c120b2c5e070745c8c71
Yvonne Tang <ytang2@stanford.edu>
2021-05-11 15:02:12 -0700

    ADAPT-2145 ADAPT-1127 New banner for interior page (#188)
    * ADAPT-2145: Disable Algolia Indexing.
    
    * ADAPT-2145: Disable Algolia Indexing.
    
    * ADAPT-2145: Disable Algolia Indexing.
    
    * ADAPT-2145: Disable Algolia Indexing.
    
    * ADAPT-2145: initial Has Image Header style updates
    
    * ADAPT-2145: improvements
    
    * ADAPT-2145: added Section menu for mobile
    
    * Header with image styles
    
    * More interior page header with image styles
    
    * title font sizes - use modular typography
    
    * ADAPT-2145: adjustments
    
    * ADAPT-2145: added Full Width image component with basic styling
    
    * ADAPT-2145: updates on FullWidthImage variant
    
    * ADAPT-2145: adjustments
    
    * ADAPT-2145: removed extra classes, changed header background from white to fog-light
    
    * WIP Fullwidth image variant tweaks; remove code not needed
    
    * More fullwidth image variant changes
    
    * logo size/position; header wrapper position
    
    * finetune logo position/size; placeholder (no image) size
    
    * ADAPT-2145: improvements for Full Width variant
    
    * ADAPT-2145: adjustments
    
    * Split large interior-page.scss file into variant files; fix up for interior page body top margin
    
    * Add collapsed content menu back to MD breakpoint; responsive behavior fixup; have logo use bottom position instead so it works for layout with or without content menu
    
    * Title font size; clean up
    
    * Make vertical image focus work; delete duplicate code
    
    * fixup rectangle size/position except mobile
    
    * Has image variant mobile
    
    * title alignment
    
    * Add option to add spacing below header
    
    * codeclimate add comma between rgba values
    
    * make intro/title wrapper spans 10 of 12 for 2XL to make Kerri happy ;)
    
    Co-authored-by: Shea McKinney <sherakama@gmail.com>
    Co-authored-by: andrewbaraniuk <andrew.baraniuk@ffwagency.com>

---
+++ src/components/partials/headerFullWidthImage.js
@@ -0,0 +1,77 @@
+import React from "react"
+import SbEditable from "storyblok-react"
+import RichTextField from "../../utilities/richTextField"
+import CreateBloks from "../../utilities/createBloks"
+import UseWindowSize from "../../hooks/useWindowSize"
+import CenteredContainer from "./centeredContainer"
+import Heading from "./heading"
+import { config } from "../../utilities/config"
+import FullWidthImage from "../media/fullWidthImage";
+import FlexCell from "./flexCell";
+
+/* The Header with Fullwidth Image component is referenced by the Interior Page type. */
+
+const HeaderFullWidthImage = (props) => {
+  let windowSize = UseWindowSize();
+
+  const full_width_image = props.blok.headerImage.filename ? (
+    <FullWidthImage
+      {...props}
+      filename={props.blok.headerImage.filename}
+      classPrefix={"ood-interior-page"}
+      visibleVertical={props.blok.visibleVertical}
+      visibleHorizontal={"center"}
+      alt={props.blok.headerImage.alt ?? ""}
+    />
+  ) : (<div className={'full-width-image-placeholder'} aria-hidden='true' />);
+
+  return (
+    <SbEditable content={props.blok}>
+      <header className={`ood-interior-page__header ood-interior-page__header--full-width-image fullwidth
+              ${props.blok.headerSpacingBottom !== "none" ? `su-mb-${props.blok.headerSpacingBottom}` : ""}`}
+      >
+        {(windowSize.width < config.breakpoint.lg && props.blok.layout !== "no-sidebar") &&
+          <div className='ood-content-nav__wrapper'>
+            <CreateBloks blokSection={props.blok.contentMenu}/>
+          </div>
+        }
+        <div className='ood-interior-page__image-logo-wrapper'>
+          {full_width_image}
+          {props.blok.headerLogo.filename &&
+            <img
+              className={'header-logo'}
+              src={props.blok.headerLogo.filename}
+              alt={props.blok.headerLogo.alt}
+            />
+          }
+        </div>
+        <CenteredContainer
+          flex={true}
+          classes={"ood-interior-page__header-content"}
+        >
+          <FlexCell
+            md={12}
+            lg={10}
+            classes={`ood-interior-page__header-content-wrapper`}
+          >
+            <Heading
+              level={"h1"}
+              weight={"bold"}
+              serif={true}
+              classes={"ood-interior-page__title"}
+            >
+              {props.blok.title}
+            </Heading>
+            {props.blok.intro &&
+              <div className='ood-interior-page__header-intro'>
+                <RichTextField data={props.blok.intro}/>
+              </div>
+            }
+          </FlexCell>
+        </CenteredContainer>
+      </header>
+    </SbEditable>
+  );
+}
+
+export default HeaderFullWidthImage
