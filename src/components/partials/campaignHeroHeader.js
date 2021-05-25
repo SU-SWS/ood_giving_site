b03ab986d41ce0aab95d5104ede9f0afe24d28b5
romakravchuk <roman4ik@gmail.com>
2021-05-25 16:12:22 +0300

    ADAPT-2558 updated structure of campaignHeroHeader.js and styles

--- src/components/partials/campaignHeroHeader.js
+++ src/components/partials/campaignHeroHeader.js
@@ -3,65 +3,70 @@ import SbEditable from "storyblok-react"
 import CreateBloks from "../../utilities/createBloks"
 import Heading from "./heading"
 import FullWidthImage from "../media/fullWidthImage";
-import FlexCell from "./flexCell";
 
 /* The Hero section with fullwidth image is referenced by the Campaign Page type. */
 
 const CampaignHeroHeader = (props) => {
+  const { blok } = props;
 
-  const isFullWidthImage = props.blok.heroStyle === 'fullwidth-image';
+  const isFullWidthImage = blok.heroStyle === 'fullwidth-image';
 
   const campaignContentClasses = `campaign-page__header-content
-  campaign-page__header-${props.blok.heroContentPosition}
-  su-bg-${props.blok.heroBgColor}
-  su-${props.blok.heroContentColor}
-  ${isFullWidthImage ? 'flex-container column-grid centered-container' : ''}
-  ${props.blok.heroContentPosition === 'right' ? 'su-flex-row-reverse' : ''}`
+  campaign-page__header-${blok.heroContentPosition}
+  su-bg-${blok.heroBgColor}
+  su-${blok.heroContentColor}
+  ${isFullWidthImage ? 'flex-container column-grid centered-container su-align-items-center' : ''}
+  ${blok.heroContentPosition === 'right' ? 'su-flex-row-reverse' : ''}`
 
-  const full_width_image = props.blok.image?.filename != null ? (
+  const full_width_image = blok.image?.filename != null ? (
     <FullWidthImage
       {...props}
-      filename={props.blok.image?.filename}
+      filename={blok.image?.filename}
       classPrefix={"campaign-page"}
-      visibleVertical={props.blok.visibleVertical}
+      visibleVertical={blok.visibleVertical}
       visibleHorizontal={"center"}
-      alt={props.blok.image?.alt ?? ""}
+      alt={blok.image?.alt ?? ""}
     />
   ) : (<div className={'full-width-image-placeholder'} aria-hidden='true' />);
 
   return (
-    <SbEditable content={props.blok}>
+    <SbEditable content={blok}>
       <header
-        className={`campaign-page__header campaign-page__header--${props.blok.heroStyle}`}
+        className={`campaign-page__header campaign-page__header--${blok.heroStyle}`}
       >
         <div className='campaign-page__image-wrapper'>
           {full_width_image}
         </div>
         <div className={campaignContentClasses}>
           <div className={`campaign-page__header-content-wrapper ${isFullWidthImage ? 'flex-md-6-of-12 column-grid__column' : ''}`}>
-            {props.blok.logo?.filename &&
+            {blok.logo?.filename &&
               <img
-                className='campaign-page__header-content-logo'
-                src={props.blok.logo?.filename}
-                alt={props.blok.logo?.alt}
+                className={`campaign-page__header-content-logo ${blok.logoAlignment}`}
+                src={blok.logo?.filename}
+                alt={blok.logo?.alt}
               />
             }
-            <Heading
-              level={"h1"}
-              weight={`${isFullWidthImage  ? 'regular' : 'semibold'}`}
-              serif={props.blok.heroStyle === 'fullwidth-image'}
-              classes={"campaign-page__title"}
-            >
-              {props.blok.title}
-            </Heading>
-            {props.blok.intro &&
-              <p className='campaign-page__header-intro su-mb-none'>
-                {props.blok.intro}
-              </p>
-            }
-            {props.blok.heroCta &&
-              <CreateBloks blokSection={props.blok.heroCta} />
-            }
+            <div className={`campaign-page__header-content-body ${blok.heroContentAlignment}`}>
+              <Heading
+                level={"h1"}
+                weight={`${isFullWidthImage  ? 'regular' : 'semibold'}`}
+                serif={blok.heroStyle === 'fullwidth-image'}
+                classes={`campaign-page__title ${blok.heroTitleType}`}
+              >
+                {blok.title}
+              </Heading>
+              {blok.bar &&
+                <div className={`campaign-page__header-bar su-bg-${blok.barBgColor} ${blok.barAlignment}`} />
+              }
+              {blok.intro &&
+                <p className='campaign-page__header-intro su-mb-none'>
+                  {blok.intro}
+                </p>
+              }
+              {blok.heroCta &&
+                <CreateBloks blokSection={blok.heroCta} />
+              }
+            </div>
           </div>
         </div>
       </header>
