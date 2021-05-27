0706ad2623d19364e84bfab6a9b1f34d537f6f9e
Elena <elena.cracea@gmail.com>
2021-05-27 11:31:12 -0600

    ADAPT-2091: PR comments fix

--- src/components/composite/oodQuoteSlider.js
+++ src/components/composite/oodQuoteSlider.js
@@ -27,15 +27,18 @@ const OodQuoteSlider = ({blok}) => {
 
   return (
     <SbEditable content={blok}>
-      <div className="ood-quote-slider centered-container">
-        <h2>{blok.title}</h2>
+      <div className="ood-quote-slider su-flex su-flex--column su-pt-2">
+        {blok.title && <h2>{blok.title}</h2>  }
         <Slider className={"ood-quote-slider__wrapper"} {...modalSliderSettings}>
           {blok.quotes && blok.quotes.map((story) => React.createElement(Components(story.content.component), {
             key: story.content._uid,
             blok: story.content,
           }))}
         </Slider>
-        <div className={"ood-quote-slider__current-position su-mod-type-1"}>{activeSlide + 1} /  {blok.quotes.length}</div>
+        <div className={"ood-quote-slider__current-position su-mod-type-1"}>
+          {activeSlide + 1} / {blok.quotes.length}
+          <span className="sr-only">{`Slide ${activeSlide + 1} of ${blok.quotes.length}`}</span>
+        </div>
       </div>
     </SbEditable>
   )
