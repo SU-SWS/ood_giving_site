21bc4e81ee43abab3f40d9ed3bf0dac8851a050b
romakravchuk <roman4ik@gmail.com>
2021-06-03 16:36:18 +0300

    ADAPT-2558 move lockup and home icon to header in oodCampaignPage

--- src/components/page-types/oodCampaignPage.js
+++ src/components/page-types/oodCampaignPage.js
@@ -5,12 +5,36 @@ import IconCardSection from '../partials/iconCardSection'
 import SeoSocial from "../partials/seoSocial"
 import CreateBloks from "../../utilities/createBloks"
 import CampaignHeroHeader from '../partials/campaignHeroHeader';
+import SbLink from "../partials/sbLink";
 
 const OodCampaignPage = (props) => {
   return (
     <SbEditable content={props.blok}>
-      <SeoSocial {...props}/>
-      <CreateBloks blokSection={props.blok.alertPicker} />
+      <header className={`campaign-page__header-lockup ${props.blok.headerColor}`}>
+        <a href="#main-content" className="su-skiplinks">
+          Skip to main content
+        </a>
+
+        <div className="flex-container centered-container su-align-items-baseline su-justify-content">
+          <CreateBloks blokSection={props.blok.lockup} />
+
+          <SbLink link={props.blok.homeLink} classes={`campaign-page__header-icon ${props.blok.headerColor}`}>
+            <svg xmlns="http://www.w3.org/2000/svg" className="home-icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+              />
+            </svg>
+
+            Home
+          </SbLink>
+        </div>
+
+        <SeoSocial {...props}/>
+        <CreateBloks blokSection={props.blok.alertPicker} />
+      </header>
       <main id="main-content"
             className={`ood-campaign-page ood-campaign-page--${props.blok.headerStyle}`}
       >
--- src/components/partials/campaignHeroHeader.js
+++ src/components/partials/campaignHeroHeader.js
@@ -3,7 +3,6 @@ import SbEditable from "storyblok-react"
 import CreateBloks from "../../utilities/createBloks"
 import Heading from "./heading"
 import FullWidthImage from "../media/fullWidthImage";
-import SbLink from "./sbLink";
 
 /* The Hero section with fullwidth image is referenced by the Campaign Page type. */
 
@@ -35,24 +34,6 @@ const CampaignHeroHeader = (props) => {
       <header
         className={`campaign-page__header campaign-page__header--${blok.heroStyle}`}
       >
-        <div className={`campaign-page__header-lockup ${blok.headerColor}`}>
-          <div className="flex-container centered-container su-align-items-baseline su-justify-content">
-            <CreateBloks blokSection={blok.lockup} />
-
-            <SbLink link={blok.homeLink} classes={`campaign-page__header-icon ${blok.headerColor}`}>
-              <svg xmlns="http://www.w3.org/2000/svg" className="home-icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-                />
-              </svg>
-
-              Home
-            </SbLink>
-          </div>
-        </div>
         <div className='campaign-page__image-wrapper'>
           {full_width_image}
         </div>
