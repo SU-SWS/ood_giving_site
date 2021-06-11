e19d74aeb689b8a9933bda9209df356937af81d7
Sherakama <sheamck@stanford.edu>
2021-06-10 17:12:47 -0700

    ADAPT-2558: Hero Component (#199)
    * Initial file set up for campaign page
    
    * campaign page and partials, campaign card components; empty scss files
    
    * Tweaks and clean ups
    
    * ADAPT-2558 created campaignHeroHeader.js component, added styles for left image and fullwidth layout
    
    * ADAPT-2558 updated structure of campaignHeroHeader.js and styles
    
    * ADAPT-2558 add hero header, update styles
    
    * ADAPT-2558 add home link, add brick color
    
    * ADAPT-2558 updated ood-text-color, cta link, fullwidthimage props
    
    * ADAPT-2558 remove unused components for campaign hero
    
    * ADAPT-2558 fix codeclimate issues
    
    * ADAPT-2558 more fixes for codeclimate
    
    * ADAPT-2558 add overlay for header, update column with on md
    
    * ADAPT-2558 remove unused prop in sblink
    
    * ADAPT-2558 update classes for hero content position
    
    * ADAPT-2558 fix few stylelint issues
    
    * ADAPT-2558 update paddings for left image layout
    
    * ADAPT-2558 update breakpoints for image left layout
    
    * ADAPT-2558 move lockup and home icon to header in oodCampaignPage
    
    * ADAPT-2558 move header to component
    
    * ADAPT-2558 create campaignHeader, rename components and classes for consistancy
    
    * ADAPT-2558 update styles for hero content and header
    
    * ADAPT-2558 update structure of campaign header
    
    * fixup! case matters on some OS.
    
    * ADAPT-2558 remove unused campaign-card.scss file
    
    * Move skiplink out of campaign-page__header-inner so when tabbing from last Alert link to skiplink doesn't cause a header content shift
    
    * ADAPT-2558 add id generator to campaign header
    
    * ADAPT-2558 add backface-visibility:hidden to gradient in header
    
    * ADAPT-2558 update max-width for content wrapper
    
    * ADAPT-2558 update px to rem
    
    * fixup! package-lock.
    
    Co-authored-by: Yvonne Tang <ytang2@stanford.edu>
    Co-authored-by: romakravchuk <roman4ik@gmail.com>

--- src/components/components.js
+++ src/components/components.js
@@ -1,5 +1,6 @@
 import Page from "./page";
 import RichTextField from "../utilities/richTextField";
+import Redirect from "./redirect/Redirect";
 import ComponentNotFound from "./component_not_found";
 // Site Identity Elements
 import GlobalFooter from "./identity/globalFooter";
@@ -9,9 +10,10 @@ import OodLocalFooter from "./identity/oodLocalFooter";
 import LocalFooterPicker from "./identity/localFooterPicker";
 import LocalHeaderPicker from "./identity/localHeaderPicker";
 import Lockup from "./identity/lockup";
+import OodCampaignHeader from "./identity/oodCampaignHeader";
 // Cards
 import BasicCard from "./cards/basicCard";
-import Redirect from "./redirect/Redirect";
+import OodCampaignCard from "./cards/oodCampaignCard";
 import OodIconCard from "./cards/oodIconCard";
 import OodSupportCard from "./cards/oodSupportCard";
 import OodQuoteCard from "./cards/oodQuoteCard";
@@ -47,6 +49,7 @@ import Section from "./layout/section";
 import SingleColumnContent from "./layout/singleColumnContent";
 // Page Type Components
 import OodInteriorPage from "./page-types/oodInteriorPage";
+import OodCampaignPage from "./page-types/oodCampaignPage";
 import OodLandingPage from "./page-types/oodLandingPage";
 import OodStory from "./page-types/story/oodStory";
 import OodSupportPage from "./page-types/oodSupportPage";
@@ -62,7 +65,7 @@ import Accordion from "./composite/accordion";
 import AccordionItem from "./composite/accordionItem";
 import CtaGroup from "./composite/ctaGroup";
 import SearchResults from "./search/searchResults";
-import oodGallerySlideshow from './composite/oodGallerySlideshow';
+import oodGallerySlideshow from "./composite/oodGallerySlideshow";
 
 const ComponentList = {
   page: Page,
@@ -105,10 +108,13 @@ const ComponentList = {
   contentNestedMenu: ContentNestedMenu,
   basicCard: BasicCard,
   redirect: Redirect,
+  oodCampaignCard: OodCampaignCard,
   oodTileCard: OodTileCard,
   oodQuoteCard: OodQuoteCard,
   oodQuoteSlider: OodQuoteSlider,
   oodHomepageHero: OodHomepageHero,
+  oodCampaignHeader: OodCampaignHeader,
+  oodCampaignPage: OodCampaignPage,
   oodInteriorPage: OodInteriorPage,
   oodLandingPage: OodLandingPage,
   oodStory: OodStory,
