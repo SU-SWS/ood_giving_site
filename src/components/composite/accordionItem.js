659a3ab55bce65b6b617ddafa559ac1df9c462cf
rebeccahongsf <rebeccahongsf@gmail.com>
2021-06-24 13:01:37 -0700

    ADAPT-2893 Add Digital Red and Sans Serif Accordion option

--- src/components/composite/accordionItem.js
+++ src/components/composite/accordionItem.js
@@ -1,37 +1,36 @@
-import React from 'react'
-import SbEditable from 'storyblok-react'
-import RichTextField from '../../utilities/richTextField'
-import Heading from "../partials/heading"
+import React from "react";
+import SbEditable from "storyblok-react";
+import RichTextField from "../../utilities/richTextField";
+import Heading from "../partials/heading";
 
 class AccordionItem extends React.Component {
-
   // This is required.
   constructor(props) {
     super(props);
     this.state = {
-      expanded: true
+      expanded: true,
     };
   }
 
   // On render, collapse all the items.
   componentDidMount() {
-    this.setState({expanded: false});
+    this.setState({ expanded: false });
   }
 
   // Expand/Collapse toggle.
   toggle = () => {
     this.state.expanded ? this.collapse() : this.expand();
-  }
+  };
 
   // Open the doors!
   expand = () => {
     this.setState({ expanded: true });
-  }
+  };
 
   // Close the hatches!
   collapse = () => {
     this.setState({ expanded: false });
-  }
+  };
 
   // RENDER!
   render() {
@@ -39,19 +38,37 @@ class AccordionItem extends React.Component {
 
     return (
       <SbEditable content={props.blok}>
-        <li className="su-accordion__item ood-accordion__item su-border-color-palo-alto-light">
-          <Heading level={props.blok.headingLevel} defaultLevel={"h4"} serif={true} classes="su-accordion__title ood-accordion__title"
-                   {...props.blok.id ? {id : props.blok.id} : {}}
+        <li
+          className={
+            "su-accordion__item ood-accordion__item " +
+            props.accordionBorderColor
+          }
+        >
+          <Heading
+            level={props.blok.headingLevel}
+            defaultLevel={"h4"}
+            serif={props.accordionFont}
+            classes="su-accordion__title ood-accordion__title"
+            {...(props.blok.id ? { id: props.blok.id } : {})}
           >
-            <button className="su-accordion__button ood-accordion__button" aria-expanded={this.state.expanded} onClick={this.toggle}>{props.blok.title}</button>
+            <button
+              className={"su-accordion__button " + props.accordionAfterColor}
+              aria-expanded={this.state.expanded}
+              onClick={this.toggle}
+            >
+              {props.blok.title}
+            </button>
           </Heading>
-          <div className="su-accordion__content ood-accordion__content" aria-hidden={!this.state.expanded}>
-            <RichTextField data={props.blok.content}/>
+          <div
+            className="su-accordion__content ood-accordion__content"
+            aria-hidden={!this.state.expanded}
+          >
+            <RichTextField data={props.blok.content} />
           </div>
         </li>
       </SbEditable>
-    )
+    );
   }
 }
 
-export default AccordionItem
+export default AccordionItem;
