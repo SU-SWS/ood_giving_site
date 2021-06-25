8ed5b3a668a72a2aa71c9dd085a10e903da48219
Elena Cracea <elena.cracea@gmail.com>
2021-06-25 17:06:20 -0600

    ADAPT-2895: Content block position and tabbed title styles (#211)
    * DAPT-2895: Content block position and tabbed title styles
    
    * Use existing decorators for WYSIWYG headline styles
    
    Co-authored-by: Yvonne Tang <ytang2@stanford.edu>

--- src/components/layout/rowTwoColumns.js
+++ src/components/layout/rowTwoColumns.js
@@ -5,7 +5,8 @@ import CreateBloks from "../../utilities/createBloks"
 const RowTwoColumns = (props) => (
   <SbEditable content={props.blok}>
     <div className={`row--2-columns su-mb-${props.blok.spacingBottom}`}>
-      <div className={`flex-container su-mx-auto su-align-items-${props.blok.contentAlignment} ${props.blok.rowWidth}`}>
+
+      <div className={`flex-container su-align-items-${props.blok.contentAlignment} ${props.blok.rowWidth} ${props.blok.align}`}>
         <div className={`${props.blok.oneColumnMd === true ? "flex-lg": "flex-md"}${!props.blok.widthRatio ? "-6-of-12" : ""}${props.blok.widthRatio === "1-to-2" ? "-4-of-12" : ""}${props.blok.widthRatio === "2-to-1" ? "-8-of-12" : ""}`}>
           <CreateBloks blokSection={props.blok.columnOneContent} />
         </div>
