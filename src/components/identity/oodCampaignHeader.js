275c6bf8ecd28264861243716ad0ea1aa83fef28
matt <mattanglin@gmail.com>
2021-08-20 15:02:58 -0700

    ADAPT-3201 | @mattanglin | campaign header logo (#223)
    * Optionally render logo image in campaign header when set
    
    * Refactor custom logo image out of lockup and into cmapaign header
    
    * Cleanup
    
    Co-authored-by: Sherakama <sheamck@stanford.edu>

--- src/components/identity/oodCampaignHeader.js
+++ src/components/identity/oodCampaignHeader.js
@@ -1,13 +1,26 @@
 import React from "react";
 import SbEditable from "storyblok-react";
 import CreateBloks from "../../utilities/createBloks";
+import FullWidthImage from "../media/fullWidthImage";
 import SbLink from "../partials/sbLink";
 
 const OodCampaignHeader = (props) => {
   return (
     <SbEditable content={props.blok}>
       <div className={`campaign-page__header-inner ${props.blok.headerColor}`}>
-        <div className="flex-container centered-container su-align-items-baseline su-justify-content">
+        <div className="flex-container centered-container su-align-items-center su-justify-content">
+          {props.blok?.logoImage?.filename ? (
+            <div className="logo-image">
+              <SbLink link={props.blok.logoLink}>
+                <FullWidthImage
+                  filename={props.blok.logoImage.filename}
+                  blok={props.blok}
+                />
+              </SbLink>
+            </div>
+          ) : (
+            <CreateBloks blokSection={props.blok.lockup} />
+          )}
           <CreateBloks blokSection={props.blok.lockup} />
           {!props.blok.hideHomeLink && (
             <SbLink link={props.blok.homeLink} classes={`campaign-page__header-icon ${props.blok.headerColor}`}>
