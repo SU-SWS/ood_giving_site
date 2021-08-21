c5593d70bf698291ac8fa46ff3da80eeecbd3242
Matt Anglin <mattanglin@gmail.com>
2021-08-20 17:33:07 -0700

    Removes duplicate lockup blok after merge conflict

--- src/components/identity/oodCampaignHeader.js
+++ src/components/identity/oodCampaignHeader.js
@@ -21,7 +21,6 @@ const OodCampaignHeader = (props) => {
           ) : (
             <CreateBloks blokSection={props.blok.lockup} />
           )}
-          <CreateBloks blokSection={props.blok.lockup} />
           {!props.blok.hideHomeLink && (
             <SbLink link={props.blok.homeLink} classes={`campaign-page__header-icon ${props.blok.headerColor}`}>
               <svg xmlns="http://www.w3.org/2000/svg" className="home-icon" fill="none" viewBox="0 0 24 24"
