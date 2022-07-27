c2db4efd710b3f9e12ae7d13540c975457c7db91
wmui51 <wmui51@users.noreply.github.com>
2022-07-27 08:42:12 -0700

    add the search input and functionality

--- src/components/endowed-positions/EndowedPositionsSearch.js
+++ src/components/endowed-positions/EndowedPositionsSearch.js
@@ -1,3 +1,33 @@
-const EndowedPositionsSearch = () => <div>test component</div>;
+import React, { useCallback, useState } from 'react';
+import { navigate } from 'gatsby';
+
+const EndowedPositionsSearch = () => {
+  const [searchTerm, setSearchTerm] = useState;
+
+  const handleInputChange = useCallback((event) => {
+    setSearchTerm(event.currentTarget.value);
+  }, []);
+  const handleSearch = useCallback(() => {
+    navigate(`/endowed-positions/search?term=${searchTerm}`);
+  }, [searchTerm]);
+  const handleKeyDown = useCallback((event) => {
+    if (event.key === 'Enter') {
+      handleSearch();
+    }
+  });
+
+  return (
+    <>
+      <input
+        // className="endowed-positions__input"
+        // id="search-input"
+        onChange={handleInputChange}
+        onKeyDown={handleKeyDown}
+        value={getSearchTerm}
+      />
+      <button type="button" onClick={handleSearch}>go</button>
+    </>
+  );
+}
 
 export default EndowedPositionsSearch;
