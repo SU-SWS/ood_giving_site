73098c157de588ad412cbaa86198dc6dcc056876
wmui51 <wmui51@users.noreply.github.com>
2023-11-02 09:24:43 -0700

    NoJira | @wmui51 | Fix/countdown updates for timezone offset (#385)
    Adjust countdown time to be 11:59:59 of 11/28
    Adjust styling for when 4 CountdownPies are shown

--- src/components/giving-tuesday/Countdown.js
+++ src/components/giving-tuesday/Countdown.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 
 import CountdownPie from './CountdownPie';
 import UseCountdown from '../../hooks/useCountdown';
@@ -7,7 +8,7 @@ const Countdown = () => {
   /**
    * manually set date to 11/28/2023 w/ a UTC offset (no daylight savings)
    */
-  const targetDate = new Date(Date.UTC(2023, 10, 28, 8));
+  const targetDate = new Date(Date.UTC(2023, 10, 29, 7, 59, 59));
   const [days, hours, minutes, seconds] = UseCountdown(targetDate);
 
   // render nothing for now when countdown is over
@@ -16,43 +17,41 @@ const Countdown = () => {
   }
 
   return (
-    <div aria-atomic="true" className="countdown-wrapper" role="timer">
+    <div
+      aria-atomic="true"
+      className={classNames('countdown-wrapper', { ['has-days']: days > 0 })}
+      role="timer"
+    >
       {days > 0 && (
-        <div className="countdown-pie-row">
-          <div className="countdown-spacer" />
-          <CountdownPie
-            className="pie-days"
-            descriptor="days"
-            percent={(days / 29) * 100}
-          >
-            {days}
-          </CountdownPie>
-          <div className="countdown-spacer" />
-        </div>
+        <CountdownPie
+          className="pie-days"
+          descriptor="days"
+          percent={(days / 29) * 100}
+        >
+          {days}
+        </CountdownPie>
       )}
-      <div className="countdown-pie-row">
-        <CountdownPie
-          className="pie-hours"
-          descriptor="hours"
-          percent={(hours / 24) * 100}
-        >
-          {hours}
-        </CountdownPie>
-        <CountdownPie
-          className="pie-minutes"
-          descriptor="minutes"
-          percent={(minutes / 60) * 100}
-        >
-          {minutes}
-        </CountdownPie>
-        <CountdownPie
-          className="pie-seconds"
-          descriptor="seconds"
-          percent={(seconds / 60) * 100}
-        >
-          {seconds}
-        </CountdownPie>
-      </div>
+      <CountdownPie
+        className="pie-hours"
+        descriptor="hours"
+        percent={(hours / 24) * 100}
+      >
+        {hours}
+      </CountdownPie>
+      <CountdownPie
+        className="pie-minutes"
+        descriptor="minutes"
+        percent={(minutes / 60) * 100}
+      >
+        {minutes}
+      </CountdownPie>
+      <CountdownPie
+        className="pie-seconds"
+        descriptor="seconds"
+        percent={(seconds / 60) * 100}
+      >
+        {seconds}
+      </CountdownPie>
     </div>
   );
 };
