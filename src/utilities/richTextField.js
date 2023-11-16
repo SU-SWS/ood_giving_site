102a1287c736106b32dad8dfc5ee006489d07359
Sait Furkan Teke <35101659+stfurkan@users.noreply.github.com>
2023-11-16 13:57:44 -0800

    remove sanitizeHtml options

--- src/utilities/richTextField.js
+++ src/utilities/richTextField.js
@@ -8,11 +8,7 @@ const Storyblok = new StoryblokClient({});
 
 function createMarkup(storyblokHTML) {
   // Run sanitizer and renderer.
-  let markup = sanitizeHtml(Storyblok.richTextResolver.render(storyblokHTML), {
-    // allowedTags: false, // This allows extra HTML tags to be rendered, e.g., H2, img
-    // allowedAttributes: false, // This allows custom class names
-    // allowVulnerableTags: true, // https://github.com/apostrophecms/sanitize-html/blob/main/CHANGELOG.md#1250
-  });
+  let markup = sanitizeHtml(Storyblok.richTextResolver.render(storyblokHTML));
 
   // Rewrite the URL to the redirect link to mask the API endpoint.
   if (config.isNetlify) {
