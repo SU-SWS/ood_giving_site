92cd3b5409d56c8bff20eddb5459f9e92347de3d
wmui51 <wmui51@users.noreply.github.com>
2024-02-14 12:54:34 -0800

    NoJira | @wmui51 | Countdown updates and fixes (#397)

--- src/components/giving-tuesday/Countdown.js
+++ src/components/giving-tuesday/Countdown.js
@@ -1,19 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import classNames from 'classnames';
 
 import CountdownPie from './CountdownPie';
 import UseCountdown from '../../hooks/useCountdown';
 
-const Countdown = () => {
-  /**
-   * manually set date to 11/28/2023 w/ a UTC offset (no daylight savings)
-   */
-  const targetDate = new Date(Date.UTC(2023, 10, 29, 7, 59, 59));
-  const [days, hours, minutes, seconds] = UseCountdown(targetDate);
+const getDescriptorString = (descriptor, time) => {
+  return time !== 1 ? `${descriptor}s` : descriptor;
+};
 
-  // render nothing for now when countdown is over
+const Countdown = () => {
+  const [countdownDate, setCountdownDate] = useState(null);
+  const [days, hours, minutes, seconds] = UseCountdown(countdownDate) || [];
+
+  useEffect(() => {
+    if (!countdownDate) {
+      /**
+       * manually set date to 04/04/2024 7AM w/ a UTC offset (no daylight savings)
+       */
+      setCountdownDate(new Date(Date.UTC(2024, 3, 4, 14, 0, 0)));
+    }
+  }, [countdownDate, setCountdownDate]);
+
   if (days + hours + minutes + seconds <= 0) {
-    return null;
+    return (
+      <div className="countdown-wrapper has-days">
+        <CountdownPie className="pie-days" descriptor="days" percent={0}>
+          0
+        </CountdownPie>
+        <CountdownPie clasName="pie-hours" descriptor="hours" percent={0}>
+          0
+        </CountdownPie>
+        <CountdownPie clasName="pie-minutes" descriptor="minutes" percent={0}>
+          0
+        </CountdownPie>
+        <CountdownPie className="pie-seconds" descriptor="seconds" percent={0}>
+          0
+        </CountdownPie>
+      </div>
+    );
   }
 
   return (
@@ -25,7 +49,7 @@ const Countdown = () => {
       {days > 0 && (
         <CountdownPie
           className="pie-days"
-          descriptor="days"
+          descriptor={getDescriptorString('day', days)}
           percent={(days / 29) * 100}
         >
           {days}
@@ -33,21 +57,21 @@ const Countdown = () => {
       )}
       <CountdownPie
         className="pie-hours"
-        descriptor="hours"
+        descriptor={getDescriptorString('hour', hours)}
         percent={(hours / 24) * 100}
       >
         {hours}
       </CountdownPie>
       <CountdownPie
         className="pie-minutes"
-        descriptor="minutes"
+        descriptor={getDescriptorString('minute', minutes)}
         percent={(minutes / 60) * 100}
       >
         {minutes}
       </CountdownPie>
       <CountdownPie
         className="pie-seconds"
-        descriptor="seconds"
+        descriptor={getDescriptorString('second', seconds)}
         percent={(seconds / 60) * 100}
       >
         {seconds}
