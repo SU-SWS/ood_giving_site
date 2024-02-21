764c1c867893f5b6488e34c68e639898b0bbce58
wmui51 <wmui51@users.noreply.github.com>
2024-02-20 23:09:22 -0800

    update endowed positions dataset (#399)
    * update endowed positions dataset
    
    * special characters fix

--- src/components/endowed-positions/EndowedPositionsFooter.js
+++ src/components/endowed-positions/EndowedPositionsFooter.js
@@ -6,7 +6,7 @@ const EndowedPositionsFooter = () => (
       <p>
         This site is a comprehensive directory of endowed positions at Stanford.
         It is updated six times per year, following meetings of Stanford&apos;s
-        Board of Trustees. Last updated January 10, 2024. For questions or
+        Board of Trustees. Last updated February 22, 2024. For questions or
         corrections please email{' '}
         <a href="mailto:donorrelations@stanford.edu">
           donorrelations@stanford.edu
