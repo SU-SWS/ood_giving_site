4381aa5ccf4f6706b76524fc0ce28e9db7760a73
wmui51 <wmui51@users.noreply.github.com>
2024-03-06 20:36:08 -0800

    add default state and storyblok configurability

--- src/components/giving-tuesday/Countdown.js
+++ src/components/giving-tuesday/Countdown.js
@@ -5,42 +5,44 @@ import SbEditable from 'storyblok-react';
 import CountdownPie from './CountdownPie';
 import UseCountdown from '../../hooks/useCountdown';
 
-const NO_DAYS_DEFAULT_HOURS_RANGE_MAX = 36;
-
 const getDescriptorString = (descriptor, time) => {
   return time !== 1 ? `${descriptor}s` : descriptor;
 };
 
 const convertDaysToHours = (days) => days ? days * 24 : 0;
 
-const getHoursMaxRange = (hours) => 
-  !hours ||  hours < NO_DAYS_DEFAULT_HOURS_RANGE_MAX ? NO_DAYS_DEFAULT_HOURS_RANGE_MAX : hours;
-
-// will also need a prop for the hours ring to know what we count down to
 const Countdown = ({blok}) => {
-  const {date, hasDays} = blok;
+  const {date, hasDays, hourPieRange} = blok;
   const [countdownDate, setCountdownDate] = useState(null);
   const [days, hours, minutes, seconds] = UseCountdown(countdownDate) || [];
   const displayHours = hasDays ? hours : convertDaysToHours(days) + hours;
-  const hourPieRange = hasDays ? 24 : getHoursMaxRange(hours); 
+  const displayHourPieRange = hasDays ? 24 : hourPieRange; 
 
   useEffect(() => {
     if (!countdownDate) {
-      const dateArray = date.split(' ');
-      /**
-       * manually set date to 04/04/2024 7AM w/ a UTC offset (no daylight savings)
-       */
-      // setCountdownDate(new Date(Date.UTC(2024, 3, 4, 14, 0, 0)));
-      setCountdownDate(new Date(`${dateArray[0]}T${dateArray[1]}`));
+      if (!date) {
+        /**
+         * manually set date to 04/04/2024 7AM w/ a UTC offset (no daylight savings)
+         * this should only stay for the current campaign, can remove this and make
+         * component purely storyblok configurable after campaign
+         */
+        setCountdownDate(new Date(Date.UTC(2024, 3, 4, 14, 0, 0)));
+      } else {
+        /**
+         * the date prop returns in the following format: "2023-11-21 23:56"
+         * we have convert it to be usable for the js Date object
+         */
+        const dateArray = date.split(' ');
+
+        setCountdownDate(new Date(`${dateArray[0]}T${dateArray[1]}`));
+      }
     }
   }, [countdownDate, setCountdownDate]);
-// console.log('displayHours', displayHours);
-// console.log('hasDays', hasDays);
-console.log('date', date);
+
   return (
     <SbEditable content={blok}>
       {days + hours + minutes + seconds <= 0 ? (
-        <div className="countdown-wrapper has-days">
+        <div className={classNames('countdown-wrapper', { ['has-days']: days > 0 })}>
           {hasDays && (
             <CountdownPie className="pie-days" descriptor="days" percent={0}>
               0
@@ -74,7 +76,7 @@ console.log('date', date);
           <CountdownPie
             className="pie-hours"
             descriptor={getDescriptorString('hour', displayHours)}
-            percent={(displayHours / hourPieRange) * 100}
+            percent={(displayHours / displayHourPieRange) * 100}
           >
             {displayHours}
           </CountdownPie>
