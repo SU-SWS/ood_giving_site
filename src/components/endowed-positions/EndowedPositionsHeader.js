0b08bd28065a63980f775cb9b70aa038c78d8489
wmui51 <wmui51@users.noreply.github.com>
2024-03-06 20:37:37 -0800

    remove test code

--- src/components/endowed-positions/EndowedPositionsHeader.js
+++ src/components/endowed-positions/EndowedPositionsHeader.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from 'gatsby';
 
 import EndowedPositionsNav from './EndowedPositionsNav';
-import Countdown from '../giving-tuesday/Countdown';
 
 const EndowedPositionsHeader = ({ to }) => (
   <>
@@ -30,7 +29,6 @@ const EndowedPositionsHeader = ({ to }) => (
                     </span>
                   </p>
                 </div>
-                <Countdown hasDays={false} />
                 <EndowedPositionsNav to={to} />
               </div>
             </div>
