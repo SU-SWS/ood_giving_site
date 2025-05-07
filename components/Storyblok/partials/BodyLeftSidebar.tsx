c36eb538f5fc5e34fa51e35541baa27ffb901069
Eric Bakenhus <1059679+ericbakenhus@users.noreply.github.com>
2025-05-07 10:59:43 -0500

    DS-1394: client all the things

--- components/Storyblok/partials/BodyLeftSidebar.tsx
+++ components/Storyblok/partials/BodyLeftSidebar.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React from 'react';
 import { type SbBlokData, storyblokEditable } from '@storyblok/react';
 import { useWindowSize } from 'usehooks-ts';
--- components/Storyblok/partials/HeaderNoImage.tsx
+++ components/Storyblok/partials/HeaderNoImage.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React from 'react';
 import { type ISbRichtext, type SbBlokData, storyblokEditable } from '@storyblok/react';
 import { RichTextField } from '@/components/RichTextField';
