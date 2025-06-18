5f1ca36e29b90b02665cc1f01eebbca8cdd78d3b
Yvonne Tang <ytang2@stanford.edu>
2025-06-18 14:59:38 -0700

    separate out search button styles; more margin to local footer heading

--- components/Search/Modal/OpenSearchModalButton.tsx
+++ components/Search/Modal/OpenSearchModalButton.tsx
@@ -2,6 +2,7 @@
 import React, { useContext } from 'react';
 import SearchModalContext from './SearchModalContext';
 import { HeroIcon } from '@/components/HeroIcon';
+import * as styles from '../Search.styles';
 
 export const OpenSearchModalButton = React.forwardRef<HTMLButtonElement, { id: string }>(({ id }, ref) => {
   const { open } = useContext(SearchModalContext);
@@ -10,19 +11,19 @@ export const OpenSearchModalButton = React.forwardRef<HTMLButtonElement, { id: s
     <button
       data-test="search--nav-bar"
       type="button"
-      className="relative ml-auto shrink-0 xl:-top-6 h-40 w-40 xl:w-fit text-digital-red hocus:text-cardinal-red rounded-full xl:text-20 xl:px-20 border border-black-30 hocus:bg-black-10 transition-colors"
+      className={styles.searchButton}
       aria-label="Search Giving"
       onClick={open}
       id={id}
       ref={ref}
     >
-      <span className="sr-only xl:not-sr-only leading-none xl:mr-6">
+      <span className={styles.searchButtonText}>
         Search
       </span>
       <HeroIcon
         icon="search"
         aria-hidden="true"
-        className="inline-block relative -top-1 w-22 lg:w-20 h-18"
+        className={styles.searchButtonIcon}
       />
     </button>
   );
