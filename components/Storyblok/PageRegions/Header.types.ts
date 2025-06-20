c760dc8f9840d9c81279c9601efe5cbe6aaab73a
Yvonne Tang <ytang2@stanford.edu>
2025-06-19 17:48:39 -0700

    Header Minimal

--- components/Storyblok/PageRegions/Header.types.ts
+++ components/Storyblok/PageRegions/Header.types.ts
@@ -2,19 +2,20 @@ import { type StoryblokRichtext } from 'storyblok-rich-text-react-renderer';
 import { type SbBlokData } from '@storyblok/react/rsc';
 import { type VisibleVerticalType } from '@/components/Image';
 import { type SbImageType } from '../Storyblok.types';
+import { type DarkBgColorsType, type MarginType } from '@/utilities/datasource';
 
 /**
  * Common page header props
  */
 export type HeaderProps = React.HTMLAttributes<HTMLDivElement> & {
   blok: SbBlokData & {
-    contentMenu: SbBlokData[];
-    title?: string;
+    contentMenu?: SbBlokData[];
+    title: string;
     intro?: StoryblokRichtext;
-    headerSpacingBottom?: string;
-    headerBackgroundColor?: string;
+    headerSpacingBottom?: MarginType;
+    headerBackgroundColor?: DarkBgColorsType;
     headerLogo?: SbImageType;
-    headerImage: SbImageType;
+    headerImage?: SbImageType;
     visibleVertical?: VisibleVerticalType;
   };
 };
---
+++ components/Storyblok/PageRegions/HeaderMinimal.tsx
@@ -0,0 +1,29 @@
+import { cnb } from 'cnbuilder';
+import { Container } from '@/components/Container';
+import { Heading } from '@/components/Typography';
+import { type HeaderProps } from '@/components/Storyblok/PageRegions';
+import { darkBgColors } from '@/utilities/datasource';
+
+type HeaderMinimalProps = Partial<HeaderProps>;
+
+export const HeaderMinimal = ({ blok }: HeaderMinimalProps) => {
+  const { title, headerBackgroundColor: bgColor } = blok;
+
+  return (
+    <Container
+      className={cnb(
+        'py-38 md:py-72 lg:py-108 xl:py-[11.1rem] break-words',
+        darkBgColors[bgColor || 'palo-alto'],
+      )}
+    >
+      <Heading
+        as="h1"
+        id="page-title"
+        className={cnb('mb-0 mx-auto fluid-type-5 max-w-1200 text-balance', bgColor === 'fog-light' ? 'text-black' : 'text-white')}
+        align="center"
+      >
+        {title}
+      </Heading>
+    </Container>
+  );
+};
