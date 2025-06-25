35c63112ede103aa885d302ba1f495c4fb089c7e
Yvonne Tang <ytang2@stanford.edu>
2025-06-25 00:53:27 -0700

    fix specificity conflict with FA sr-only class which hides Search text in button for xl and up

--- components/Search/Search.styles.ts
+++ components/Search/Search.styles.ts
@@ -2,5 +2,5 @@
  * Search button
  */
 export const searchButton = 'relative ml-auto shrink-0 xl:-top-6 h-40 w-40 xl:w-fit text-digital-red hocus:text-cardinal-red rounded-full xl:text-20 xl:px-20 border border-black-30 hocus:bg-black-10 transition-colors';
-export const searchButtonText = 'sr-only xl:not-sr-only leading-none xl:mr-6';
+export const searchButtonText = 'max-xl:sr-only leading-none xl:mr-6';
 export const searchButtonIcon = 'inline-block relative -top-1 w-22 lg:w-20 h-18';
