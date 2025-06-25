bbe75f4076c5415f3b6a6c9d45c66a60a36e2d8c
Yvonne Tang <ytang2@stanford.edu>
2025-06-25 00:34:59 -0700

    Handle icon and icon style logic from Storyblok

--- components/FAIcon/FAIcon.tsx
+++ components/FAIcon/FAIcon.tsx
@@ -1,19 +1,27 @@
 import { library, type IconName } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon, type FontAwesomeIconProps } from '@fortawesome/react-fontawesome';
 
+/**
+ * We use FontAwesome icons in Icon Cards, Support Card and other components.
+ * See setup instructions and usage:
+ * https://docs.fontawesome.com/web/use-with/react/add-icons
+ * https://docs.fontawesome.com/web/use-with/react/style
+ */
 export type FAIconProps = Omit<FontAwesomeIconProps, 'icon'> & {
   icon: IconName;
   // For this project we only use the free outline "far" or solid "fas" icons
   iconStyle?: 'far' | 'fas';
-  // Optional title for accessibility. Add title if icon is not decorative only.
-  title?: string;
 }
 
 export const FAIcon = ({
   icon,
   iconStyle,
+  /**
+   * Optional svg title for accessibility. Add title if icon is not decorative.
+   * Icon is aria-hidden unless a title is provided.
+   */
   title,
   ...props
 }: FAIconProps) => {
@@ -22,7 +30,7 @@ export const FAIcon = ({
   }
 
   // Add both free solid and regular FA icons to the library so we can use any of them
-  library.add(fas, far);
+  library.add(far, fas);
 
   const faStyle = iconStyle || 'far';
 
