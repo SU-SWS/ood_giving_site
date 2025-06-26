8fc88e2e993b49d7f2f1d029dee3f6dde7a0e881
Yvonne Tang <ytang2@stanford.edu>
2025-06-26 15:25:32 -0700

    Remove text color styles from cut and paste in WYSIWYG

--- components/RichText/RichText.tsx
+++ components/RichText/RichText.tsx
@@ -6,6 +6,7 @@ import {
   MARK_STYLED,
   MARK_LINK,
   MARK_STRIKE,
+  MARK_TEXT_STYLE,
   NODE_HEADING,
   NODE_PARAGRAPH,
   NODE_IMAGE,
@@ -83,6 +84,7 @@ export const RichText = ({
       [MARK_BOLD]: (children) => <strong>{children}</strong>,
       [MARK_ITALIC]: (children) => <em>{children}</em>,
       [MARK_STRIKE]: (children) => <del>{children}</del>,
+      [MARK_TEXT_STYLE]: (children, { color }) => <>{children}</>,
       [MARK_LINK]: (children, props) => {
         const {
           href,
