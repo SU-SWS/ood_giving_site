c7793dbac87137a1d8e8d46e9b5634f73fb4c689
Eric Bakenhus <1059679+ericbakenhus@users.noreply.github.com>
2025-07-07 14:38:03 -0500

    DS-1409, DS-1496, DS-1497 | @ericbakenhus | Search modal (#523)
    * DS-1409: Add search modal
    
    * DS-1409: Clean up
    
    * DS-1409: Clean up
    
    * DS-1409: Add clear, close modal on route change, and add scroll on overflow-y
    
    * DS-1409: Clean up
    
    * DS-1409: Fix reset button

--- components/Search/Modal/SearchModalContext.tsx
+++ components/Search/Modal/SearchModalContext.tsx
@@ -1,75 +1,62 @@
 'use client';
 
-import React, { createContext, useState, useRef } from 'react';
-import { useEscape } from '@/hooks/useEscape';
-import { useDisplay } from '@/hooks/useDisplay';
+import React, {
+  createContext,
+  useState,
+  useEffect,
+} from 'react';
+import { type SearchConfig } from '@/utilities/data/getSearchConfigBlok';
+import { usePathname } from 'next/navigation';
 
 type SearchModalContextProps = {
   isOpen: boolean;
   open: () => void;
   close: () => void;
-  desktopButtonRef: React.RefObject<HTMLButtonElement>;
-  mobileButtonRef: React.RefObject<HTMLButtonElement>;
-  modalSearchInputRef: React.RefObject<HTMLInputElement>;
-  searchInputRef: React.RefObject<HTMLInputElement>;
+  searchConfig: SearchConfig;
 };
 
 /**
  * A context to manage the state of the search modal.
  */
-const SearchModalContext = createContext<SearchModalContextProps>({
+export const SearchModalContext = createContext<SearchModalContextProps>({
   isOpen: false,
   open: () => null,
   close: () => null,
-  desktopButtonRef: null,
-  mobileButtonRef: null,
-  modalSearchInputRef: null,
-  searchInputRef: null,
+  searchConfig: null,
 });
 export const SearchModalContextProvider = SearchModalContext.Provider;
-export default SearchModalContext;
+
+type SearchModalProviderProps = {
+  children: React.ReactElement;
+  searchConfig: SearchConfig;
+};
 
 /**
  * A provider to manage the state of the search modal.
- * @param {Object} props
- * @param {React.ReactNode} props.children
  */
-export function SearchModalProvider({ children }: { children: React.ReactElement }) {
-  const { showDesktop, showMobile } = useDisplay();
+export function SearchModalProvider({
+  children,
+  searchConfig,
+}: SearchModalProviderProps) {
+  const pathname = usePathname();
   const [isOpen, setIsOpen] = useState(false);
-  const desktopButtonRef = useRef<HTMLButtonElement>(null);
-  const mobileButtonRef = useRef<HTMLButtonElement>(null);
-  const modalSearchInputRef = useRef<HTMLInputElement>(null);
-  const searchInputRef = useRef<HTMLInputElement>(null);
 
   // Close handler.
   const close = () => {
     setIsOpen(false);
-    if (showDesktop) desktopButtonRef.current.focus();
-    if (showMobile) mobileButtonRef.current.focus();
   };
 
   // Open handler.
   const open = () => {
-    // Don't open the modal if the user is already on the search page.
-    // Instead focus on the search input.
-    if (
-      window
-      && window.location
-      && window.location.pathname.startsWith('/search')
-    ) {
-      searchInputRef.current.focus();
-      searchInputRef.current.scrollIntoView();
-      return;
-    }
-
     setIsOpen(true);
   };
 
-  // Close the modal when the escape key is pressed.
-  useEscape(() => {
-    if (isOpen) close();
-  });
+  // Close the modal on route change
+  useEffect(() => {
+    if (pathname) {
+      setIsOpen(false);
+    }
+  }, [pathname]);
 
   // Provider wrapper.
   return (
@@ -78,10 +65,7 @@ export function SearchModalProvider({ children }: { children: React.ReactElement
         isOpen,
         open,
         close,
-        desktopButtonRef,
-        mobileButtonRef,
-        modalSearchInputRef,
-        searchInputRef,
+        searchConfig,
       }}
     >
       {children}
