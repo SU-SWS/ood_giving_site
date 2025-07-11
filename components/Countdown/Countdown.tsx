b3771f54972896de62890399b5aa9de5fdc520cd
Eric Bakenhus <1059679+ericbakenhus@users.noreply.github.com>
2025-07-11 12:05:06 -0500

    DS-1453: WIP add countdown

--- components/Countdown/Countdown.tsx
+++ components/Countdown/Countdown.tsx
@@ -1,13 +1,19 @@
 'use client';
-import { useMemo, useState } from 'react';
-import { parse, interval, intervalToDuration } from 'date-fns';
+import { useEffect, useMemo, useState } from 'react';
+import {
+  parseJSON,
+  differenceInSeconds,
+  secondsToMinutes,
+  secondsToHours,
+} from 'date-fns';
+import { TZDate, tz } from '@date-fns/tz';
 
 type CountdownProps = {
   date?: string;
   dayPieRange?: number;
   hourPieRange?: number;
   hasDays?: boolean;
-  isDST?: boolean;
+  // isDST?: boolean;
 };
 
 export const Countdown = ({
@@ -15,19 +21,35 @@ export const Countdown = ({
   dayPieRange,
   hourPieRange,
   hasDays = false,
-  isDST = false,
+  // isDST = false,
 }: CountdownProps) => {
-  const [now, setNow] = useState(new Date());
-  const targetDate = useMemo(() => parse(date, 'yyyy-MM-dd T', new Date()), [date]);
-  const {
-    days,
-    hours,
-    minutes,
-    seconds,
-  } = useMemo(() => intervalToDuration(interval(now, targetDate)), [now, targetDate]);
+  const [nowInPT, setnowInPT] = useState(TZDate.tz('America/Los_Angeles'));
+  const targetDate = useMemo(() => new TZDate(parseJSON(date), 'America/Los_Angeles'), [date]);
+  const diffInSeconds = useMemo(() => differenceInSeconds(targetDate, nowInPT), [targetDate, nowInPT]);
+  const seconds = useMemo(() => diffInSeconds % 60, [diffInSeconds]);
+  const minutes = useMemo(() => secondsToMinutes(diffInSeconds) % 60, [diffInSeconds]);
+  const hours = useMemo(() => (
+    hasDays
+      ? secondsToHours(diffInSeconds)
+      : secondsToHours(diffInSeconds) % 24
+    ), [hasDays, diffInSeconds]);
+  const days = useMemo(() => (
+    hasDays
+      ? secondsToHours(diffInSeconds) / 24
+      : undefined
+  ), [hasDays, diffInSeconds]);
+
+  // Reset now every second
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setnowInPT(TZDate.tz('America/Los_Angeles'));
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
 
   return (
-    <div aria-atomic>
+    <div aria-atomic role="timer">
 
     </div>
   );
