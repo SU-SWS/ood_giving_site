7b03a30f80c1f274548e1bb6749441360c8dc593
Eric Bakenhus <1059679+ericbakenhus@users.noreply.github.com>
2025-07-11 12:05:37 -0500

    DS-1453: WIP add countdown

--- components/Countdown/Countdown.styles.ts
+++ components/Countdown/Countdown.styles.ts
@@ -1,5 +1,7 @@
 import { cnb } from 'cnbuilder';
 
 export const CountdownPie = cnb(
-  '',
+  'w-180',
+  'before:bg-[radial-gradient(farthest-side,#8C1515_98%,#000),_]',
+  'after:'
 );
--- components/Countdown/CountdownPie.tsx
+++ components/Countdown/CountdownPie.tsx
@@ -1,5 +1,16 @@
 import { useWindowSize } from 'usehooks-ts';
 
-export const CountdownPie = () => {
+export const CountdownPie = ({ children, descriptor, percent }) => {
   const { width } = useWindowSize();
+
+  return (
+    <div className="flex items-center justify-center" style={{
+      background: `conic-gradient(hotpink calc(${percent} * 1%), transparent 0)`,
+    }}>
+      <div className="bg-white p-8">
+        <span>{children}</span>
+        <span>{descriptor}</span>
+      </div>
+    </div>
+  )
 };
