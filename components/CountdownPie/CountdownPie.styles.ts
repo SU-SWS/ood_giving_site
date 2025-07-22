1fc579cca5288bb439c55fb402b1540d61c9986b
Eric Bakenhus <1059679+ericbakenhus@users.noreply.github.com>
2025-07-22 18:26:20 -0500

    DS-1447: Adjust styles

--- components/CountdownPie/CountdownPie.styles.ts
+++ components/CountdownPie/CountdownPie.styles.ts
@@ -12,11 +12,11 @@ export const countdownContent = ({ shrink = false }: { shrink?: boolean } = {}) 
 export const countdownNumber = ({ larger = false, font }: { larger?: boolean, font: 'sans' | 'serif' }) => cnb(
   'font-bold text-center',
   {
-    'text-[clamp(1.6rem,_6vi_-_.1543rem,_6rem)] md:text-[6rem]': !larger,
+    'text-[clamp(1.6rem,_6vw_-_.1543rem,_6rem)] md:text-[6rem]': !larger,
     'text-[3.6rem] lg:text-[4rem] xl:text-[5rem] 2xl:text-[6rem]': larger,
     'font-sans': font === 'sans',
     'font-serif': font === 'serif',
   },
 );
-export const countdownDescriptionCenter = 'uppercase text-[1rem] sm:text-14 md:text-18 text-center';
+export const countdownDescriptionCenter = 'uppercase text-[1rem] xs:text-14 md:text-18 text-center';
 export const countdownDescriptionBottom = 'font-semibold fluid-type-0 mt-17 text-center';
