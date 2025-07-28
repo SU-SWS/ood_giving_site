bf4e4184495994a59474f0f5daef914497ddf45c
Yvonne Tang <42749717+yvonnetangsu@users.noreply.github.com>
2025-07-28 15:33:20 -0700

    DS-1415 DS-1439 | Support Page, Support Cards (#557)
    * Support card
    
    * Support page card filtering
    
    * Change URL hash based on filter clicked and initialize filter from URL hash
    
    * Add animation
    
    * Clean up
    
    * just in case icon from icon selector returns undefined
    
    * Move GTAG outside of body tag just so it's same as nextjs doc
    
    * Wrap filter buttons in li
    
    * Remove aria-label of mega menu parent items; remove role="region" for aria-live container
    
    * Remove unnecessary role="group" on CtaGroup

---
+++ components/SupportCard/SupportCard.tsx
@@ -0,0 +1,53 @@
+import { FAIcon, type FAIconProps } from '@/components/FAIcon';
+import { FlexBox } from '@/components/FlexBox';
+import { SbLink } from '@/components/Storyblok/partials/SbLink';
+import { SimpleCard, type SimpleCardProps } from '@/components/SimpleCard';
+import { Text } from '@/components/Typography';
+import { type SbLinkType } from '@/components/Storyblok/Storyblok.types';
+import { type AreasToSupportType } from '../Storyblok/SbSupportPage';
+import * as styles from './SupportCard.styles';
+
+type SupportCardProps = SimpleCardProps & FAIconProps & {
+  taxonomy?: AreasToSupportType[];
+  headline?: string;
+  link?: SbLinkType;
+};
+
+export const SupportCard = ({
+  taxonomy,
+  headline,
+  link,
+  icon,
+  iconStyle,
+  bgColor = 'lagunita',
+  ...props
+}: SupportCardProps) => {
+  const isDarkCardBg = bgColor !== 'white';
+
+  return (
+    <SimpleCard
+      {...props}
+      hasLink
+      bgColor={bgColor}
+      className={styles.root}
+    >
+      <FlexBox direction="col" alignItems="start" justifyContent="between" className={styles.wrapper}>
+        <SbLink link={link} classes={styles.link(bgColor)}>
+          <Text
+            as="span"
+            size="f2"
+            leading="display"
+            weight="semibold"
+            color={bgColor === 'white' ? 'black' : 'white'}
+            icon={link?.linktype !== 'story' ? 'external' : undefined}
+            iconProps={{ className: styles.linkIcon, noBaseStyle: true }}
+            className={styles.linkText}
+          >
+            {headline}
+          </Text>
+        </SbLink>
+        <FAIcon icon={icon} iconStyle={iconStyle} size="3x" className={styles.icon(isDarkCardBg)} />
+      </FlexBox>
+    </SimpleCard>
+  );
+};
