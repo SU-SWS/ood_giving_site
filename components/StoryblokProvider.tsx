663b592e6e10f02fa66a30e1399c88d69bfb9232
Eric Bakenhus <1059679+ericbakenhus@users.noreply.github.com>
2025-08-19 13:39:12 -0500

    DS-1816: Test

--- components/StoryblokProvider.tsx
+++ components/StoryblokProvider.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { initStoryblokClient } from '@/utilities/storyblok';
+// import { initStoryblokClient } from '@/utilities/storyblok';
 
 type ProviderProps = {
   children: React.ReactNode;
@@ -8,16 +8,16 @@ type ProviderProps = {
 
 export const StoryblokProvider = ({ children, isEditor = false }: ProviderProps) => {
   // No access token because this is in client side code.
-  let accessToken = 'thisisnotarealtokenasitisontheclientsideandgoesintothecode';
+  // let accessToken = 'thisisnotarealtokenasitisontheclientsideandgoesintothecode';
 
-  if (isEditor) {
-    if (typeof window !== 'undefined') {
-      const urlParams = new URLSearchParams(window.location.search);
-      accessToken = urlParams.get('access_key') || accessToken;
-    }
-  }
+  // if (isEditor) {
+  //   if (typeof window !== 'undefined') {
+  //     const urlParams = new URLSearchParams(window.location.search);
+  //     accessToken = urlParams.get('access_key') || accessToken;
+  //   }
+  // }
 
-  initStoryblokClient({ accessToken, isEditor });
+  // initStoryblokClient({ accessToken, isEditor });
 
   return children;
 };
