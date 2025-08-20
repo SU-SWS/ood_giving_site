abff560baab6ce0b24c809984ea932e3cbb75313
Eric Bakenhus <1059679+ericbakenhus@users.noreply.github.com>
2025-08-20 00:01:34 -0500

    DS-1816: Fix

--- utilities/storyblok.tsx
+++ utilities/storyblok.tsx
@@ -1,4 +1,4 @@
-import { apiPlugin, storyblokInit } from '@storyblok/react/rsc';
+import { apiPlugin, StoryblokClient, storyblokInit } from '@storyblok/react/rsc';
 import { ComponentNotFound } from '@/components/Storyblok/ComponentNotFound';
 import { SbContentMenuPicker } from '@/components/Storyblok/SbContentMenu';
 import { SbEmbedScript } from '@/components/Storyblok/SbEmbedScript';
@@ -143,7 +143,7 @@ export type GetStoryblokApiConfig = {
 export const getStoryblokClient = ({
   accessToken,
   isEditor,
-}: GetStoryblokApiConfig = {}) => {
+}: GetStoryblokApiConfig = {}): StoryblokClient => {
   accessToken ??= isEditor ? process.env.STORYBLOK_PREVIEW_EDITOR_TOKEN : process.env.STORYBLOK_ACCESS_TOKEN;
 
   return storyblokInit({
