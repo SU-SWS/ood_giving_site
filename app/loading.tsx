0eff60076a57bca4390ba94b9e8daae806c12a8f
Yvonne Tang <ytang2@stanford.edu>
2025-08-21 18:49:21 -0700

    Add simplified header to loading; remove storyblok route loading file

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,8 +1,14 @@
 import { Grid } from '@/components/Grid';
+import { LogoLockup } from '@/components/Logo';
 
 const Loading = () => {
   return (
     <div className="bg-white">
+      <header className="cc border-t-[4rem] md:border-t-[1rem] border-t-cardinal-red shadow-md">
+        <div className="flex max-md:h-[6.8rem] md:pt-70 md:pb-21 lg:pt-45 lg:pb-[7.1rem] text-21 sm:text-25 md:text-[3.2rem]">
+          <LogoLockup text="Giving" />
+        </div>
+      </header>
       <main>
         <Grid gap="default" mt={10} mb={10} className="cc bg-white animate-[skeleton_2s_linear_infinite]">
           <div className="w-full h-300 bg-black-10" />
