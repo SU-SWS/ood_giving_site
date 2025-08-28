cd73a9f35e7bd3d0388ed5e30346db42db7bed55
Yvonne Tang <ytang2@stanford.edu>
2025-08-28 15:39:39 -0700

    Allow nested ctaLink in WYSIWYG; test simplify add UTM in CtaExternalLink

--- components/Cta/CtaExternalLink.tsx
+++ components/Cta/CtaExternalLink.tsx
@@ -1,16 +1,14 @@
 'use client';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { cnb } from 'cnbuilder';
 import { CtaContent } from './CtaContent';
 import { type CtaCommonProps } from './Cta.types';
-import { type SbLinkType } from '../Storyblok/Storyblok.types';
 import { marginTops, marginBottoms } from '@/utilities/datasource';
 import * as styles from './Cta.styles';
 import useUTMs from '@/hooks/useUTMs';
 
 export type CtaExternalLinkProps = React.ComponentPropsWithoutRef<'a'> & CtaCommonProps & {
-  sbLink?: SbLinkType;
-  href?: string;
+  href: string;
   rel?: string;
 };
 
@@ -36,13 +34,7 @@ export const CtaExternalLink = React.forwardRef<HTMLAnchorElement, CtaExternalLi
 
   // Add UTM params to Stanford URLs.
   const { isStanfordUrl, addUTMsToUrl } = useUTMs();
-  const [myHref, setMyHref] = useState<string>(href);
-  useEffect(() => {
-    if (typeof window === 'undefined') return;
-    if (isStanfordUrl(href)) {
-      setMyHref(addUTMsToUrl(href));
-    }
-  }, [href, isStanfordUrl, addUTMsToUrl]);
+  const myHref = isStanfordUrl(href) ? addUTMsToUrl(href) : href;
 
   return (
     <a
