558d136f9a06bce1e886f6404626545306d90c45
Yvonne Tang <ytang2@stanford.edu>
2025-09-10 18:09:48 -0700

    try cache limit hack

--- next.config.ts
+++ next.config.ts
@@ -17,6 +17,12 @@ const nextConfig: NextConfig = {
     CONTEXT: process.env.CONTEXT || 'development',
     STORYBLOK_PREVIEW_EDITOR_TOKEN: process.env.STORYBLOK_PREVIEW_EDITOR_TOKEN,
   },
+  /**
+   * Hack to get over the 2MB limit on cacheHandler
+   * This just reimports the default one but it skips the limit
+   * See: https://github.com/vercel/next.js/discussions/48324#discussioncomment-10542097
+   */
+  cacheHandler: require.resolve('next/dist/server/lib/incremental-cache/file-system-cache.js'),
 };
 
 export default nextConfig;
