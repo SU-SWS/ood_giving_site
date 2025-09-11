ce9d4bf7c9be7af3d32dfbacf65a82f2c3ce616f
Yvonne Tang <ytang2@stanford.edu>
2025-09-10 17:47:16 -0700

    Rewrite netlify-plugin-contextual-auth as es module

--- netlify/build-plugins/netlify-plugin-contextual-auth/index.js
+++ netlify/build-plugins/netlify-plugin-contextual-auth/index.js
@@ -1,30 +1,26 @@
-const fs = require('fs');
+import { appendFileSync } from 'fs';
 
-module.exports = {
-  async onPostBuild({ inputs, utils: { build, status } }) {
-    const contextMatch = inputs.context.includes(process.env.CONTEXT);
-    const branchMatch = inputs.branch
-      ? inputs.branch === process.env.BRANCH
-      : true;
+export async function onPostBuild({ inputs, utils: { build, status } }) {
+  const contextMatch = inputs.context.includes(process.env.CONTEXT);
+  const branchMatch = inputs.branch
+    ? inputs.branch === process.env.BRANCH
+    : true;
 
-    // Correct build context?
-    if (contextMatch && branchMatch) {
-      try {
-        // Add basic auth headers
-        fs.appendFileSync(
-          inputs.file,
-          `\n${inputs.path}\n  Basic-Auth: ${inputs.username}:${inputs.password}`,
-        );
-      } catch (error) {
-        // Report a user error
-        build.failBuild('Error message', { error });
-      }
-
-      // Display success information
-      console.log(
-        `Added basic auth "${inputs.username}:${inputs.password} for path "${inputs.path}""`,
-      );
-      status.show({ summary: 'Success!' });
+  // Correct build context?
+  if (contextMatch && branchMatch) {
+    try {
+      // Add basic auth headers
+      appendFileSync(
+        inputs.file,
+        `\n${inputs.path}\n  Basic-Auth: ${inputs.username}:${inputs.password}`);
+    } catch (error) {
+      // Report a user error
+      build.failBuild('Error message', { error });
     }
-  },
-};
+
+    // Display success information
+    console.log(
+      `Added basic auth "${inputs.username}:${inputs.password} for path "${inputs.path}""`);
+    status.show({ summary: 'Success!' });
+  }
+}
