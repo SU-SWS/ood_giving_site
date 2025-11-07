28d32ee89466ff4171a609939ffb5d7ab5d4effd
Shea McKinney <sheamck@stanford.edu>
2025-11-06 21:15:37 -0800

    Add origin to settings.

--- components/EmbedVideo/EmbedVideo.tsx
+++ components/EmbedVideo/EmbedVideo.tsx
@@ -48,6 +48,7 @@ export const EmbedVideo = ({
           config={{
             youtube: {
               start: startTimeInSeconds,
+              origin: process.env.DEPLOY_PRIME_URL || process.env.URL || 'https://giving.stanford.edu',
             },
           }}
         />
