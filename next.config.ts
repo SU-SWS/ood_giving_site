9e5f69508c0ffc7a652d5749eb213298c9fcc88b
Shea McKinney <sheamck@stanford.edu>
2025-12-04 10:57:59 -0800

    Removed ghost env var.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   cacheComponents: true,
+  experimental: {
+    cpus: 10,
+  },
   images: {
     remotePatterns: [
       {
