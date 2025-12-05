5f46fdb802531ba6ab98ee392a5cf5c9e94bbda3
Yvonne Tang <ytang2@stanford.edu>
2025-12-04 17:23:46 -0800

    Remove ideal from support page taxonomy

--- components/Storyblok/SbSupportPage/taxonomy.ts
+++ components/Storyblok/SbSupportPage/taxonomy.ts
@@ -5,7 +5,6 @@ export const areasToSupport = {
   'athletics': 'Athletics',
   'business-economics': 'Business + Economics',
   'culture-ethics': 'Culture, Ethics, + Service',
-  ideal: 'IDEAL',
   'law-policy-government': 'Law, Policy, + Government',
   'medicine-healthcare': 'Medicine',
   'science-technology': 'Science + Technology',
